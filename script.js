120f638a1cc5fb077d82aabdb3deb832a9059169
jyjiayi <sojiayi@gmail.com>
2021-07-16 00:33:07 +0800

    I have completed the Reverse Game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var main = function (input) {
+  //firstly, process if the input is valid
   if (inputValidation(input)) {
-    return `You entered ${input} which is not scissors, paper or stone. Please try again.`;
+    return `You entered ${input} which is not a valid input. Only the following are accepted: scissors, paper, stone, reversed scissors, reversed paper or reversed stone. Please try again.`;
   }
 
   var randNumber = randomNumGenerator();
@@ -8,20 +9,36 @@ var main = function (input) {
   var handFromProgram = playByProgram(randNumber);
   var result = winningHand(input, handFromProgram);
 
-  var myOutputValue = `You played ${input} while the the computer played ${handFromProgram}. <br><br> ${result} <br><br> Let's play again!!!`;
+  //next, process if user wants to play reversed
+  if (
+    input == "reversed scissors" ||
+    input == "reversed paper" ||
+    input == "reversed stone"
+  ) {
+    var result = reversedGameWinningHand(input, handFromProgram);
+  }
+
+  var myOutputValue = `You played ${input} while the computer played ${handFromProgram}. <br><br> ${result} <br><br> Let's play again!!!`;
   return myOutputValue;
 };
 
 //to validate user input
 var inputValidation = function (userGuess) {
   if (
-    !(userGuess == "scissors" || userGuess == "stone" || userGuess == "paper")
+    !(
+      userGuess == "scissors" ||
+      userGuess == "stone" ||
+      userGuess == "paper" ||
+      userGuess == "reversed scissors" ||
+      userGuess == "reversed stone" ||
+      userGuess == "reversed paper"
+    )
   ) {
     return true;
   }
 };
 
-//to evaluate the winner
+//to evaluate the winner for normal play
 var winningHand = function (userInput, programPlay) {
   if (userInput == programPlay) {
     return "It is a draw!";
@@ -39,6 +56,31 @@ var winningHand = function (userInput, programPlay) {
     (userInput == "paper" && programPlay == "scissors")
   ) {
     return "Oh no, you lost!";
+  }
+};
+
+//to evaluate the winner for reversed play
+var reversedGameWinningHand = function (userInput, programPlay) {
+  if (
+    (userInput == "reversed scissors" && programPlay == "scissors") ||
+    (userInput == "reversed paper" && programPlay == "paper") ||
+    (userInput == "reversed stone" && programPlay == "stone")
+  ) {
+    return "It is a draw!";
+  }
+  if (
+    (userInput == "reversed scissors" && programPlay == "stone") ||
+    (userInput == "reversed paper" && programPlay == "scissors") ||
+    (userInput == "reversed stone" && programPlay == "paper")
+  ) {
+    return "You are playing the reversed game. <br><br> Congrats, you win!";
+  }
+  if (
+    (userInput == "reversed stone" && programPlay == "scissors") ||
+    (userInput == "reversed scissors" && programPlay == "paper") ||
+    (userInput == "reversed paper" && programPlay == "stone")
+  ) {
+    return "You are playing the reversed game. <br><br> Oh no, you lost!";
   }
 };
 
