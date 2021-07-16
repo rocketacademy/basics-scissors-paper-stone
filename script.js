fcb483172ca30e31815bb061cb40c2bf649d6eaf
codingisabella <isabella.lung@hotmail.com>
2021-07-17 07:17:09 +0800

    created the base SPS game

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  // randomly chooses scissors, peper, or stone
+  var userOption = input;
+  console.log("userOption", userOption);
+  var computerOption = computerChoice();
+  console.log("computerOption", computerOption);
+
+  var myOutputValue = `you guessed ${input}, there are only three options stone, paper, scissors`;
+
+  // scissor beats paper, paper beats stone, stone beats scissors, draw
+  // myOutputValue = user won, program won, or its a draw
+  if (
+    (userOption == "scissors" && computerOption == "paper") ||
+    (userOption == "paper" && computerOption == "stone") ||
+    (userOption == "stone" && computerOption == "scissors")
+  ) {
+    myOutputValue = `user won!`;
+  }
+  if (
+    (computerOption == "scissors" && userOption == "paper") ||
+    (computerOption == "paper" && userOption == "stone") ||
+    (computerOption == "stone" && userOption == "scissors")
+  ) {
+    myOutputValue = `computer won!`;
+  }
+
+  if (userOption == computerOption) {
+    myOutputValue = `draw`;
+  }
   return myOutputValue;
+};
+// user inputs == scissors, paper or stone
+// computer inputs == scissors, paper or stone
+var computerChoice = function () {
+  var randomNumber = Math.random() * 4;
+  var randomInteger = Math.floor(randomNumber);
+  var computerNumber = randomInteger;
+  if (computerNumber == 1) {
+    computerOption = "scissors";
+  }
+  if (computerNumber == 2) {
+    computerOption = "paper";
+  }
+  if (computerNumber == 3) {
+    computerOption = "stone";
+  }
+  return computerOption;
 };
