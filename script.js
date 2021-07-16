63380f25b19182ddf8c433fea2dca6a073a037ac
brianiptzekai <brian.iptzekai@gmail.com>
2021-07-17 03:02:30 +0800

    adding emoji object

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 // Randomly select a choice for computer
 var randomChoice = function () {
   var choiceNumber = Math.ceil(Math.random() * 3);
+  var choiceText;
   if (choiceNumber == 1) {
-    return "scissors";
+    choiceText = 'scissors';
   } else if (choiceNumber == 2) {
-    return "paper";
+    choiceText = 'paper';
   } else if (choiceNumber == 3) {
-    return "stone";
+    choiceText = 'stone';
   }
+  return choiceText;
 };
 
+// Main function
 var main = function (input) {
   var computerChoice = randomChoice();
   var reverseCheck;
 
-  if (input == "scissors" || input == "paper" || input == "stone") {
+  if (input == 'scissors' || input == 'paper' || input == 'stone') {
     reverseCheck = false;
   } else if (
-    input == "reversed scissors" ||
-    input == "reversed paper" ||
-    input == "reversed stone"
+    input == 'reversed scissors' ||
+    input == 'reversed paper' ||
+    input == 'reversed stone'
   ) {
     reverseCheck = true;
-    if (input == "reversed scissors") {
-      input = "scissors";
-    } else if (input == "reversed paper") {
-      input = "paper";
-    } else if (input == "reversed stone") {
-      input = "stone";
+    if (input == 'reversed scissors') {
+      input = 'scissors';
+    } else if (input == 'reversed paper') {
+      input = 'paper';
+    } else if (input == 'reversed stone') {
+      input = 'stone';
     }
   } else {
     return `"${input}" is not an option. Please enter either "scissors", "paper", or "stone" (lower case).`;
@@ -41,34 +44,52 @@ var main = function (input) {
 
 // Output message
 var outputMessage = function (score, userChoice, computerChoice) {
+  var message;
+  var userEmoji = choiceToObject(userChoice);
+  var computerEmoji = choiceToObject(computerChoice);
   if (score == 0) {
-    return `You chose ${userChoice}. <br> The computer chose ${computerChoice}. <br><br> It's a draw!`;
+    message = `You chose ${userEmoji}. <br> The computer chose ${computerEmoji}. <br><br> It's a draw!`;
   } else if (score == 1) {
-    return `You chose ${userChoice}. <br> The computer chose ${computerChoice}. <br><br> You win!`;
+    message = `You chose ${userEmoji}. <br> The computer chose ${computerEmoji}. <br><br> You win!`;
   } else if (score == -1) {
-    return `You chose ${userChoice}. <br> The computer chose ${computerChoice}. <br><br> You lose!`;
+    message = `You chose ${userEmoji}. <br> The computer chose ${computerEmoji}. <br><br> You lose!`;
   }
+  return message;
+};
+
+var choiceToObject = function (choice) {
+  var object;
+  if (choice == 'scissors') {
+    object = 'scissors ✂️';
+  } else if (choice == 'paper') {
+    object = 'paper 📄';
+  } else if (choice == 'stone') {
+    object = 'stone 🗿';
+  }
+  return object;
 };
 
 // Compare user's choice vs computer's choice
 var compareWinner = function (user, computer, reversed) {
+  var score;
   if (user == computer) {
-    return 0;
+    score = 0;
   } else if (
-    (user == "scissors" && computer == "paper") ||
-    (user == "paper" && computer == "stone") ||
-    (user == "stone" && computer == "scissors")
+    (user == 'scissors' && computer == 'paper') ||
+    (user == 'paper' && computer == 'stone') ||
+    (user == 'stone' && computer == 'scissors')
   ) {
     if (reversed) {
-      return -1;
+      score = -1;
     } else {
-      return 1;
+      score = 1;
     }
   } else {
     if (reversed) {
-      return 1;
+      score = 1;
     } else {
-      return -1;
+      score = -1;
     }
   }
+  return score;
 };
