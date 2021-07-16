0a7bb7ed4f704203c9055dd8a6ce9cb1c4e25e74
Goh Yong Siang <gysiang@gmail.com>
2021-07-16 14:09:08 +0800

    working version with input validator

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
-};
+
+  var myOutputValue = `you lose`;
+
+  var comChoice = generateChoice();
+  console.log(input, comChoice)
+
+//input validator
+  if (input != `scissors` | input != `paper` | input != `stone`){
+  return myOutputValue = `You entered ${input}, please enter scissors, paper or stone and try again`
+  }
+
+// winning conditions
+  if (
+  (input == `stone` && comChoice == `scissors`) ||
+  (input == `paper` && comChoice == `stone`) ||
+  (input == `scissors` && comChoice == `paper`)
+  ) {
+    myOutputValue = `I picked ` + input + ` and Computer picked ` + comChoice + `. I win! Hooray!`
+  }
+// draw conditions
+  if (input == comChoice) {
+    myOutputValue = `I picked ` + input + ` and Computer picked ` + comChoice + `. Try Again!`
+  }
+  console.log(input,comChoice)
+  return myOutputValue
+}
+
+// Assign a choice to computer via randomNumber function
+
+var generateChoice = function() {
+  var randomComChoice = randomNumber();
+  if (randomComChoice == 1) {
+    return `scissors`;
+  }
+
+  if (randomComChoice == 2) {
+    return `paper`;
+  }
+
+  else { return`stone`;
+  }
+
+}
+
+var randomNumber = function () {
+
+  var randomDecimal = Math.random() * 3;
+
+  var randomInterger = Math.floor(randomDecimal);
+
+  var randomNumber = randomInterger + 1;
+
+  return randomNumber;
+
+}
