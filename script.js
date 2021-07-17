1fa0d2d4b0f7c9aa78d20ca5a2b3274d8a3565d8
EricFooSD <fooshida@gmail.com>
2021-07-17 15:23:10 +0800

    include comments to explain code

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-var main = function (input) {
-  var computerMove = randomItem();
-  console.log("computer", computerMove);
-
-  var iconComputer = iconLogo(computerMove);
-  console.log("iconComputer", iconComputer);
-
-  var iconPlayer = iconLogo(input);
-  console.log("iconPlayer", iconPlayer);
-
-  var draw = drawGame(input, computerMove);
-  var win = playerWin(input, computerMove);
-  var tryAgain = invalid(input);
-
-  var myOutputValue = `The computer played ${computerMove} ${iconComputer} .<br>You played ${input} ${iconPlayer}.<br>You lost ='( <br>Lets play another round!`;
-  if (tryAgain) {
-    myOutputValue = `Please type in scissors, paper or stone.`;
-  }
-  if (win) {
-    myOutputValue = `The computer played ${computerMove} ${iconComputer}.<br>You played ${input} ${iconPlayer}.<br>You won!!!<br>Lets play another round!`;
-  }
-  if (draw) {
-    myOutputValue = `The computer played ${computerMove} ${iconComputer}.<br>You played ${input} ${iconPlayer}.<br>It's a draw!<br>Lets play another round!`;
-  }
-  return myOutputValue;
-};
-
+// function to generate random computer move
 var randomItem = function () {
   var random = Math.floor(Math.random() * 3);
   var programMove = "stone";
@@ -37,20 +11,12 @@ var randomItem = function () {
   return programMove;
 };
 
+// function to determine when game is a draw where computer's move and players move is the same.
 var drawGame = function (input, computerMove) {
   return input.includes(computerMove);
 };
 
-/*
-var drawGameReversed = function (input, computerMove) {
-  return (
-    (input == "reversed scissors" && computerMove == "scissors") ||
-    (input == "reversed stone" && computerMove == "stone") ||
-    (input == "reversed paper" && computerMove == "paper")
-  );
-};
-*/
-
+// function to determine when player wins.
 var playerWin = function (input, computerMove) {
   return (
     ((input == "scissors" || input == "reversed stone") &&
@@ -62,16 +28,7 @@ var playerWin = function (input, computerMove) {
   );
 };
 
-/*
-var playerWinReversed = function (input, computerMove) {
-  return (
-    (input == "reversed paper" && computerMove == "scissors") ||
-    (input == "reversed stone" && computerMove == "paper") ||
-    (input == "reversed scissors" && computerMove == "stone")
-  );
-};
-*/
-
+// function to determine when player inputs an invalid guess.
 var invalid = function (input) {
   return (
     input != "scissors" &&
@@ -83,6 +40,7 @@ var invalid = function (input) {
   );
 };
 
+// function to return the logo corresponding to the move.
 var iconLogo = function (look) {
   var logo = "none";
   if (look == "scissors" || look == "reversed scrissors") {
@@ -95,4 +53,42 @@ var iconLogo = function (look) {
     logo = "🗿";
   }
   return logo;
+};
+
+// Main game
+
+var main = function (input) {
+  // to generate computer's move. console log to see response and verify outcome.
+  var computerMove = randomItem();
+  console.log("computer", computerMove);
+
+  // to generate the icons for the moves
+  var iconComputer = iconLogo(computerMove);
+  console.log("iconComputer", iconComputer);
+  var iconPlayer = iconLogo(input);
+  console.log("iconPlayer", iconPlayer);
+
+  // to call the win, draw, invalid functions in main to help determine outcome.
+  var draw = drawGame(input, computerMove);
+  var win = playerWin(input, computerMove);
+  var tryAgain = invalid(input);
+
+  //set base output as lose scenario
+  var myOutputValue = `The computer played ${computerMove} ${iconComputer} .<br>You played ${input} ${iconPlayer}.<br>You lost ='( <br>Lets play another round!`;
+
+  // IF condition to determine if player has input an invalid guess with corresponding. output.
+  if (tryAgain) {
+    myOutputValue = `Please type in scissors, paper or stone.`;
+  }
+
+  // IF condition to determine if player wins with corresponding output.
+  if (win) {
+    myOutputValue = `The computer played ${computerMove} ${iconComputer}.<br>You played ${input} ${iconPlayer}.<br>You won!!!<br>Lets play another round!`;
+  }
+
+  // IF condition to determine if game is a draw with corresponding output.
+  if (draw) {
+    myOutputValue = `The computer played ${computerMove} ${iconComputer}.<br>You played ${input} ${iconPlayer}.<br>It's a draw!<br>Lets play another round!`;
+  }
+  return myOutputValue;
 };
