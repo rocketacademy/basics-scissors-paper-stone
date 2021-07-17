50a9e0dbe3645e07f8dbcc1bf3988450752fb576
justinnjamesleong <justinnjamesleong@gmail.com>
2021-07-17 13:02:31 +0800

    second try success

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-var main = function (input) {
-  // ensure that player types the correct input//
-  if (input != "scissors" || input != "paper" || input != "stone") return;
-  ('Please type in either "scissors" , "paper", or "stone" to begin playing.');
-
-  // list the 9 different scenarious//
-  if (input == "scissors" && spsthrow == 1) return;
-  ("You threw scissors and the computer threw scissors. Nobody won.");
-
-  if (input == "scissors" && spsthrow == 2) return;
-  ("You threw scissors and the computer threw paper. You won.");
-
-  if (input == "scissors" && spsthrow == 3) return;
-  ("You threw scissors and the computer threw rock. You lost.");
-
-  if (input == "paper" && spsthrow == 1) return;
-  ("You threw paper and the computer threw scissors. You lost.");
-
-  if (input == "paper" && spsthrow == 2) return;
-  ("You threw paper and the computer threw paper. Nobody won.");
-
-  if (input == "paper" && spsthrow == 3)
-    return "You threw paper and the computer threw rock. You won.";
-
-  if (input == "stone" && spsthrow == 1) return;
-  ("You threw rock and the computer threw scissors. You won.");
-
-  if (input == "stone" && spsthrow == 2) return;
-  ("You threw rock and the computer threw paper. You lost.");
-
-  if (input == "stone" && spsthrow == 3) return;
-  ("You threw rock and the computer threw rock. Nobody won.");
-  return myOutputValue;
-};
-
 //create a random number generator from 1 to 3
-var spsthrow = function () {
-  var decimal = Math.random() * 3;
-  var integer = Math.floor(decimal);
-  var rand = integer + 1;
-  return rand;
+var cthrow = function () {
+  var randomdecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomdecimal);
+  var scissorpaperstonevalue = randomInteger + 1;
+  return scissorpaperstonevalue;
+};
+//player phase
+var main = function (input) {
+  var cputhrow = cthrow();
+  // ensure that player types the correct input
+  if (input != "scissors" && input != "paper" && input != "stone")
+    var myOutputValue =
+      'Please type in either "scissors" , "paper", or "stone" to begin playing.';
+  // list the 9 different scenarious
+  if (input == "scissors" && cputhrow == 1)
+    var myOutputValue =
+      "You threw scissors and the computer threw scissors. Nobody won.";
+  if (input == "scissors" && cputhrow == 2)
+    var myOutputValue =
+      "You threw scissors and the computer threw paper. You won.";
+  if (input == "scissors" && cputhrow == 3)
+    var myOutputValue =
+      "You threw scissors and the computer threw rock. You lost.";
+  if (input == "paper" && cputhrow == 1)
+    var myOutputValue =
+      "You threw paper and the computer threw scissors. You lost.";
+  if (input == "paper" && cputhrowno == 2)
+    var myOutputValue =
+      "You threw paper and the computer threw paper. Nobody won.";
+  if (input == "paper" && cputhrow == 3)
+    var myOutputValue = "You threw paper and the computer threw rock. You won.";
+  if (input == "stone" && cputhrow == 1)
+    var myOutputValue =
+      "You threw rock and the computer threw scissors. You won.";
+  if (input == "stone" && cputhrow == 2)
+    var myOutputValue =
+      "You threw rock and the computer threw paper. You lost.";
+  if (input == "stone" && cputhrow == 3)
+    var myOutputValue =
+      "You threw rock and the computer threw rock. Nobody won.";
+  return myOutputValue;
 };
