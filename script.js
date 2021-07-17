5540778a7cfa0550c8883e476984b2ba41f6e07c
diyanarmln <diyana.rmln@gmail.com>
2021-07-18 01:33:36 +0800

    refactoring for part 1

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+// generate random number
+var getRandomNumber = function () {
+  // generate random whole number from 1 to 3
+  var setRandomInteger = Math.floor(Math.random() * 3) + 1;
+  return setRandomInteger;
+};
+
+// generate random game values
+var getRandomValue = function () {
+  var randomValue = getRandomNumber();
+  // assign number to game values
+  if (randomValue == 1) {
+    return "scissors";
+  }
+  if (randomValue == 2) {
+    return "paper";
+  }
+  if (randomValue == 3) {
+    return "stone";
+  }
+};
+
 var main = function (input) {
-  // to run function of generating random hand
-  var compHand = randomValue();
+  // generate value when submit button is clicked
+  var compHand = getRandomValue();
   console.log(compHand);
-  // input validation if input is not scissors paper or stone
+
+  // set display message if user lost
+  var myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Sorry, you lost!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
+
+  // input validation
   if (
     input != "scissors" ||
     input != "paper" ||
@@ -11,66 +37,29 @@ var main = function (input) {
     input != "reversed paper" ||
     input != "reversed stone"
   ) {
+    // set display message if input validation not met
     myOutputValue = `Please enter "scissors" "paper" or "stone" to start playing!
     <br><br> Like a challenge? <br>Begin your entry with "reversed"<space> to try your hand at a reversed Scissors Paper Stone game!`;
   }
 
-  // if input is same as comp = draw
-  if (input == compHand || input == "reversed " + compHand) {
-    myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Bummer, it's a draw!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
+  // check if user draw
+  if (input == compHand || input == "reversed ${getRandomValue}") {
+    // set display message if user draw
+    return `You threw ${input}.<br> The computer threw ${compHand}.<br> Bummer, it's a draw!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
   }
 
-  // REVERSE GAMEPLAY
+  // check if user won
   if (
-    input == "reversed scissors" ||
-    input == "reversed paper" ||
-    input == "reversed stone"
-  ) {
-    //game rules for losing hand
-    if (
-      (input == "reversed scissors" && compHand == "paper") ||
-      (input == "reversed paper" && compHand == "stone") ||
-      (input == "reversed stone" && compHand == "scissors")
-    ) {
-      myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Sorry, you lost!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
-    }
-    // game rules for winning hand
-    if (
-      (input == "reversed paper" && compHand == "scissors") ||
-      (input == "reversed stone" && compHand == "paper") ||
-      (input == "reversed scissors" && compHand == "stone")
-    ) {
-      myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Congratulations, you won!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
-    }
-  }
-
-  // NORMAL GAMEPLAY
-  // game rules for winning hands
-  if (
+    (input == "reversed paper" && compHand == "scissors") ||
+    (input == "reversed stone" && compHand == "paper") ||
+    (input == "reversed scissors" && compHand == "stone") ||
     (input == "scissors" && compHand == "paper") ||
     (input == "paper" && compHand == "stone") ||
     (input == "stone" && compHand == "scissors")
   ) {
-    myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Congratulations, you won!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
-  }
-  // game rules for losing hands
-  if (
-    (input == "paper" && compHand == "scissors") ||
-    (input == "stone" && compHand == "paper") ||
-    (input == "scissors" && compHand == "stone")
-  ) {
-    myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Sorry, you lost!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
+    // set display message if user won
+    return `You threw ${input}.<br> The computer threw ${compHand}.<br> Congratulations, you won!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
   }
 
   return myOutputValue;
 };
-
-// function to generate random game values in output
-var randomValue = function (input) {
-  var gameValues = ["scissors", "paper", "stone"],
-    randomGen = gameValues[Math.floor(Math.random() * gameValues.length)];
-  // console.log(gameValues);
-  return randomGen;
-};
-
-// if entry starts with reversed, then follow below, else
