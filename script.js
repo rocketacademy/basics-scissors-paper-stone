62c2cfee97a67bb29e7d106badab9e9a85901e87
daintyflowerss <84612691+daintyflowerss@users.noreply.github.com>
2021-07-17 10:07:36 +0800

    Abigail-Section-6

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  // randomly chooses scissors, paper or stone
+  var useroption =input;
+  var option1 = "scissors";
+  var option2 = "paper";
+  var option3 = "stone";
+  if (user option == !("scissors" || "paper" || "rock")){
+    myOutputValue = 'you guessed ${input}, there are only theree options stone, paper, scissors';
+  }
+  if(
+    ( useroption == "scissors" && computeroption == "paper") ||
+    (useroption == "paper" && computeroption =="stone") ||
+    (useroption == "stone" && computeroption == "scissors")
+  ) {
+    myOutputValue = 'user won!';
+  }
+  if (
+    (computeroption == "scissors" && useroption == "paper") ||
+    (computeroption == "paper" && useroption == "stone") ||
+    (computeroption == "stone" && useroption == "scissors")
+  ) {
+    myOutputValue = 'computer won!';
+  }
+  if (useroption == computeroption) {
+    myOutputValue = 'draw';
+  }
+  return myOutputValue
+  return "Invaild Input. Please input either Scissors, Paper, or Stone"
 };
