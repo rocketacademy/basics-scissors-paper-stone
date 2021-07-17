00b77d7bf1db08e3c22ecad171ed574644c1592a
Norman <norman.au@rate.com.sg>
2021-07-17 09:57:04 +0800

    added scissorspaperstone game - norman's version

--- script.js
+++ script.js
@@ -1,4 +1,77 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  return scissorsPaperStone(input);
+};
+
+let scissorsPaperStone = function (input) {
+  let randomDecimal = Math.random() * 3;
+  let randomInteger = Math.floor(randomDecimal);
+  // 0 == scissors
+  // 1 == paper
+  // 2 == stone
+  if (input == "scissors" || input == "paper" || input == "stone") {
+    if (randomInteger == 0) {
+      if (input == "scissors") {
+        return `user uses ${input} and program chooses scissors. <br><br> Draw!`;
+      } else if (input == "paper") {
+        return `user uses ${input} and program chooses scissors. <br><br>User loses!`;
+      } else {
+        return `user uses ${input} and program chooses scissors. <br><br>User wins!`;
+      }
+    } else if (randomInteger == 1) {
+      if (input == "scissors") {
+        return `user uses ${input} and program chooses paper. <br><br>User wins!`;
+      } else if (input == "paper") {
+        return `user uses ${input} and program chooses paper. <br><br>Draw!`;
+      } else {
+        return `user uses ${input} and program chooses paper. <br><br>User loses!`;
+      }
+    } else {
+      if (input == "scissors") {
+        return `user uses ${input} and program chooses stone. <br><br>User loses!`;
+      } else if (input == "paper") {
+        return `user uses ${input} and program chooses stone. <br><br>User wins!`;
+      } else {
+        return `user uses ${input} and program chooses stone. <br><br>Draw!`;
+      }
+    }
+  } else {
+    return `error, user did not input scissors paper or stone. <br><br>Try again!`;
+  }
+};
+
+let scissorsPaperStoneReversed = function (input) {
+  let randomDecimal = Math.random() * 3;
+  let randomInteger = Math.floor(randomDecimal);
+  // 0 == scissors
+  // 1 == paper
+  // 2 == stone
+  if (input == "scissors" || input == "paper" || input == "stone") {
+    if (randomInteger == 0) {
+      if (input == "scissors") {
+        return `user uses ${input} and program chooses scissors. <br><br> Draw!`;
+      } else if (input == "paper") {
+        return `user uses ${input} and program chooses scissors. <br><br>User wins!`;
+      } else {
+        return `user uses ${input} and program chooses scissors. <br><br>User loses!`;
+      }
+    } else if (randomInteger == 1) {
+      if (input == "scissors") {
+        return `user uses ${input} and program chooses paper. <br><br>User loses!`;
+      } else if (input == "paper") {
+        return `user uses ${input} and program chooses paper. <br><br>Draw!`;
+      } else {
+        return `user uses ${input} and program chooses paper. <br><br>User wins!`;
+      }
+    } else {
+      if (input == "scissors") {
+        return `user uses ${input} and program chooses stone. <br><br>User wins!`;
+      } else if (input == "paper") {
+        return `user uses ${input} and program chooses stone. <br><br>User loses!`;
+      } else {
+        return `user uses ${input} and program chooses stone. <br><br>Draw!`;
+      }
+    }
+  } else {
+    return `error, user did not input scissors paper or stone. <br><br>Try again!`;
+  }
 };
