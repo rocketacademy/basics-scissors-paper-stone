b47c85d9ac7cd401a0bd3a1ab1f3f1055ce4dce4
diyanarmln <diyana.rmln@gmail.com>
2021-07-18 14:04:56 +0800

    add win-loss record function

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// win-loss record
+stateWinUser = 0;
+stateWinComp = 0;
+stateDraw = 0;
+stateWinUserPercent = 0;
+stateWinCompPercent = 0;
+numGamesPlayed = 0;
+
+// calculate win % of user
+var getWinUserPercent = function () {
+  var calcWinUserPercent = Math.floor((stateWinUser / numGamesPlayed) * 100);
+  if (stateWinUser == 0) {
+    return `You have won 0 times.`;
+  }
+  return `You have won ${stateWinUser} time(s), which is ${calcWinUserPercent}% of the time.`;
+};
+
+// calculate win % of computer
+var getWinCompPercent = function () {
+  var calcWinCompPercent = Math.floor((stateWinComp / numGamesPlayed) * 100);
+  if (stateWinComp == 0) {
+    return `The computer has won 0 times.`;
+  }
+  return `Computer has won ${stateWinComp} time(s) which is ${calcWinCompPercent}% of the time.`;
+};
+
+// set display message for number of draws
+var getDrawMessage = function () {
+  if (stateDraw == 0) {
+    return `Number of draws is 0.`;
+  }
+  return `Number of draws is ${stateDraw}`;
+};
+
 // generate random number
 var getRandomNumber = function () {
   // generate random whole number from 1 to 3
@@ -44,8 +78,18 @@ var main = function (input) {
 
   // check if user draw
   if (input == compHand || input == `reversed ${compHand}`) {
+    // add count to number of games played
+    numGamesPlayed += 1;
+    // add count to number of wins by user
+    stateDraw += 1;
+    console.log("stateDraw", stateDraw);
+    var drawMessage = getDrawMessage();
+    // get display message about win rate of user
+    var winRate = getWinUserPercent();
+    // get display message about win rate of comp
+    var winRateComp = getWinCompPercent();
     // set display message if user draw
-    myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Bummer, it's a draw!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
+    myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Bummer, it's a draw!<br><br>${winRate}<br>${winRateComp}<br>${drawMessage}<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
   }
 
   // check if user won
@@ -56,11 +100,22 @@ var main = function (input) {
       compHand == "paper") ||
     ((input == "reversed scissors" || input == "paper") && compHand == "stone")
   ) {
+    // add count to number of games played
+    numGamesPlayed += 1;
+    // add count to number of wins by user
+    stateWinUser += 1;
+    // get display message about draw count
+    var drawMessage = getDrawMessage();
+    // get display message about win rate of user
+    var winRate = getWinUserPercent();
+    // get display message about win rate of comp
+    var winRateComp = getWinCompPercent();
+    console.log("stateWinUser", stateWinUser);
     // set display message if user won
-    myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Congratulations, you won!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
+    myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Congratulations, you won!<br><br>${winRate}<br>${winRateComp}<br> ${drawMessage}<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
   }
 
-  // set display message if user lost
+  // check if user lost
   if (
     ((input == "reversed stone" || input == "paper") &&
       compHand == "scissors") ||
@@ -68,7 +123,20 @@ var main = function (input) {
       compHand == "paper") ||
     ((input == "reversed paper" || input == "scissors") && compHand == "stone")
   ) {
-    myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Sorry, you lost!<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
+    // add count to number of games played
+    numGamesPlayed += 1;
+    // add count to number of wins by comp
+    stateWinComp += 1;
+    // get display message about draw count
+    var drawMessage = getDrawMessage();
+    // get display message about win rate of user
+    var winRate = getWinUserPercent();
+    // get display message about win rate of comp
+    var winRateComp = getWinCompPercent();
+    console.log("stateWinComp", stateWinComp);
+    // set display message if user lost
+    myOutputValue = `You threw ${input}.<br> The computer threw ${compHand}.<br> Sorry, you lost!<br><br>${winRate}<br>${winRateComp}<br>${drawMessage}<br><br>Enter "scissors" "paper" or "stone" to play another round!`;
   }
+  console.log("numGamesPlayed", numGamesPlayed);
   return myOutputValue;
 };
