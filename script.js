114950943368bd72de6695a9668c9e2c3bb12266
Norman <norman.au@rate.com.sg>
2021-07-19 00:28:03 +0800

    finished part 2 base

--- script.js
+++ script.js
@@ -2,7 +2,29 @@ var main = function (input) {
   return scissorsPaperStone(input);
 };
 
+// scissorspaperstone with part 2
+// win-loss record storing
+let countWin = 0;
+let countLose = 0;
+let countDraw = 0;
+let gameStarted = false;
+let userName = ``;
+
 let scissorsPaperStone = function (input) {
+  if (gameStarted == false) {
+    if (isNaN(Number(input)) == true) {
+      userName = input;
+      gameStarted = true;
+      return `welcome to the classic game of scissors paper stone, ${input}!<br><br>To play, key in either scissors, paper or stone and click submit!`;
+    } else {
+      return `you have keyed in an invalid username. Please try again!`;
+    }
+  } else if (gameStarted == true) {
+    return scissorsPaperStoneGame(input);
+  }
+};
+
+let scissorsPaperStoneGame = function (input) {
   let randomDecimal = Math.random() * 3;
   let randomInteger = Math.floor(randomDecimal);
   // 0 == scissors
@@ -11,27 +33,36 @@ let scissorsPaperStone = function (input) {
   if (input == "scissors" || input == "paper" || input == "stone") {
     if (randomInteger == 0) {
       if (input == "scissors") {
-        return `user uses ${input} and program chooses scissors. <br><br> Draw!`;
+        countDraw += 1;
+        return `${userName}<br><br>wins: ${countWin}<br>losses: ${countLose}<br>draws: ${countDraw}<br><br>user uses ${input} and program chooses scissors. <br><br> Draw!`;
       } else if (input == "paper") {
-        return `user uses ${input} and program chooses scissors. <br><br>User loses!`;
+        countLose += 1;
+        return `${userName}<br><br>wins: ${countWin}<br>losses: ${countLose}<br>draws: ${countDraw}<br><br>user uses ${input} and program chooses scissors. <br><br>User loses!`;
       } else {
-        return `user uses ${input} and program chooses scissors. <br><br>User wins!`;
+        countWin += 1;
+        return `${userName}<br><br>wins: ${countWin}<br>losses: ${countLose}<br>draws: ${countDraw}<br><br>user uses ${input} and program chooses scissors. <br><br>User wins!`;
       }
     } else if (randomInteger == 1) {
       if (input == "scissors") {
-        return `user uses ${input} and program chooses paper. <br><br>User wins!`;
+        countWin += 1;
+        return `${userName}<br><br>wins: ${countWin}<br>losses: ${countLose}<br>draws: ${countDraw}<br><br>user uses ${input} and program chooses paper. <br><br>User wins!`;
       } else if (input == "paper") {
-        return `user uses ${input} and program chooses paper. <br><br>Draw!`;
+        countDraw += 1;
+        return `${userName}<br><br>wins: ${countWin}<br>losses: ${countLose}<br>draws: ${countDraw}<br><br>user uses ${input} and program chooses paper. <br><br>Draw!`;
       } else {
-        return `user uses ${input} and program chooses paper. <br><br>User loses!`;
+        countLose += 1;
+        return `${userName}<br><br>wins: ${countWin}<br>losses: ${countLose}<br>draws: ${countDraw}<br><br>user uses ${input} and program chooses paper. <br><br>User loses!`;
       }
     } else {
       if (input == "scissors") {
-        return `user uses ${input} and program chooses stone. <br><br>User loses!`;
+        countLose += 1;
+        return `${userName}<br><br>wins: ${countWin}<br>losses: ${countLose}<br>draws: ${countDraw}<br><br>user uses ${input} and program chooses stone. <br><br>User loses!`;
       } else if (input == "paper") {
-        return `user uses ${input} and program chooses stone. <br><br>User wins!`;
+        countWin += 1;
+        return `${userName}<br><br>wins: ${countWin}<br>losses: ${countLose}<br>draws: ${countDraw}<br><br>user uses ${input} and program chooses stone. <br><br>User wins!`;
       } else {
-        return `user uses ${input} and program chooses stone. <br><br>Draw!`;
+        countDraw += 1;
+        return `${userName}<br><br>wins: ${countWin}<br>losses: ${countLose}<br>draws: ${countDraw}<br><br>user uses ${input} and program chooses stone. <br><br>Draw!`;
       }
     }
   } else {
