0d2918af1279f559752cea1ec0f4c28544efc688
solihinf <solihinf@hotmail.com>
2021-07-18 13:51:52 +0700

    create basic psp

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //generate random number between 1 to 3 as computer choice
-
 var main = function (input){
   var gameSuit = compChoice ();
   
@@ -20,45 +19,61 @@ var main = function (input){
  console.log ('computer choice')
  console.log (gameSuit)
   
-
-
-  
-  var chooseWinner = compare();
+// comparing user input vs comp choice
+  var chooseWinner = compare(input, gameSuit);
   console.log ('compare result')
   console.log (chooseWinner)
   
-  return myOutputValue;
-};
-
-
+  return chooseWinner;
+};  
 
 var compChoice = function (input) {
   var randomNum = Math.floor(Math.random() * 3) + 1; 
   return randomNum
-
+ 
 };
 
-var compare = function (input, compChoice) {
+var compare = function (input, gameSuit) {
+  var output;
+  if (input == 'scissors' && gameSuit == 'scissors') {
+     output = 'It is a draw'
+    console.log ('same input');
+  } 
 
-if (input == compChoice) {
-  myOutputValue = 'It is a draw'
-}
+  if (input == 'paper' && gameSuit == 'paper') {
+    output = 'It is a draw'
+    console.log ('same input');
 
-if (input == 'scissors' && compChoice == 'stone'){
- myOutputValue = 'You lose'}
+  } 
+  if (input == 'stone' && gameSuit == 'stone') {
+    output = 'It is a draw'
+  console.log ('same input');} 
 
- if (input == 'scissors' && compChoice == 'paper'){ myOutputValue = 'You win'}
-
- if (input == 'stone' && compChoice == 'scissors') { myOutputValue = 'You win'}
-
- if (input == 'stone' && compChoice == 'paper'){
-   myOutputValue = 'You lose'}
-
- if (input == 'paper' && compChoice == 'scissors')
-  { myOutputValue = 'You lose'}
-
-  if (input == 'paper' && compChoice == 'stone') {
-    myOutputValue ='You win'
+  if (input == 'scissors' && gameSuit == 'stone'){
+  output = 'You lose'
+  console.log ('scissors stone');
   }
 
-  return myOutputValue;}
+  
+  if (input == 'scissors' && gameSuit == 'paper')
+  { output = 'You win'
+  console.log('scissorpaper')}
+
+  if (input == 'stone' && gameSuit == 'scissors') { output = 'You win'}
+
+  if (input == 'stone' && gameSuit== 'paper'){
+   output  = 'You lose'
+    console.log ('stone paper');}
+
+    
+  if (input == 'paper' && gameSuit == 'scissors')
+    { output = 'You lose'
+  console.log ('paper scissors');}
+
+
+    if (input == 'paper' && gameSuit == 'stone') {
+      output ='You win'
+    console.log ('paper stone');}
+
+  return output;
+  }
