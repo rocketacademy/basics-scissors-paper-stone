f56d04b6f98300774fa79424b0b9a5ec67bb61f7
whyexis <chongyingxiang@gmail.com>
2021-07-19 14:09:45 +0800

    Added Korean version SPS logic

--- project/sps/script.js
+++ project/sps/script.js
@@ -2,50 +2,55 @@
 var SCISSORS = "scissors";
 var PAPER = "paper";
 var STONE = "stone";
-var REVERSED_SCISSORS = "reversed scissors";
-var REVERSED_PAPER = "reversed paper";
-var REVERSED_STONE = "reversed stone";
 var WIN = "win";
 var LOSE = "lose";
 var DRAW = "draw";
+var NORMAL = "normal";
+var REVERSED = "reversed";
+var KOREAN = "korean";
+var COMPUTER = "Computer";
+var NOBODY = "Nobody";
 
 // Global variables
-var reversedMode = false;
+var userName = "Player";
+var result;
 var userWins = 0;
 var computerWins = 0;
 var draws = 0;
-var regexpReversedChoice = /reversed (scissors|paper|stone)/;
+var lastWinner = NOBODY;
 
 // Main
-var main = function (input) {
-  // Check for reversed mode selection first and invalid input
-  if (regexpReversedChoice.test(input)) {
-    reversedMode = true;
-    // Assign the capture group of the regex to user shape
-    input = input.match(regexpReversedChoice)[1];
+var main = function (name, mode, choice) {
+  // Set user name if the input is not blank, otherwise use the default value "Player"
+  if (name != "") {
+    userName = name;
   }
 
-  // Check for other invalid input
-  if (!(input == SCISSORS || input == PAPER || input == STONE)) {
-    return `You entered "${input}", please enter "${SCISSORS}", "${PAPER}" or "${STONE}" to play. Or add "reversed" before your choice (e.g."reversed scissors") for a different challenge.`;
-  }
+  // Set computer and user choices
   var computerShape = getComputerShape();
-  var userShape = getUserShape(input);
-  var result = didUserWin(userShape, computerShape, reversedMode);
+  var userShape = getUserShape(choice);
+
+  // Game play depending on mode selected
+  switch (mode) {
+    case NORMAL:
+      result = playNormalSPS(userShape, computerShape);
+      break;
+    case REVERSED:
+      result = playReversedSPS(userShape, computerShape);
+      break;
+    case KOREAN:
+      result = playKoreanSPS(userShape, computerShape);
+      break;
+    case COMPUTER:
+      userShape = getComputerShape();
+      result = playNormalSPS(userShape, computerShape);
+      break;
+  }
+
+  // Calculate total number of plays
   var totalPlays = Number(userWins) + Number(computerWins) + Number(draws);
 
-  // Reset reversed mode
-  if (reversedMode) {
-    reversedMode = false;
-  }
-
-  return generateOutputMessage(
-    userShape,
-    computerShape,
-    result,
-    userWins,
-    totalPlays
-  );
+  return generateOutputMessage(userShape, computerShape, totalPlays);
 };
 
 // Modules
@@ -81,63 +86,103 @@ function getEmoji(shape) {
   }
 }
 
-// SPS Logic
-function didUserWin(userShape, computerShape, reversedMode) {
-  var difference = mod(Number(computerShape) - Number(userShape), 3);
-  if (difference == 0) {
-    draws += 1;
-    return DRAW;
-  }
-
-  if (reversedMode) {
-    switch (difference) {
-      case 1:
-        computerWins += 1;
-        return LOSE;
-      case 2:
-        userWins += 1;
-        return WIN;
-    }
-  } else {
-    switch (difference) {
-      case 1:
-        userWins += 1;
-        return WIN;
-      case 2:
-        computerWins += 1;
-        return LOSE;
-    }
-  }
-}
-
 // Modulus Function
 function mod(n, m) {
   var remainder = n % m;
   return Math.floor(remainder >= 0 ? remainder : remainder + m);
 }
 
+// Normal SPS Logic
+function playNormalSPS(userShape, computerShape) {
+  var difference = mod(Number(computerShape) - Number(userShape), 3);
+  switch (difference) {
+    case 0:
+      draws += 1;
+      return DRAW;
+    case 1:
+      userWins += 1;
+      return WIN;
+    case 2:
+      computerWins += 1;
+      return LOSE;
+  }
+}
+
+// Reversed SPS Logic
+function playReversedSPS(userShape, computerShape) {
+  var difference = mod(Number(computerShape) - Number(userShape), 3);
+  switch (difference) {
+    case 0:
+      draws += 1;
+      return DRAW;
+    case 1:
+      computerWins += 1;
+      return LOSE;
+    case 2:
+      userWins += 1;
+      return WIN;
+  }
+}
+
+// Korean SPS Logic
+function playSPS(userShape, computerShape) {
+  var difference = mod(Number(computerShape) - Number(userShape), 3);
+  switch (difference) {
+    case 0:
+      return DRAW;
+    case 1:
+      return WIN;
+    case 2:
+      return LOSE;
+  }
+}
+
+function playKoreanSPS(userShape, computerShape) {
+  switch (playSPS(userShape, computerShape)) {
+    case WIN:
+      lastWinner = userName;
+      return KOREAN;
+    case LOSE:
+      lastWinner = COMPUTER;
+      return KOREAN;
+    case DRAW:
+      if (lastWinner == userName) {
+        userWins += 1;
+        lastWinner = NOBODY;
+        return WIN;
+      }
+      if (lastWinner == COMPUTER) {
+        computerWins += 1;
+        lastWinner = NOBODY;
+        return LOSE;
+      } else {
+        return KOREAN;
+      }
+  }
+}
+
 // Output Message Generator
-function generateOutputMessage(
-  userShape,
-  computerShape,
-  result,
-  userWins,
-  totalPlays
-) {
+function generateOutputMessage(userShape, computerShape, totalPlays) {
   var message = `&#x1F476 VS &#x1F916 <br>`;
   message += `${getEmoji(userShape)} VS ${getEmoji(computerShape)}<br>`;
   switch (result) {
     case WIN:
-      message += `You won! Congrats! &#x1F389`;
+      message += `${userName} won! Congrats! &#x1F389`;
       break;
     case LOSE:
-      message += `You lose! Better luck next time! &#x1F340`;
+      message += `${userName} lost! Better luck next time! &#x1F340`;
       break;
-    case "draw":
+    case DRAW:
       message += `It's a draw! What are the odds!? &#x1F914`;
       break;
+    case KOREAN:
+      if (lastWinner == NOBODY) {
+        message += `${NOBODY} has the advantage at the moment, only time will tell.`;
+      } else {
+        message += `${lastWinner} has the advantage and needs a draw in the next round to win!`;
+      }
   }
-  message += `<br>You have won ${userWins}/${totalPlays} turns so far!<br>`;
-  message += `Now you can type ${SCISSORS}, "${PAPER}" or "${STONE}" to play another round!<br>Or add "reversed" before your choice for a different challenge &#x1F648`;
+  message += `<br>${userName}'s score so far:<br>Out of ${totalPlays} rounds, ${userWins} wins, ${computerWins} losses, ${draws} draws.<br>`;
+  message += `Choose a version and shape to play another round!&#x1F648`;
   return message;
 }
