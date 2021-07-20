43287cbd611926b032c769166eb0ddad1cad3b2f
nadinelk <nadinekurnadi@gmail.com>
2021-07-20 18:24:46 +0700

    part 2 of sps project

--- script.js
+++ script.js
@@ -4,72 +4,136 @@ var STONE = "stone";
 var REVERSE_SCISSORS = "reverse scissors";
 var REVERSE_PAPER = "reverse paper";
 var REVERSE_STONE = "reverse stone";
+var numOfUserWins = 0;
+var numOfComputerWins = 0;
+var numOfDraws = 0;
+var numOfRounds = 0;
+var modeAskForUserName = "user types in their name";
+var modeStartPlaying = "sps game starts";
+var gameMode = modeAskForUserName;
+var userName = "";
 
 var programOutput = function () {
-  // scissors = 1
-  // paper = 2
-  // stone = 3
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal);
   var randomNumber = randomInteger + 1;
-
   return randomNumber;
+};
+var computersRandomWord = function () {
+  var computersRandomNumber = programOutput();
+  if (computersRandomNumber == 1) {
+    return SCISSORS;
+  }
+  if (computersRandomNumber == 2) {
+    return PAPER;
+  }
+  if (computersRandomNumber == 3) {
+    return STONE;
+  }
 };
 
 var main = function (input) {
-  var programChoice = programOutput();
-  console.log("this is the random number " + programChoice + "");
-  var myOutputValue =
-    "Whoops, seems like we can't process your input. Please input one of these choices below: scissors, paper, stone.";
+  // when the game starts, game mode is modeAskForUserName
+  if (gameMode == modeAskForUserName) {
+    userName = input;
 
-  // if user gives scissors and the program gives paper, user wins.
-  // if user gives paper and the program gives stone, user wins.
-  // if user gives stone and the program gives scissors, user wins.
-  if (
-    (input == SCISSORS && programChoice == 2) ||
-    (input == PAPER && programChoice == 3) ||
-    (input == STONE && programChoice == 1)
-  ) {
-    myOutputValue = "Congrats! You won. Up for another game?";
-  }
-
-  // if user gives (scissors and the program gives stone, user loses.
-  // if user gives paper and the program gives scissors, user loses.
-  // if user gives stone and the program gives paper, user loses.
-  if (
-    (input == SCISSORS && programChoice == 3) ||
-    (input == PAPER && programChoice == 1) ||
-    (input == STONE && programChoice == 2)
-  ) {
-    myOutputValue = "Oh no, you lost. Wanna try again?";
-  }
-
-  // if user inputs == program choice, it's a draw
-  if (
-    ((input == SCISSORS || input == REVERSE_SCISSORS) && programChoice == 1) ||
-    ((input == PAPER || input == REVERSE_PAPER) && programChoice == 2) ||
-    ((input == STONE || input == REVERSE_STONE) && programChoice == 3)
-  ) {
-    myOutputValue = "It's a draw!";
-  }
-
-  // if user inputs == reversed + (rock, paper, scissors), the result would be the reversed result.
-  if (
-    (input == REVERSE_SCISSORS && programChoice == 3) ||
-    (input == REVERSE_PAPER && programChoice == 1) ||
-    (input == REVERSE_STONE && programChoice == 2)
-  ) {
+    // mode changes to sps game once the username is inputted.
+    gameMode = modeStartPlaying;
+    var myOutputValue = "Hi " + userName + "! Ready to play?";
+  } else if (gameMode == modeStartPlaying) {
+    var programChoice = computersRandomWord();
+    console.log("this is the random word " + programChoice + "");
     myOutputValue =
-      "Congrats! You won in this reversed version. Up for another game?";
-  }
+      "Whoops, seems like we can't process your input " +
+      userName +
+      ". Please input one of these choices below: scissors, paper, stone.";
 
-  if (
-    (input == REVERSE_SCISSORS && programChoice == 2) ||
-    (input == REVERSE_PAPER && programChoice == 3) ||
-    (input == REVERSE_STONE && programChoice == 1)
-  ) {
-    myOutputValue = "Oh no, you lost this reversed version. Try again?";
+    // define sps normal rules
+    if (
+      (input == SCISSORS && programChoice == PAPER) ||
+      (input == PAPER && programChoice == STONE) ||
+      (input == STONE && programChoice == SCISSORS)
+    ) {
+      numOfUserWins = numOfUserWins + 1;
+      myOutputValue =
+        "Congrats " +
+        userName +
+        "! You won. Your current record is " +
+        numOfUserWins +
+        " wins, " +
+        numOfComputerWins +
+        " losses, and " +
+        numOfDraws +
+        " draws. Up for another game?";
+    }
+    if (
+      (input == SCISSORS && programChoice == STONE) ||
+      (input == PAPER && programChoice == SCISSORS) ||
+      (input == STONE && programChoice == PAPER)
+    ) {
+      numOfComputerWins = numOfComputerWins + 1;
+      myOutputValue =
+        "Oh no, you lost " +
+        userName +
+        ". Your current record is " +
+        numOfUserWins +
+        " wins, " +
+        numOfComputerWins +
+        " losses, and " +
+        numOfDraws +
+        " draws. Wanna try again?";
+    }
+    if (
+      input == programChoice ||
+      (input == REVERSE_SCISSORS && programChoice == SCISSORS) ||
+      (input == REVERSE_PAPER && programChoice == PAPER) ||
+      (input == REVERSE_STONE && programChoice == STONE)
+    ) {
+      numOfDraws = numOfDraws + 1;
+      myOutputValue =
+        "It's a draw! Your current record is " +
+        numOfUserWins +
+        " wins, " +
+        numOfComputerWins +
+        " losses, and " +
+        numOfDraws +
+        " draws.";
+    }
+    // if user inputs == reversed + (rock, paper, scissors), the result would be the reversed result.
+    if (
+      (input == REVERSE_SCISSORS && programChoice == STONE) ||
+      (input == REVERSE_PAPER && programChoice == SCISSORS) ||
+      (input == REVERSE_STONE && programChoice == PAPER)
+    ) {
+      numOfUserWins = numOfUserWins + 1;
+      myOutputValue =
+        "Congrats " +
+        userName +
+        "! You won in this reversed version. Your current record is " +
+        numOfUserWins +
+        " wins, " +
+        numOfComputerWins +
+        " losses, and " +
+        numOfDraws +
+        " draws. Up for another game?";
+    }
+    if (
+      (input == REVERSE_SCISSORS && programChoice == PAPER) ||
+      (input == REVERSE_PAPER && programChoice == STONE) ||
+      (input == REVERSE_STONE && programChoice == SCISSORS)
+    ) {
+      numOfComputerWins = numOfComputerWins + 1;
+      myOutputValue =
+        "Oh no, you lost this reversed version " +
+        userName +
+        ". Your current record is " +
+        numOfUserWins +
+        " wins, " +
+        numOfComputerWins +
+        " losses, and " +
+        numOfDraws +
+        " draws. Try again?";
+    }
   }
-
   return myOutputValue;
 };
