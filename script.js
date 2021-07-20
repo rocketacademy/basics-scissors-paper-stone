7dc7cd35685f919d3cb40e7742a0fccf9cee313e
Amelia Wibisono <ameliawibi@gmail.com>
2021-07-20 18:21:03 +0800

    switch game mode

--- script.js
+++ script.js
@@ -10,49 +10,49 @@ const getRandomHand = () => {
 
 // Convert user's text input into symbol for output display
 const iconForNoun = (hand) => {
-  if (
-    hand.toLowerCase() === "paper" ||
-    hand.toLowerCase() === "reversed paper"
-  ) {
+  if (hand.toLowerCase() === "paper") {
     return "🧻";
-  } else if (
-    hand.toLowerCase() === "scissors" ||
-    hand.toLowerCase() === "reversed scissors"
-  ) {
+  } else if (hand.toLowerCase() === "scissors") {
     return "✂️";
-  } else if (
-    hand.toLowerCase() === "rock" ||
-    hand.toLowerCase() === "reversed rock"
-  ) {
+  } else if (hand.toLowerCase() === "rock") {
     return "💎";
   } else return hand.toLowerCase();
 };
 
+// all output messages
+const myOutputValue = [
+  (invalid = (a) =>
+    `${iconForNoun(
+      a
+    )} is an invalid input.\<br>\Please type scissors/paper/rock to continue the game`),
+  (draw = (a, b, c, d) =>
+    `Draw.\<br>\Your ${iconForNoun(a)} vs system's ${iconForNoun(
+      b
+    )}.\<br>\You: ${c}\<br>\System: ${d}`),
+  (win = (a, b, c, d) => `You won.\<br>\Your ${iconForNoun(
+    a
+  )} beats system's ${iconForNoun(b)}.\<br>\
+        You: ${c}\<br>\System: ${d}`),
+  (lose = (a, b, c, d) =>
+    `You lost.\<br>\ System's ${iconForNoun(b)} beats your ${iconForNoun(
+      a
+    )}.\<br>\You: ${c}\<br>\System: ${d}`),
+];
+
 // All invalid cases of user's input
-const invalidCases = (userInput) =>
-  userInput.toLowerCase() !== "paper" &&
-  userInput.toLowerCase() !== "rock" &&
-  userInput.toLowerCase() !== "scissors" &&
-  userInput.toLowerCase() !== "reversed scissors" &&
-  userInput.toLowerCase() !== "reversed paper" &&
-  userInput.toLowerCase() !== "reversed rock";
+const invalidCases = (a) =>
+  a.toLowerCase() !== "paper" &&
+  a.toLowerCase() !== "rock" &&
+  a.toLowerCase() !== "scissors";
 
 // All cases where user wins
-const winCases = (userInput, showMeHand) =>
-  (userInput.toLowerCase() === "paper" && showMeHand === "rock") ||
-  (userInput.toLowerCase() === "rock" && showMeHand === "scissors") ||
-  (userInput.toLowerCase() === "scissors" && showMeHand === "paper") ||
-  (userInput.toLowerCase() === "reversed scissors" && showMeHand === "rock") ||
-  (userInput.toLowerCase() === "reversed paper" && showMeHand === "scissors") ||
-  (userInput.toLowerCase() === "reversed rock" && showMeHand === "paper");
+const winCases = (b, c) =>
+  (b.toLowerCase() === "paper" && c === "rock") ||
+  (b.toLowerCase() === "rock" && c === "scissors") ||
+  (b.toLowerCase() === "scissors" && c === "paper");
 
 // All cases where both parties choose the same object.
-const drawCases = (userInput, showMeHand) =>
-  userInput.toLowerCase() === showMeHand ||
-  (userInput.toLowerCase() === "reversed scissors" &&
-    showMeHand === "scissors") ||
-  (userInput.toLowerCase() === "reversed rock" && showMeHand === "rock") ||
-  (userInput.toLowerCase() === "reversed paper" && showMeHand === "paper");
+const drawCases = (d, e) => d.toLowerCase() === e;
 
 //track scores
 let userWin = 0;
@@ -62,53 +62,89 @@ let systemWin = 0;
 let userName = "";
 let gameMode = "pending for username";
 
-// The game
+// normal mode game play
+
+const normalModeGamePlay = (userInput) => {
+  let showMeHand = getRandomHand();
+
+  if (invalidCases(userInput)) {
+    //If invalid input , return feedback.
+    return myOutputValue[0](userInput);
+  }
+  //If both parties choose the same object, it's a draw.
+  else if (drawCases(userInput, showMeHand)) {
+    return myOutputValue[1](userInput, showMeHand, userWin, systemWin);
+  }
+  // User wins.
+  else if (winCases(userInput, showMeHand)) {
+    //increment userWin score
+    userWin += 1;
+    return myOutputValue[2](userInput, showMeHand, userWin, systemWin);
+  }
+
+  // System wins.
+  else if (!winCases(userInput, showMeHand)) {
+    //increment systemWin score
+    systemWin += 1;
+    return myOutputValue[3](userInput, showMeHand, userWin, systemWin);
+  }
+};
+
+// reverse mode game play
+
+const reverseModeGamePlay = (userInput) => {
+  let showMeHand = getRandomHand();
+
+  if (invalidCases(userInput)) {
+    //If invalid input , return feedback.
+    return myOutputValue[0](userInput);
+  }
+  //If both parties choose the same object, it's a draw.
+  else if (drawCases(userInput, showMeHand)) {
+    return myOutputValue[1](userInput, showMeHand, userWin, systemWin);
+  }
+  // User wins.
+  else if (!winCases(userInput, showMeHand)) {
+    //increment userWin score
+    userWin += 1;
+    return myOutputValue[2](userInput, showMeHand, userWin, systemWin);
+  }
+
+  // System wins.
+  else if (winCases(userInput, showMeHand)) {
+    //increment systemWin score
+    systemWin += 1;
+    return myOutputValue[3](userInput, showMeHand, userWin, systemWin);
+  }
+};
+
+// Main game
 const main = (input) => {
+  // waiting for user to enter username
   if (gameMode == "pending for username") {
     userName = input;
-    gameMode = "dice game";
+    gameMode = "normal";
     return `Welcome ${userName}`;
   }
 
-  if (gameMode == "dice game") {
-    let showMeHand = getRandomHand();
-    let userInput = input;
-    let myOutputValue = "";
+  //user input to switch to reverse game mode
+  if (gameMode == "normal" && input.toLowerCase() == "reverse") {
+    gameMode = input.toLowerCase();
+    return `Welcome to reverse mode`;
+  }
 
-    if (input === userInput)
-      if (invalidCases(userInput)) {
-        //If invalid input , return feedback.
-        return (myOutputValue = `${iconForNoun(
-          userInput
-        )} is an invalid input.\<br>\Please type scissors/paper/rock to continue the game`);
-      }
-      //If both parties choose the same object, it's a draw.
-      else if (drawCases(userInput, showMeHand)) {
-        return (myOutputValue = `Draw.\<br>\Your ${iconForNoun(
-          userInput
-        )} vs system's ${iconForNoun(
-          showMeHand
-        )}.\<br>\You: ${userWin}\<br>\System: ${systemWin}`);
-      }
-      // User wins.
-      else if (winCases(userInput, showMeHand)) {
-        //increment userWin score
-        userWin += 1;
-        return (myOutputValue = `You won.\<br>\Your ${iconForNoun(
-          userInput
-        )} beats system's ${iconForNoun(showMeHand)}.\<br>\
-        You: ${userWin}\<br>\System: ${systemWin}`);
-      }
-
-      // System wins.
-      else if (!winCases(userInput, showMeHand)) {
-        //increment systemWin score
-        systemWin += 1;
-        return (myOutputValue = `You lost.\<br>\ System's ${iconForNoun(
-          showMeHand
-        )} beats your ${iconForNoun(
-          userInput
-        )}.\<br>\You: ${userWin}\<br>\System: ${systemWin}`);
-      }
+  //user input to switch to normal game mode
+  if (gameMode == "reverse" && input.toLowerCase() == "normal") {
+    gameMode = input.toLowerCase();
+    return `Welcome to normal mode`;
+  }
+  // game mode to execute
+  switch (gameMode) {
+    case "normal":
+      let normalGameOutput = normalModeGamePlay(input);
+      return normalGameOutput;
+    case "reverse":
+      let reverseGameOutput = reverseModeGamePlay(input);
+      return reverseGameOutput;
   }
 };
