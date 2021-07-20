fbe0d6e41e200a995076403f1678610c540f82bc
YOUR_GITHUB_Gloriahe <YOUR_GITHUB_mengyagloriahe@gmail.com>
2021-07-20 19:48:31 +0800

    firstproject

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
 var main = function (input) {
-  //assign a random number to computer
-  var randomResult = function () {
-    var randomLuckyNum = Math.floor(Math.random() * 3) + 1;
-    return randomLuckyNum;
-  };
+  //computer choice
+  var comChoice = generateComChoice();
+  console.log(comChoice);
 
-  //assign a choice to computer based on ramdomResult to pick among scissors paper stone
-  var computerResult = function () {
-    var randomComputerResult = randomResult();
-    if (randomComputerResult == 1) {
-      var myOutputValue = "scissors";
-      return myOutputValue;
-    }
+  //draw
+  if (input == comChoice) {
+    return "The computer chose ${comChoice} and you chose ${input}. It is a draw. Play again!";
+  }
 
-    if (randomComputerResult == 2) {
-      var myOutputValue = "stone";
-      return myOutputValue;
-    }
+  //winning
+  else if (
+    (input == "scissors" && comChoice == "paper") ||
+    (input == "paper" && comChoice == "stone") ||
+    (input == "stone" && comChoice == "scissors")
+  ) {
+    return "The computer chose ${comChoice} and you chose ${input}. You won! Congrats!";
+  }
 
-    if (randomComputerResult == 3) {
-      var myOutputValue = "paper";
-      return myOutputValue;
-    }
-
-    //play time
-    var gameTime = function () {
-      var randomComChoice = computerResult();
-      var genericOutput =
-        "The computer chose $(randomComChoice) <br>. You chose $(input) <br>.";
-      //draw
-      if (randomComChoice == input) {
-        return "${genericOutput} It is a draw. <br> Now play again!";
-      }
-      //winning
-      if (
-        (randomComChoice == "scissors" && input == "stone") ||
-        (randomComChoice == "stone" && input == "paper") ||
-        (randomComChoice == "paper" && input == "scissors")
-      ) {
-        return "${genericOutput} You win! Congrats. <br> Now play again!";
-      }
-      //losing
-      var myOutputValue = "${genericOutput} Sorry you lost. <br> Try again!";
-      return myOutputValue;
-    };
-  };
-
+  //lost
+  else if (
+    (input == "scissors" && comChoice == "stone") ||
+    (input == "paper" && comChoice == "scissors") ||
+    (input == "stone" && comChoice == "paper")
+  ) {
+    return "The computer chose ${comChoice} and you chose ${input}. You lost! Try again!";
+  }
   //input validation
-  if (input != "scissors" || input != "paper" || input != "stone") {
-    return "You entered ${input}, please only enter scissors, paper or stone and try again.";
+  return "Your input ${input} is invalid. Please only enter scissors, paper, or stone.";
+};
+
+//generate a random number between 0 and 3
+var generateNum = function () {
+  var randomDecimal = Math.random() * 3;
+
+  //convert to integer
+  var randomInteger = Math.floor(randomDecimal) + 1;
+  return randomInteger;
+};
+
+//assign an element to a value between 0 and 3
+var generateComChoice = function () {
+  var numComChoice = 3;
+  var randomComChoice = generateNum(numComChoice);
+
+  if (randomComChoice == 1) {
+    return "scissors";
+  }
+
+  if (randomComChoice == 2) {
+    return "paper";
+  }
+
+  if (randomComChoice == 3) {
+    return "stone";
   }
 };
