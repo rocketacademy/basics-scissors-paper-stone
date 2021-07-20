fd4bcc841a41e42f779b48938608cfa80d60875e
wsantoso5 <winsensantoso@gmail.com>
2021-07-20 10:11:38 +0700

    9.10 winsen

--- script.js
+++ script.js
@@ -12,127 +12,131 @@ var main = function (input) {
     return (myOutputValue =
       " hi " + user_name + "<br>" + "it's " + current_mode);
   } else if (current_mode == "gametime") {
-    // Generate a random dice number
-    var randomDiceNumber = rollDice();
-
-    var stone = 1;
-    var scissors = 2;
-    var paper = 3;
-    if (input != "stone" || input != "scissors" || input != "paper") {
-      myOutputValue =
-        "Try again, <br> available input are: scissors , paper or stone";
-    }
-
-    if (input == "stone" && randomDiceNumber == 1) {
-      user_draw = user_draw + 1;
-      myOutputValue =
-        "The computer also chose stone <br> it's a draw <br> Now you can type scissors paper or stone to play another round!" +
-        "<br>" +
-        " so far you draw " +
-        user_draw +
-        " times";
-    }
-    if (input == "scissors" && randomDiceNumber == 2) {
-      user_draw = user_draw + 1;
-      myOutputValue =
-        "The computer also chose scissors <br> it's a draw <br> Now you can type scissors paper or stone to play another round!" +
-        "<br>" +
-        " so far you draw " +
-        user_draw +
-        " times";
-    }
-    if (input == "paper" && randomDiceNumber == 3) {
-      user_draw = user_draw + 1;
-      myOutputValue =
-        "The computer also chose paper <br> it's a draw <br> Now you can type scissors paper or stone to play another round!" +
-        "<br>" +
-        " so far you draw " +
-        user_draw +
-        " times";
-    }
-
-    // comparing input and random.
-    if (input == "stone" && randomDiceNumber == scissors) {
-      user_win = user_win + 1;
-      myOutputValue =
-        "The computer chose scissors <br> you win <br> Now you can type scissors paper or stone to play another round!" +
-        "<br>" +
-        " so far " +
-        user_name +
-        " " +
-        user_win +
-        " Computer " +
-        user_lose;
-    }
-
-    if (input == "stone" && randomDiceNumber == paper) {
-      user_lose = user_lose + 1;
-      yOutputValue =
-        "The computer chose paper <br>you lose <br> Now you can type scissors paper or stone to play another round!" +
-        "<br>" +
-        " so far " +
-        user_name +
-        " " +
-        user_win +
-        " Computer " +
-        user_lose;
-    }
-
-    if (input == "paper" && randomDiceNumber == stone) {
-      user_win = user_win + 1;
-      myOutputValue =
-        "The computer chose stone <br> you win <br> Now you can type scissors paper or stone to play another round!" +
-        "<br>" +
-        " so far " +
-        user_name +
-        " " +
-        user_win +
-        " Computer " +
-        user_lose;
-    }
-
-    if (input == "paper" && randomDiceNumber == scissors) {
-      user_lose = user_lose + 1;
-      myOutputValue =
-        "The computer chose scissors <br>you lose <br> Now you can type scissors paper or stone to play another round!" +
-        "<br>" +
-        " so far " +
-        user_name +
-        " " +
-        user_win +
-        " Computer " +
-        user_lose;
-    }
-
-    if (input == "scissors" && randomDiceNumber == paper) {
-      user_win = user_win + 1;
-      myOutputValue =
-        "The computer chose paper <br>you win <br> Now you can type scissors paper or stone to play another round!" +
-        "<br>" +
-        " so far " +
-        user_name +
-        " " +
-        user_win +
-        " Computer " +
-        user_lose;
-    }
-
-    if (input == "scissors" && randomDiceNumber == stone) {
-      user_lose = user_lose + 1;
-      myOutputValue =
-        "The computer chose stone <br>you lose <br> Now you can type scissors paper or stone to play another round!" +
-        "<br>" +
-        " so far " +
-        user_name +
-        " " +
-        user_win +
-        " Computer " +
-        user_lose;
-    }
-    // Return output.
-    //
-    return myOutputValue;
+    return (myOutputValue = playgame(user_name, input));
   }
+};
+
+var playgame = function (user_name, input) {
+  // Generate a random dice number
+  var randomDiceNumber = rollDice();
+
+  var stone = 1;
+  var scissors = 2;
+  var paper = 3;
+  if (input != "stone" || input != "scissors" || input != "paper") {
+    myOutputValue =
+      "Try again, <br> available input are: scissors , paper or stone";
+  }
+
+  if (input == "stone" && randomDiceNumber == 1) {
+    user_draw = user_draw + 1;
+    myOutputValue =
+      "The computer also chose stone <br> it's a draw <br> Now you can type scissors paper or stone to play another round!" +
+      "<br>" +
+      " so far you draw " +
+      user_draw +
+      " times";
+  }
+  if (input == "scissors" && randomDiceNumber == 2) {
+    user_draw = user_draw + 1;
+    myOutputValue =
+      "The computer also chose scissors <br> it's a draw <br> Now you can type scissors paper or stone to play another round!" +
+      "<br>" +
+      " so far you draw " +
+      user_draw +
+      " times";
+  }
+  if (input == "paper" && randomDiceNumber == 3) {
+    user_draw = user_draw + 1;
+    myOutputValue =
+      "The computer also chose paper <br> it's a draw <br> Now you can type scissors paper or stone to play another round!" +
+      "<br>" +
+      " so far you draw " +
+      user_draw +
+      " times";
+  }
+
+  // comparing input and random.
+  if (input == "stone" && randomDiceNumber == scissors) {
+    user_win = user_win + 1;
+    myOutputValue =
+      "The computer chose scissors <br> you win <br> Now you can type scissors paper or stone to play another round!" +
+      "<br>" +
+      " so far " +
+      user_name +
+      " " +
+      user_win +
+      " Computer " +
+      user_lose;
+  }
+
+  if (input == "stone" && randomDiceNumber == paper) {
+    user_lose = user_lose + 1;
+    yOutputValue =
+      "The computer chose paper <br>you lose <br> Now you can type scissors paper or stone to play another round!" +
+      "<br>" +
+      " so far " +
+      user_name +
+      " " +
+      user_win +
+      " Computer " +
+      user_lose;
+  }
+
+  if (input == "paper" && randomDiceNumber == stone) {
+    user_win = user_win + 1;
+    myOutputValue =
+      "The computer chose stone <br> you win <br> Now you can type scissors paper or stone to play another round!" +
+      "<br>" +
+      " so far " +
+      user_name +
+      " " +
+      user_win +
+      " Computer " +
+      user_lose;
+  }
+
+  if (input == "paper" && randomDiceNumber == scissors) {
+    user_lose = user_lose + 1;
+    myOutputValue =
+      "The computer chose scissors <br>you lose <br> Now you can type scissors paper or stone to play another round!" +
+      "<br>" +
+      " so far " +
+      user_name +
+      " " +
+      user_win +
+      " Computer " +
+      user_lose;
+  }
+
+  if (input == "scissors" && randomDiceNumber == paper) {
+    user_win = user_win + 1;
+    myOutputValue =
+      "The computer chose paper <br>you win <br> Now you can type scissors paper or stone to play another round!" +
+      "<br>" +
+      " so far " +
+      user_name +
+      " " +
+      user_win +
+      " Computer " +
+      user_lose;
+  }
+
+  if (input == "scissors" && randomDiceNumber == stone) {
+    user_lose = user_lose + 1;
+    myOutputValue =
+      "The computer chose stone <br>you lose <br> Now you can type scissors paper or stone to play another round!" +
+      "<br>" +
+      " so far " +
+      user_name +
+      " " +
+      user_win +
+      " Computer " +
+      user_lose;
+  }
+  // Return output.
+  //
+  return myOutputValue;
 };
 
 var rollDice = function () {
