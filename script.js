a6be7e49b0e0fc25bc6488a92b4880249069cef8
arnolddhu <arnold.hu93@gmail.com>
2021-07-21 23:52:48 +0800

    Part 2

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//win loss record
+var numOfWin = 0;
+var numOfLose = 0;
+var numOfDraw = 0;
+var winPercentage = 0;
+
+//User name
+var currentGameMode = "waiting for username";
+var userName = "";
+
 // Generate a random integer from 1 to max inclusive
 var generateRandomInteger = function (max) {
   var randomDecimal = Math.random() * max;
@@ -20,29 +30,43 @@ var generateSPS = function () {
 };
 //the game
 var main = function (input) {
-  var randomSPS = generateSPS();
-  var genericOutput = `Your play: ${input}. Program's play: ${randomSPS}.`;
+  var myOutputValue = "";
+  if (currentGameMode == "waiting for username") {
+    //set the name
+    userName = input;
+    currentGameMode = "ScissorsPaperStone";
+    return (myOutputValue =
+      "Welcome " +
+      userName +
+      "! Time for you to make a move! Scissors, Paper or Stone?");
+  } else if (currentGameMode == "ScissorsPaperStone") {
+    var winPercentage = 100 * (numOfWin / (numOfWin + numOfLose + numOfDraw));
+    var genericOutput = `${userName}'s play: ${input}. Program's play: ${randomSPS}.`;
+    var randomSPS = generateSPS();
+  }
+  if (
+    (input == "scissors" && randomSPS == "paper") ||
+    (input == "paper" && randomSPS == "stone") ||
+    (input == "stone" && randomSPS == "scissors")
+  ) {
+    numOfWin = numOfWin + 1;
+    return `${genericOutput} You win! <br> Your Win:Lose:Draw Ratio is ${numOfWin}: ${numOfLose} :${numOfDraw}. <br> Your winning percentage is ${winPercentage}% `;
+  }
+
   if (input == randomSPS) {
-    return `${genericOutput} You draw`;
+    numOfDraw = numOfDraw + 1;
+    return `${genericOutput} You draw! <br> Your Win:Lose:Draw Ratio is ${numOfWin}: ${numOfLose} :${numOfDraw}. <br> Your winning percentage is ${winPercentage}%`;
   }
-  if (input == "scissors" && randomSPS == "paper") {
-    return `${genericOutput} You win`;
+  if (
+    (input == "scissors" && randomSPS == "stone") ||
+    (input == "paper" && randomSPS == "scissors") ||
+    (input == "stone" && randomSPS == "paper")
+  ) {
+    numOfLose = numOfLose + 1;
+    return `${genericOutput} You lose! <br> Your Win:Lose:Draw Ratio is ${numOfWin}: ${numOfLose} :${numOfDraw}. <br> Your winning percentage is ${winPercentage}% `;
   }
-  if (input == "scissors" && randomSPS == "stone") {
-    return `${genericOutput} You lose`;
-  }
-  if (input == "paper" && randomSPS == "stone") {
-    return `${genericOutput} You win`;
-  }
-  if (input == "paper" && randomSPS == "scissors") {
-    return `${genericOutput} You lose`;
-  }
-  if (input == "stone" && randomSPS == "paper") {
-    return `${genericOutput} You lose`;
-  }
-  if (input == "stone" && randomSPS == "scissors") {
-    return `${genericOutput} You win`;
-  }
-  if (!(input == "scissors" || input == "paper" || input == "stone"))
+
+  if (!(input == "scissors" || input == "paper" || input == "stone")) {
     return "Invalid input, please choose either scissors, paper or stone.";
+  }
 };
