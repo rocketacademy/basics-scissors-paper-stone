029e08db39c7875ce2217003fea017347bd230e0
IanSeow <ian_seow@hotmail.com>
2021-07-22 02:23:07 +0800

    added game mode selection for normal, reverse, and korean SPS

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Create a basic version of Scissors Paper Stone where the user inputs one of "scissors", "paper", or "stone", the program internally randomly chooses scissors, paper, or stone, and the program outputs whether the user won, the program won, or it's a draw. Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
 
+// -------------------------------------------------------------------
+// -------------------- GLOBAL VARIABLES -----------------------------
+// -------------------------------------------------------------------
+
 // Define 'scissors', 'paper', and 'stone' as variables
 var scissors = "scissors";
 var paper = "paper";
@@ -9,6 +13,18 @@ var stone = "stone";
 var winCount = 0;
 var loseCount = 0;
 var drawCount = 0;
+
+// Add default variable for current mode, user Name, and game mode
+var currentMode = "waiting for username";
+var UserName = "";
+var gameMode = "";
+
+// Add default variable for most recent winner (for Korean mode SPS)
+var mostRecentWinner = "";
+
+// -------------------------------------------------------------------
+// ------------------- HELPER FUNCTIONS ------------------------------
+// -------------------------------------------------------------------
 
 // Create Random Scissors Paper Stone result
 var getSpsResult = function () {
@@ -45,9 +61,10 @@ var getSpsResult = function () {
 var WinLoseHistory = function () {
   return "wins:" + winCount + " lose:" + loseCount + " draw: " + drawCount;
 };
-// Add current game mode, and variable for User Name
-var currentGameMode = "waiting for user name";
-var UserName = "";
+
+// -------------------------------------------------------------------
+// ----------------------- MAIN FUNCTION -----------------------------
+// -------------------------------------------------------------------
 
 var main = function (input) {
   // Generate and define random SPS
@@ -57,19 +74,39 @@ var main = function (input) {
 
   // Get user to input username if not already done so
 
-  if (currentGameMode == "waiting for user name") {
+  if (currentMode == "waiting for username") {
     UserName = input;
-    // Switch game mode to 'dice game'
-    currentGameMode = "dice game";
-    var myOutputValue = "Hello " + UserName;
-    //return "Hello " + UserName;
-  } else if (currentGameMode == "dice game") {
+    // Switch current mode to 'mode selection'
+    currentMode = "mode selection";
+    return (
+      "Hello " +
+      UserName +
+      "!" +
+      " Please select a mode by typing in one of the following: <br> 1. normal <br> 2. reverse <br> 3. korean"
+    );
+  } else if (currentMode == "mode selection") {
+    if (input != "normal" && input != "reverse" && input != "korean") {
+      myOutputValue =
+        'Kindly enter "normal", "reverse", or "korean" to select a game mode';
+    }
+    if (input == "normal" || input == "reverse" || input == "korean") {
+      // Switch current mode to "dice game"
+      currentMode = "dice game";
+      // Switch game mode to user's choice
+      gameMode = input;
+    }
+  }
+  if (gameMode == "normal") {
+    // NORMAL SPS RULES APPLY
+
+    // Define error message
     if (input != scissors && input != paper && input != stone) {
       console.log("invalid input");
       myOutputValue =
-        'Invalid input! <br> Kindly enter "scissors", "paper", or "stone" <br>' +
-        WinLoseHistory();
-    } // Define winning message (win)
+        'Kindly enter "scissors", "paper", or "stone" <br>' + WinLoseHistory();
+    }
+
+    // Define winning message (win)
     if (
       (input == scissors && sps == paper) ||
       (input == paper && sps == stone) ||
@@ -119,7 +156,151 @@ var main = function (input) {
         "! <br>" +
         WinLoseHistory();
     }
+  } else if (gameMode == "reverse") {
+    // REVERSE SPS RULES APPLY
+
     // Define error message
+    if (input != scissors && input != paper && input != stone) {
+      console.log("invalid input");
+      myOutputValue =
+        'Kindly enter "scissors", "paper", or "stone" <br>' + WinLoseHistory();
+    }
+
+    // Define winning message (win)
+    if (
+      (input == scissors && sps == paper) ||
+      (input == stone && sps == paper) ||
+      (input == scissors && sps == stone)
+    ) {
+      console.log("win");
+      winCount = winCount + 1;
+      myOutputValue =
+        "You win! <br> You input " +
+        input +
+        " and your opponent chose " +
+        sps +
+        "! <br>" +
+        WinLoseHistory();
+    }
+
+    // Define losing message (lose)
+    if (
+      (input == scissors && sps == paper) ||
+      (input == paper && sps == stone) ||
+      (input == stone && sps == scissors)
+    ) {
+      console.log("lose");
+      loseCount = loseCount + 1;
+      myOutputValue =
+        "You lose! <br>  You input " +
+        input +
+        " and your opponent chose " +
+        sps +
+        "! <br>" +
+        WinLoseHistory();
+    }
+
+    // Define draw message (draw)
+    if (
+      (input == scissors && sps == scissors) ||
+      (input == paper && sps == paper) ||
+      (input == stone && sps == stone)
+    ) {
+      console.log("draw");
+      drawCount = drawCount + 1;
+      myOutputValue =
+        "Its a draw! <br> You input " +
+        input +
+        " and your opponent chose " +
+        sps +
+        "! <br>" +
+        WinLoseHistory();
+    }
+  } else if (gameMode == "korean") {
+    // KOREAN RULES APPLY: Update the program state such that it can keep track of the most recent winner, and update the program logic to declare the most recent winner the ultimate winner when there is a draw
+
+    // Define error message
+    if (input != scissors && input != paper && input != stone) {
+      console.log("invalid input");
+      myOutputValue =
+        'Kindly enter "scissors", "paper", or "stone" <br>' + WinLoseHistory();
+    }
+    // Define draw what happens when a draw occurs
+    if (
+      (input == scissors && sps == scissors) ||
+      (input == paper && sps == paper) ||
+      (input == stone && sps == stone)
+    ) {
+      // Check if there was a most recent winner, if none, show first-round draw message
+      if (mostRecentWinner == "") {
+        console.log("first round draw");
+        drawCount = drawCount + 1;
+        myOutputValue =
+          "Its a first-round draw! <br> You input " +
+          input +
+          " and your opponent chose " +
+          sps +
+          "! <br>" +
+          WinLoseHistory();
+        // If most recent winner was "player", define winning message
+      }
+      if (mostRecentWinner == "player") {
+        console.log("final win");
+        winCount = winCount + 1;
+        myOutputValue =
+          "You win! <br> You were the most recent winner, and its a now a draw! <br> You input " +
+          input +
+          " and your opponent chose " +
+          sps +
+          "! <br>" +
+          WinLoseHistory();
+        // If most recent winner was "computer", define losing message
+      }
+      if (mostRecentWinner == "computer") {
+        console.log("lose");
+        loseCount = loseCount + 1;
+        myOutputValue =
+          "You lose! <br> The comupter was the most recent winner, and its a now a draw! <br> You input " +
+          input +
+          " and your opponent chose " +
+          sps +
+          "! <br>" +
+          WinLoseHistory();
+      }
+    }
+    // Define most recent win (win)
+    if (
+      (input == scissors && sps == paper) ||
+      (input == stone && sps == scissors) ||
+      (input == paper && sps == stone)
+    ) {
+      console.log("win");
+      mostRecentWinner = "player";
+      myOutputValue =
+        "You win this match! <br> You are currently the most recent winner! <br> Get a draw to REALLY win! <br> You input " +
+        input +
+        " and your opponent chose " +
+        sps +
+        "! <br>" +
+        WinLoseHistory();
+    }
+
+    // Define most recent win (lose)
+    if (
+      (input == scissors && sps == paper) ||
+      (input == paper && sps == scissors) ||
+      (input == stone && sps == paper)
+    ) {
+      console.log("lose");
+      mostRecentWinner = "computer";
+      myOutputValue =
+        "You lost this match! <br> Your opponent is currently the most recent winner! <br> Win the next match to gain back lead! <br> You input " +
+        input +
+        " and your opponent chose " +
+        sps +
+        "! <br>" +
+        WinLoseHistory();
+    }
   }
   return myOutputValue;
 };
