1beefe041180b5998f1b090e6e79598162d5ed39
Hikaruimouto <nicole_thf93@hotmail.com>
2021-07-21 18:51:08 +0800

    added Part 2

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //Create a basic version of Scissors Paper Stone where the user inputs one of "scissors", "paper", or "stone", the program internally randomly chooses scissors, paper, or stone, and the program outputs whether the user won, the program won, or it's a draw.
 
+// [Part 02] Add state to your program such that it keeps track of the number of times the user has won and the number of times the computer has won. Output this win-loss record in a format you like in the program output. You can also output the number of draws and/or each party's winning percentage if you'd like.
+
+//Global variables
+var comScore = 0;
+var userScore = 0;
+var totalPlays = 0;
+
 // Randomise what the computer chooses from "Scissors, Paper + Stone".
 var randomAction = function () {
   // Generate a number from 0 through 3, inclusive of 0 and exclusive of 3
@@ -22,6 +29,10 @@ var main = function (input) {
   var userChoose = input;
   console.log("user chooses " + userChoose);
   var myOutputValue = "Please input something"; //If it doesn't match with anything on top
+  if (input == "score check") {
+    myOutputValue =
+      " You have won " + userScore + " out of " + totalPlays + " times";
+  }
   if (userChoose == aiChoose) {
     myOutputValue = "It's a draw.";
   }
@@ -31,13 +42,19 @@ var main = function (input) {
     (userChoose == "stone" && aiChoose == "paper")
   ) {
     myOutputValue = "You have lost";
+    totalPlays = totalPlays + 1;
+    comScore = comScore + 1;
+    console.log(comScore);
   }
   if (
     (userChoose == "stone" && aiChoose == "scissors") || // User wins Computer
     (userChoose == "scissors" && aiChoose == "paper") ||
     (userChoose == "paper" && aiChoose == "stone")
   ) {
-    myOutputValue = "You have won!";
+    totalPlays = totalPlays + 1;
+    userScore = userScore + 1;
+    console.log(userScore);
+    myOutputValue = "You have won! You have won " + userScore + " times";
   }
 
   return myOutputValue;
