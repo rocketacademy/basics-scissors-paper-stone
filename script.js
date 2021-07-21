978a15666ae02cd1df8a1ea5f6af1f8b2a0a9a17
MichaelCK1 <kurnadimichael@gmail.com>
2021-07-21 16:12:34 +0700

    added more var

--- script.js
+++ script.js
@@ -1,39 +1,67 @@
-var sciccors = "scissors"
-var rock = "rock"
-var paper = "paper"
-
-
-var main = function (input) {
-  var myOutputValue = "hi, your move doesn't seem to be valid, please try again with rock, paper, or scissors";
-  return myOutputValue;
-
-  // if(!input == "rock" && !input == "paper" && !input == "scissors"){
-  //   myOutputValue = "hi, your move doesn't seem to be valid, please try again with rock, paper, or scissors"
-  // }
-
-if(input == systemMove){
-  myOutputValue = "oops! you tied"
-  } else if((input == "scissors" && systemMove == "paper") || (input == "paper" && systemMove == "rock") || (input == "rock" && systemMove == "scissors")){
-    myOutputValue = "congratulations! you won!";
-  } else {
-    myOutputValue = "unlucky! you lost"
-  };
-  return myOutputValue;
-};  
-
+var SCISSORS = "scissors";
+var PAPER = "paper";
+var STONE = "stone";
+var numOfWins = 0;
+var numOfLost = 0;
+var playerName = "";
+var gameMode = "please enter player's name";
 
 var RPS = function(){
-  var randomHand = Math.random() * 3;
-  var randomInteger = Math.floor(randomHand);
-  var systemMove = randomInteger + 1;
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal);
+  var randomNumber = randomInteger + 1;
+  
+  return randomNumber;
+};
 
-var systemMove = "";
-  if(randomHand == 1){
-    systemMove = "rock";
-  } else if (randomHand == 2){
-    systemMove = "scissors";
-  } else if (randomHand == 3){
-    systemMove = "paper";
+var startGame = function(){
+  var hands = RPS();
+  if (hands == 1) {
+    return SCISSORS;
   }
-  myOutputValue = "System picks" + systemMove;
-}
+  if (hands == 2) {
+    return PAPER;
+  }
+  if (hands == 3) {
+    return STONE;
+  }
+};
+
+var main = function(input){
+  if(gameMode == "please enter player's name"){
+    playerName = input;
+    gameMode = "rock paper scissors game";
+    var myOutputValue = "hello! "+input+"! welcome to the game";
+    
+  }
+  else if(gameMode == "rock paper scissors game"){
+  var NPC = startGame();
+  var myOutputValue = "hello! your input does not seem to be valid, please try again with either scissors, paper, or stone";
+  }
+  
+   
+
+  if (
+    (input == SCISSORS && NPC == PAPER) ||
+    (input == PAPER && NPC == STONE) ||
+    (input == STONE && NPC == SCISSORS)
+  )  {
+      numOfWins = numOfWins + 1
+      myOutputValue = "You won! you chose "+input+" and the NPC chose "+NPC+" Your current record is " +numOfWins+" wins, " +numOfLost +" losses";
+    }
+ 
+    if (
+      (input == SCISSORS && NPC == STONE) ||
+      (input == PAPER && NPC == SCISSORS) ||
+      (input == STONE && NPC == PAPER)
+    ) {
+      numOfLost = numOfLost + 1
+      myOutputValue = "You Lost! you chose "+input+" and the NPC chose "+NPC+" Your current record is " +numOfWins+" wins, " +numOfLost +" losses";
+    }
+    if (
+      input == NPC
+    )  {
+      myOutputValue = "Draw! you chose "+input+" and the NPC chose "+NPC+" Your current record is " +numOfWins+" wins, " +numOfLost +" losses";
+    }
+    return myOutputValue;
+  }
