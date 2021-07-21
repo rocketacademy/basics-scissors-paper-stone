8fe7b84c5f5b0d6f4d9ea258bd5604f7a77aa663
YOUR_GITHUB_Gloriahe <YOUR_GITHUB_mengyagloriahe@gmail.com>
2021-07-21 20:58:10 +0800

    firstproject

--- script.js
+++ script.js
@@ -9,37 +9,35 @@ var numPlayerWins = 0;
 var numComputerWins = 0;
 var numDraws = 0;
 
+//game mode normal
+var mode = "normal";
+
 var main = function (input) {
   //computer choice
   var comChoice = generateComChoice();
   console.log(comChoice);
+  console.log(input);
 
-  //draw
-  if (input == comChoice) {
-    return "The computer chose ${comChoice} and you chose ${input}. It is a draw. Play again!";
+  console.log(userName);
+  if (userName == "") {
+    userName = input;
+    return `${userName}, let's play!`;
+  }
+  console.log(userName);
+
+  if (input == `reversePlay`) {
+    mode = `reverse`;
+    return "reverse mode activated";
+  } else if (input == `normalPlay`) {
+    mode = `normal`;
+    return "back to normal mode";
   }
 
-  //winning
-  else if (
-    (input == "scissors" && comChoice == "paper") ||
-    (input == "paper" && comChoice == "stone") ||
-    (input == "stone" && comChoice == "scissors")
-  ) {
-    return "The computer chose ${comChoice} and you chose ${input}. You won! Congrats!";
+  if (mode == `reverse`) {
+    return reverseGame(input, comChoice);
   }
 
-  //lost
-  else if (
-    (input == "scissors" && comChoice == "stone") ||
-    (input == "paper" && comChoice == "scissors") ||
-    (input == "stone" && comChoice == "paper")
-  ) {
-    return "The computer chose ${comChoice} and you chose ${input}. You lost! Try again!";
-  }
-  //input validation
-  else if (input != "scissors" || input != "paper" || input != "stone") {
-    return "Your input ${input} is invalid. Please only enter scissors, paper, or stone.";
-  }
+  return normalGame(input, comChoice);
 };
 
 //generate a random number between 0 and 3
@@ -66,5 +64,62 @@ var generateComChoice = function () {
 
   if (randomComChoice == 3) {
     return "stone";
+  }
+};
+//scissors beat stone, stone beats paper, paper beats scissors
+var reverseGame = function (input, comChoice) {
+  //draw stays the same
+  if (input == comChoice) {
+    return `The computer chose ${comChoice} and you chose ${input}. It is a draw. Play again!`;
+  }
+
+  //lost
+  else if (
+    (input == "scissors" && comChoice == "paper") ||
+    (input == "paper" && comChoice == "stone") ||
+    (input == "stone" && comChoice == "scissors")
+  ) {
+    return `The computer chose ${comChoice} and you chose ${input}. You lost! Try again!`;
+  }
+
+  //Winning
+  else if (
+    (input == "scissors" && comChoice == "stone") ||
+    (input == "paper" && comChoice == "scissors") ||
+    (input == "stone" && comChoice == "paper")
+  ) {
+    return `The computer chose ${comChoice} and you chose ${input}. You won! Congrats!`;
+  }
+  //input validation
+  else if (input != "scissors" || input != "paper" || input != "stone") {
+    return `Your input ${input} is invalid. Please only enter scissors, paper, or stone.`;
+  }
+};
+var normalGame = function (input, comChoice) {
+  //draw
+  if (input == comChoice) {
+    return `The computer chose ${comChoice} and you chose ${input}. It is a draw. Play again!`;
+  }
+
+  //winning
+  else if (
+    (input == "scissors" && comChoice == "paper") ||
+    (input == "paper" && comChoice == "stone") ||
+    (input == "stone" && comChoice == "scissors")
+  ) {
+    return `The computer chose ${comChoice} and you chose ${input}. You won! Congrats!`;
+  }
+
+  //lost
+  else if (
+    (input == "scissors" && comChoice == "stone") ||
+    (input == "paper" && comChoice == "scissors") ||
+    (input == "stone" && comChoice == "paper")
+  ) {
+    return `The computer chose ${comChoice} and you chose ${input}. You lost! Try again!`;
+  }
+  //input validation
+  else if (input != "scissors" || input != "paper" || input != "stone") {
+    return `Your input ${input} is invalid. Please only enter scissors, paper, or stone.`;
   }
 };
