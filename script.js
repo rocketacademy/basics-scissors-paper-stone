88d6a83a461ffb2dae966e23c013b7bbd412e682
Lee Xiao Hui <leexiaohui94@gmail.com>
2021-07-22 01:03:04 +0800

    added win count & reverse game mode

--- script.js
+++ script.js
@@ -1,25 +1,77 @@
 var main = function (input) {
+   
   var computerChoice = assignComputerChoice(getComputerChoice());
+
+  if(currentGameMode == 'waiting for user name'){
+    userName = input;
+    currentGameMode = 'Waiting for game mode...';
   
-  if(input != "Scissors" || "Paper" || "Stone"){
-    myOutputValue = "Please type 'Scissors', 'Paper' or 'Stone' to play the game."
+    return 'Hello '+ userName+ "! Please enter 'normal' or 'reverse' to select the game mode.";   
   }
-  if(input == computerChoice){
-    myOutputValue = "Draw";
+
+  if(currentGameMode == 'Waiting for game mode...'){
+    gameMode = input;
+    currentGameMode = 'Ready to start';
+  }
+
+  if(currentGameMode == "Ready to start"){
+
+    if(gameMode == "normal"){
+    
+    if(input != "scissors" || "paper" || "stone"){
+      myOutputValue = "Please type 'scissors', 'paper' or 'stone' to play the game."
     }
-  if(computerChoice == winningMapping[input]){
-    myOutputValue = "You Win";
+    if(input == computerChoice){
+      myOutputValue = "You have picked "+input+". Computer has picked "+computerChoice+". Result is a draw. Win count: "+wincount;
+      }
+    if(computerChoice == winningMapping[input]){
+      wincount += 1
+      myOutputValue = "You have picked "+input+". Computer has picked "+computerChoice+". You won! Win count: "+wincount;
+      }
+    if(computerChoice == losingMapping[input]){
+      myOutputValue = "You have picked "+input+". Computer has picked "+computerChoice+". You lost! Win count: "+wincount;
+      }
+  
+    return myOutputValue;
+    };
+  
+  
+    if(gameMode == "reverse"){
+      
+    if(input != "scissors" || "paper" || "stone"){
+      myOutputValue = "Please type 'scissors', 'paper' or 'stone' to play the game."
     }
-  if(computerChoice == losingMapping[input]){
-    myOutputValue = "You Lose";
-    }
- 
-  return myOutputValue;
+    if(input == computerChoice){
+      myOutputValue = "You have picked "+input+". Computer has picked "+computerChoice+". Result is a draw. Win count: "+wincount;
+      }
+    if(computerChoice == winningMappingReverse[input]){
+      wincount += 1
+      myOutputValue = "You have picked "+input+". Computer has picked "+computerChoice+". You won! Win count: "+wincount;
+      }
+    if(computerChoice == losingMappingReverse[input]){
+      myOutputValue = "You have picked "+input+". Computer has picked "+computerChoice+". You lost! Win count: "+wincount;
+      }
+  
+    return myOutputValue;
+    };
+  };
 };
 
-//convert computer's choice to an actual string using array - based on winning and losing game outcome
-var winningMapping = {Stone: "Scissors", Paper: "Stone", Scissors: "Paper"};
-var losingMapping = {Stone: "Paper", Paper: "Scissors", Scissors: "Stone"};
+// global state
+var wincount = 0;
+
+//set up the gamemode
+currentGameMode = 'waiting for user name'
+userName = ""
+gameMode = ""
+
+//convert user's choice using array - based on winning and losing game outcome
+var winningMapping = {stone: "scissors", paper: "stone", scissors: "paper"};
+var losingMapping = {stone: "paper", paper: "scissors", scissors: "stone"};
+
+//[for reverse mode] convert user's choice using array - based on winning and losing game outcome
+var losingMappingReverse = {stone: "scissors", paper: "stone", scissors: "paper"};
+var winningMappingReverse = {stone: "paper", paper: "scissors", scissors: "stone"};
 
 //generate random number from one to three
 var getComputerChoice = function () {
@@ -29,16 +81,16 @@ var getComputerChoice = function () {
   return randomInteger;
 };
   
-//Assign random number to Scissors, Paper, Stone
+//assign random number to scissors, paper, stone
 var assignComputerChoice = function(randomInteger){
   if (randomInteger == 1){
-    computerChoice = "Scissors";
+    computerChoice = "scissors";
   }
   if (randomInteger == 2){
-    computerChoice = "Paper";
+    computerChoice = "paper";
   } 
   if (randomInteger == 3){
-    computerChoice = "Stone";
+    computerChoice = "stone";
   }
   console.log('Computer choice is '+ computerChoice);
   return computerChoice;
