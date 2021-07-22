e939959da48faaa4670f692bb953bda34651225a
tanzheen <ntan418@gmail.com>
2021-07-22 16:09:14 +0800

    for final code review

--- script.js
+++ script.js
@@ -16,9 +16,8 @@ var whoWins = function (input, computerUse) {
   console.log(`computer hand = ${computerUse}`);
   // this is if the computer wins
   if ((computerUse == `scissors` && input == `paper`) || (computerUse == `paper` && input == `stone`) || (computerUse == `stone` && input == `scissors`)) {
-    playcount = playcount + 1;
     computerWin = computerWin + 1;
-    possibleChampion = " the computer ";
+
     console.log(`you lose`);
     var outcome = `you lose!`;
   }
@@ -26,28 +25,19 @@ var whoWins = function (input, computerUse) {
   // this is if the human wins
   else if ((input == `scissors` && computerUse == `paper`) || (input == `paper` && computerUse == `stone`) || (input == `stone` && computerUse == `scissors`)) {
     humanWin = humanWin + 1;
-    playcount = playcount + 1;
-    possibleChampion = " you";
+
     console.log(`you win`);
     var outcome = `you win!`;
-  }
-
-  //if there is a draw, the first duel will have no ultimate winner
+  } else if (input == computerUse) {
+    var outcome = "there is a draw!";
+  } //if there is a draw
   else if (input == computerUse) {
-    if (playcount == 0) {
-      console.log(`draw`);
-      possibleChampion = " no one";
-      var outcome = `you drew with the computer`;
-    }
-
-    // the ultimate winner is revealed
-    else if (playcount > 0) {
-      var outcome = `the ultimate champion is ${possibleChampion}! `;
-      playcount = 0;
-    }
+    console.log(`draw`);
+    var outcome = "there is a draw!";
   }
   return outcome;
 };
+
 // this is for the reversed game
 var ReversalWhoWins = function (input, computerUse) {
   console.log(`human uses ${input}`);
@@ -55,41 +45,64 @@ var ReversalWhoWins = function (input, computerUse) {
   console.log(`computer uses ${computerUse}`);
 
   // these is if the computer loses
-  if ((computerUse == `scissors` && input == `reversed paper`) || (computerUse == `paper` && input == `reversed stone`) || (computerUse == `stone` && input == `reversed scissors`)) {
+  if ((computerUse == `scissors` && input == `paper`) || (computerUse == `paper` && input == `stone`) || (computerUse == `stone` && input == `scissors`)) {
     humanWin = humanWin + 1;
-    playcount = playcount + 1;
+
     console.log(`you win`);
-    possibleChampion = " you";
+
     var outcome = "you win!";
   }
 
   // this is if the human loses
-  else if ((input == "reversed scissors" && computerUse == `paper`) || (input == `reversed paper` && computerUse == `stone`) || (input == `reversed stone` && computerUse == `scissors`)) {
+  else if ((input == "scissors" && computerUse == `paper`) || (input == `paper` && computerUse == `stone`) || (input == `stone` && computerUse == `scissors`)) {
     computerWin = computerWin + 1;
-    playcount = playcount + 1;
+
     console.log(`you lose`);
-    possibleChampion = " the computer ";
+
     var outcome = "you lose!";
   }
 
   //if there is a draw
-  else if (input == "reversed " + computerUse) {
+  else if (input == computerUse) {
     console.log(`draw`);
-    //if first play there wud be no ultimate winner
-    if (playcount == 0) {
-      var outcome = "you drew with the computer ";
-      possibleChampion = " no one";
-    }
+    var outcome = "there is a draw!";
+  }
 
-    // the ultimate winner is revealed
+  return outcome;
+};
+var koreanWhoWins = function (input, computerUse) {
+  console.log(`computer hand = ${computerUse}`);
+  // this is if the computer wins
+  if ((computerUse == `scissors` && input == `paper`) || (computerUse == `paper` && input == `stone`) || (computerUse == `stone` && input == `scissors`)) {
+    computerWin = computerWin + 1;
+    playcount = playcount + 1;
+    possibleChampion = "the computer";
+    console.log(`you lose`);
+    var outcome = `you lose! A draw on the next turn would make ${possibleChampion} the ultimate winner!! `;
+  }
+
+  // this is if the human wins
+  else if ((input == `scissors` && computerUse == `paper`) || (input == `paper` && computerUse == `stone`) || (input == `stone` && computerUse == `scissors`)) {
+    humanWin = humanWin + 1;
+    playcount = playcount + 1;
+    possibleChampion = "you";
+    console.log(`you win`);
+    var outcome = `you win! A draw on the next turn would make ${possibleChampion} the ultimate winner!! `;
+  } else if (input == computerUse) {
+    //if on first play , there is a draw
+    if (playcount == 0) {
+      var outcome = "there is a draw!";
+    }
+    //after there is at least one winner
     else if (playcount > 0) {
-      var outcome = `the ultimate champion is ${possibleChampion}! `;
-      playcount = 0;
+      var outcome = `.MUKJIIPPAPA! the ultimate winner is ${possibleChampion}`;
     }
   }
 
   return outcome;
 };
+
+var gamemode = "start";
 var possibleChampion = "";
 var playcount = 0;
 var username = "";
@@ -97,22 +110,24 @@ var computerWin = 0;
 var humanWin = 0;
 var main = function (input) {
   var computerUse = computerHand();
-  //the option to let the computer play for u starts here
-  if (input == `computer`) {
-    var urHand = computerHand();
-    var winner = whoWins(urHand, computerUse);
-    return winner + ` the computer chose ${computerUse}.<br> you were assigned ${urHand}. <br><br>the computer has won ${computerWin} times. You have won ${humanWin} times. `;
+
+  if (gamemode == "start") {
+    username = input;
+    gamemode = "choose mode";
+    outputvalue = `welcome ${username}. please choose a mode of SPS you wish to play. please choose either "normal", "reverse" or "korean"`;
+    return outputvalue;
+  } else if (input != "normal" && input != "reverse" && input != "korean" && input != "paper" && input != "scissors" && input != "stone") {
+    return "you have entered an invalid input";
+  } else if (gamemode == "choose mode") {
+    gamemode = `${input}`;
+    return ` you have chosen ${gamemode} mode. Now you can type "scissors" "paper" or "stone" to play`;
+  } else if (gamemode == "normal") {
+    var outcome = whoWins(input, computerUse);
+  } else if (gamemode == "reverse") {
+    var outcome = ReversalWhoWins(input, computerUse);
+  } else if (gamemode == "korean") {
+    var outcome = koreanWhoWins(input, computerUse);
   }
-  //the option to go reversed or stay normal remains here
-  else if (input == `reversed scissors` || input == `reversed stone` || input == `reversed paper`) {
-    var realInput = input.replace("reversed", "");
-    var reversedwinner = ReversalWhoWins(input, computerUse);
-    return reversedwinner + ` the computer chose ${computerUse} <br> you chose ${realInput} <br><br>Now you can type "scissors" "paper" or "stone" to play another round! the computer has won ${computerWin} times. You have won ${humanWin} times. the most recent winner is ${possibleChampion}`;
-  } // if there is invalid input
-  else if (input != "scissors" && input != "stone" && input != "paper") {
-    return " you have entered an invalid input";
-  }
-  //here go normal
-  else var winner = whoWins(input, computerUse);
-  return winner + ` the computer chose ${computerUse} <br> you chose ${input} <br><br>Now you can type "scissors" "paper" or "stone" to play another round! the computer has won ${computerWin} times. You have won ${humanWin} times. the most recent winner is ${possibleChampion}`;
+  var finishingSentence = ` you chose ${input}. the computer chose ${computerUse}. ${username}'s number of wins:${humanWin}. Computer's number of wins: ${computerWin}`;
+  return outcome + finishingSentence;
 };
