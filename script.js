9bfb181268dc329a10d58c7efe4fb206a63d4c90
melodii8 <melodiipohyi@gmail.com>
2021-07-23 01:55:46 +0800

    korean mukjjippa still not working yet

--- script.js
+++ script.js
@@ -10,23 +10,6 @@ var reversedStone = `reversed stone`;
 var currentMode = `pending inputs`;
 var userName = "";
 
-//how do i update this in real time?
-var userWins = 0;
-var compWins = 0;
-var trackwinloss = function (userWins, compWins) {
-  // if (userWins == 0 || compWins == 0) {
-  //   return `no one won`;
-  // } else 
-  if (userWins > compWins) {
-    return `You win ${userWins - compWins} more than the computer`;
-  } else if (userWins < compWins) {
-    return `The computer wins ${compWins - userWins} more than you`;
-  } else if (userWins == compWins) {
-    console.log(`user and comp wins : ${(userWins, compWins)}`);
-    return `you and the computer draw!`;
-  }
-};
-
 var rollDice = function () {
   //determine max number here
   return Math.floor(Math.random() * 3) + 1;
@@ -50,75 +33,258 @@ var generateComputerResult = function () {
   }
   return computerPlays;
 };
-
-
-var generateStdMsg = function (userName, computerPlays, input, winloss){
- return `hi ${userName}, Computer: ${computerPlays}. <br> Your input: ${input}. <br> Win-loss tracker : ${winloss}<br>`;
+//generate base msg
+var generateStdMsg = function (userName, computerPlays, input) {
+  return `Hello ${userName} :) Good game! <br> The computer played: ${computerPlays}. <br> You played: ${input}.`;
 };
-var playNormalGame = function (input,computerPlays, standardMessage){
-if (
-  (input == scissors && computerPlays == paper) ||
-  (input == paper && computerPlays == stone) ||
-  (input == stone && computerPlays == scissors)
+//generate overall result tracker
+var userWins = 0;
+var compWins = 0;
+//THIS GOT ERROR :(
+var trackwinloss = function (userWins, compWins) {
+var overallResult;
+  if (userWins > compWins) {
+    overallResult = `<br>You win (${(userWins/(userWins+compWins))*100}% more than the computer`;
+  } else if (userWins < compWins) {
+    overallResult = `<br>The computer wins ${(userWins/(userWins+compWins))*100}% more than you`;
+  } else if (userWins == compWins) {
+    overallResult =`<br>You and the computer draw!`
+  } return overallResult
+};
+
+//game modes
+//=========================== NORMAL ============================
+
+var playNormalGame = function (input, computerPlays, standardMessage) {
+  var myOutputValue;
+  if ( //user wins
+    (input == scissors && computerPlays == paper) ||
+    (input == paper && computerPlays == stone) ||
+    (input == stone && computerPlays == scissors)
+  ) {
+    myOutputValue = `${standardMessage} <br> YAY You win! <br>Now you can type "scissors", "paper" or "stone" to play another round!`;
+    userWins = userWins + 1;
+    console.log("if statement", myOutputValue);
+  } else if ( //computer wins
+    (computerPlays == scissors && input == paper) ||
+    (computerPlays == paper && input == stone) ||
+    (computerPlays == stone && input == scissors)
+  ) {
+    myOutputValue = `${standardMessage} <br> You lost :( <br>Now you can type "scissors", "paper" or "stone" to play another round!`;
+    compWins = compWins + 1;
+    console.log("if statement", myOutputValue);
+  } else if ( //user draw with com
+    input == computerPlays) {
+    myOutputValue = `${standardMessage} <br> it's a draw! <br> Now you can type "scissors", "paper" or "stone" to play another round!`;
+    console.log("else if statement", computerPlays, input);
+  } else if ( //input validation
+    input != scissors &&
+    input != paper &&
+    input != stone
+  ) {
+    myOutputValue = `You are playing Normal Scissors Paper Stone. Please input "scissors", "paper" or "stone" to play.`;
+    console.log("else statement", computerPlays, input);
+  }
+  return myOutputValue;
+};
+//=========================== REVERSED ============================
+var playReversedGame = function (
+  input,
+  computerPlays,
+  standardMessage,
+  userWins,
+  compWins
 ) {
-  myOutputValue = `you win! <br>${standardMessage} Now you can type "scissors", "paper" or "stone" to play another round!`;
-  userWins = userWins + 1;
-  console.log("if statement", myOutputValue);
-} else if (input == scissors || input == paper || input == stone) {
-  myOutputValue = `you lose! <br> ${standardMessage} <br> Now you can type "scissors", "paper" or "stone" to play another round!`;
-  compWins = compWins + 1;
-  console.log("else if statement", computerPlays, input);
-}
-};
-var playReversedGame = function (input,computerPlays)
-{if ((input == reversedScissors && computerPlays ==  stone) ||
+  var myOutputValue;
+  if (
+    //user wins
+    (input == reversedScissors && computerPlays == stone) ||
     (input == reversedPaper && computerPlays == scissors) ||
     (input == reversedStone && computerPlays == paper)
   ) {
-    myOutputValue = `reversal! you won!`;
+    
+    myOutputValue = `${standardMessage} <br> Reversal! You won!<br> <br> Please input "reversed scissors", "reversed paper" or "reversed stone" to try again.`;
     userWins = userWins + 1;
-  } else if (
-    input == reversedScissors ||
-    input == reversedPaper ||
-    input == reversedStone
+  } else if (//computer wins
+    (input == reversedScissors && computerPlays == paper) ||
+    (input == reversedPaper && computerPlays == stone) ||
+    (input == reversedStone && computerPlays == scissors)
   ) {
-    myOutputValue = `reversal! you lost!!`;
+    myOutputValue = `${standardMessage}  <br> Reversal! You won! <br><br>Please input "reversed scissors", "reversed paper" or "reversed stone" to try again.`;
     compWins = compWins + 1;
-  } else {
-    myOutputValue = `Please input "scissors", "paper" or "stone".`;
-    console.log("else statement", computerPlays, input);
+    //draw conditions
+  } else if (
+    (input == reversedScissors && computerPlays == scissors) ||
+    (input == reversedPaper && computerPlays == paper) ||
+    (input == reversedStone && computerPlays == stone)
+  ) {
+    myOutputValue = `${standardMessage}<br> Reversal! It's a draw!<br><br>Please input "reversed scissors", "reversed paper" or "reversed stone" to try again.`;
+    //input validation
+  } else if (
+    input != reversedScissors && input != reversedPaper &&
+    input != reversedStone
+  ) {
+    myOutputValue = `You are playing Reversed Scissors Paper Stone. Please input "reversed scissors", "reversed paper" or "reversed stone" to play.`;
+    console.log("else if statement", computerPlays, input);
   }
+  return myOutputValue;
 };
 
+//======================== KOREAN MUKJJIPPA =========================
+var mukJjiPpaMode = false;
 
-//main function
+var playKoreanGame = function (input, computerPlays, standardMessage) {
+  var myOutputValue;
+  var koreanUserWins = 0;
+  var koreanCompWins = 0;
+  if (mukJjiPpaMode = false) {
+    if (
+      (input == scissors && computerPlays == paper) ||
+      (input == paper && computerPlays == stone) ||
+      (input == stone && computerPlays == scissors)
+    ) {
+      myOutputValue = `User shouts Muk-Jji-Ppa!`;
+      mukJjiPpaMode = true;
+      koreanUserWins = koreanUserWins + 1;
+      console.log(`user wins. mukJjiPpaMode`, mukJjiPpaMode);
+
+    } else if (
+      (computerPlays == scissors && input == paper) ||
+      (computerPlays == paper && input == stone) ||
+      (computerPlays == stone && input == scissors)
+    ) {
+      myOutputValue = `Computer shouts: Muk-Jji-Ppa!`;
+      mukJjiPpaMode = true;
+      koreanCompWins = koreanCompWins + 1;
+      console.log(`comp wins. mukJjiPpaMode`, mukJjiPpaMode);
+
+      //input validation
+    } else if (input != scissors && input != paper && input != stone) { myOutputValue = `You're playing Muk-Jji-Ppa! Please input "scissors", "paper" or "stone".`;
+      koreanUserWins = 0;
+      koreanCompWins = 0;
+      console.log(`else if statement: comp plays ${computerPlays}, you play${input} & mukJjiPpaMode is ${mukJjiPpaMode}`);
+    } else if (input == computerPlays){
+      myOutputValue = `It's a draw. Please input "scissors", "paper" or "stone" to try again.`;
+    }
+  //if mukjjippa mode is on and user wins, total user winnings +1
+  } else if (mukJjiPpaMode == true) {
+    if (
+      input == computerPlays &&
+      koreanUserWins == 1
+    ) {
+      myOutputValue = `${standardMessage}<br> You win Muk Jji Ppa!`;
+      userWins = userWins + 1;
+      mukJjiPpaMode = false;
+
+    //if mukjjippa mode is on and computer wins, total computer winnings +1
+    }
+    if (input == computerPlays &&
+      koreanCompWins == 1
+    ) {
+      myOutputValue = `${standardMessage}<br> Computer wins Muk Jji Ppa!`;
+      compWins = compWins + 1;
+      mukJjiPpaMode = false;
+    }
+  }
+  var overallWinner;
+  if (userWins > compWins) {
+    overallWinner = `<br> Overall winner is ${userName}. You won ${
+      (userWins / (userWins + compWins)) * 100
+    }% of the time`;
+  } else if (userWins < compWins) {
+    overallWinner = `<br> Overall winner is computer. Computer won ${
+      (userWins / (userWins + compWins)) * 100
+    }% of the time`;
+  } else if ((userWins = compWins)) {
+    overallWinner = `<br> It's a draw overall!`;
+  }
+  return myOutputValue + overallWinner;
+};
+//=========================== COM v COM ============================
+var playCOMvCOMgame = function (userGuess, computerPlays, standardMessage){
+    var myOutputValue;
+    var userGuess = generateComputerResult();
+    console.log (`userGuess =`,userGuess)
+    if ( //user wins
+      (userGuess == scissors && computerPlays == paper) ||
+      (userGuess == paper && computerPlays == stone) ||
+      (userGuess == stone && computerPlays == scissors)
+    ) {
+      myOutputValue = `${standardMessage} Computer generated ${userGuess} for you.<br> You win YAY! <br><br> Now you can type "scissors", "paper" or "stone" to play another round!`;
+      userWins = userWins + 1;
+      console.log("if statement", myOutputValue);
+    } else if ( //computer wins
+      (computerPlays == scissors && userGuess == paper) ||
+      (computerPlays == paper && userGuess == stone) ||
+      (computerPlays == stone && userGuess == scissors)
+    ) {
+      myOutputValue = `${standardMessage} Computer generated ${userGuess} for you.<br> You lose! <br><br> Now you can type "scissors", "paper" or "stone" to play another round!`;
+      compWins = compWins + 1;
+      console.log("if statement", myOutputValue);
+    } else if ( //its a draw
+      userGuess == computerPlays){
+      myOutputValue = `${standardMessage} <br> Computer generated ${userGuess} for you.<br> It's a draw! <br><br> Now you can type "scissors", "paper" or "stone" to play another round!`;;
+      console.log("else if statement", computerPlays, userGuess);
+    }
+    return myOutputValue;
+  };
+
+
+//=========================== MAIN FUNC ============================
 var main = function (input) {
   //Prevent caps errors
   input = input.toLowerCase();
   var myOutputValue = ``;
 
   var computerPlays = generateComputerResult();
-  console.log("generateComputerResult function");
-  console.log(computerPlays);
-  var winloss = trackwinloss(userWins, compWins);
+  console.log(`generateComputerResult function, computer plays: ${computerPlays}`);
 
-  var standardMessage = generateStdMsg (userName, computerPlays, input, winloss)
+  var standardMessage = generateStdMsg(userName, computerPlays, input);
+  console.log(standardMessage);
+  
   //determine the game mode
   if (currentMode == `pending inputs`) {
     userName = input;
+    console.log(`user name: ${userName}`);
     // now that we have the name, switch the mode
     currentMode = "normal game mode";
-    myOutputValue = "Hello " + userName;
-    console.log (`current normal mode: ${currentMode}`)
+    myOutputValue = `Hello ${userName}! <br> Enter 'scissors', 'paper' or 'stone' to start the game! <br><br> Alternatively you can input any of the following to change the game mode: <br> 
+    'reversed', 'korean' or 'com v com'.`;
+    console.log(`current normal mode: ${currentMode}`);
+  } else if (currentMode != `pending inputs` && input == "normal") {
+    currentMode = "normal game mode";
+    myOutputValue = `Let's play normal SPS!`;
 
-  } else if (input =='reverse'){
-    currentMode = 'reversed';
-    console.log (`current mode: ${currentMode}`)
-    myOutputValue = playReversedGame (input,computerPlays, standardMessage)
-  } else if (currentMode == 'normal game mode'){
-    myOutputValue = playNormalGame (input,computerPlays,standardMessage)
+    //allow selection of game modes
+  } else if (currentMode != `pending inputs` && input == "korean") {
+    currentMode = `mukJjiPpa`;
+    myOutputValue = `Let's play Muk-Jji-Ppa!`;
+  } else if (currentMode != `pending inputs` && input == `reversed`) {
+    currentMode = `reversed`;
+    myOutputValue = `Let's play reversed SPS!`
+  } else if (currentMode != `pending inputs` && input == `com v com`) {
+    currentMode = `com v com`;
+    myOutputValue = `Let's play Computer VS Computer!`;
+
+    //start of actual game
+  } else if (currentMode == "normal game mode") {
+    console.log(`current mode: ${currentMode}`);
+    myOutputValue = playNormalGame(input, computerPlays, standardMessage) + overallResult;
+    //introduce reversed game mode
+  } else if (currentMode == "reversed") {
+    console.log(`current mode: ${currentMode}`);
+    myOutputValue = playReversedGame(input, computerPlays, standardMessage)+ overallResult;
+    // introduce muk-jji-ppa game mode
+  } else if (currentMode == `mukJjiPpa`) {
+    console.log(`current mode: ${currentMode}`);
+    myOutputValue = playKoreanGame(input, computerPlays, standardMessage)+ overallResult;
+  } else if (currentMode == `com v com`){
+    console.log(`current mode: ${currentMode}`);
+    myOutputValue = playCOMvCOMgame(input, computerPlays, standardMessage)+ overallResult;
   }
-  console.log("std msg" + computerPlays, input);
+  
+  var overallResult = trackwinloss (userWins, compWins);
+  console.log(`User and comp wins : ${(userWins, compWins)}`)
 
   return myOutputValue;
 };
