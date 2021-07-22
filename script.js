e16aadc65fedb48e2cc820311b262691f24b4d39
ayupermhm <ayupermata19602@gmail.com>
2021-07-22 14:16:05 +0800

    did sps part 1

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
+//Program randomly choose scissors, paper or stone
+var myArray = ["Scissors", "Paper", "Stone"];
+var randomItem = function () {
+  var randomObject = myArray[Math.floor(Math.random() * myArray.length)];
+  return randomObject;
+};
+
+var sps = function (userInput) {
+  var programmeInput = randomItem();
+
+  //user won
+  if (
+    (userInput == "Scissors" && programmeInput == "Paper") ||
+    (userInput == "Stone" && programmeInput == "Scissors") ||
+    (userInput == "Paper" && programmeInput == "Stone")
+  ) {
+    return "you won";
+  }
+
+  //program won
+  else if (
+    (userInput == "Paper" && programmeInput == "Scissors") ||
+    (userInput == "Scissors" && programmeInput == "Stone") ||
+    (userInput == "Stone" && programmeInput == "Paper")
+  ) {
+    return "you lose";
+  }
+
+  //draw
+  else {
+    return "it's a draw";
+  }
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (input != "Scissors" && input != "Paper" && input != "Stone") {
+    var myOutputValue =
+      "Please choose the 3 valid input options which are Scissors, Paper or Stone";
+    return myOutputValue;
+  } else {
+    return sps(input);
+  }
 };
