d32ebb144c2fd73360aaba94e362d681d5131a98
prawirapikanto <prawira.pikanto@gmail.com>
2021-07-24 10:28:03 +0800

    I finished the code

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  return conditionList(input);
+};
+
+var generateRandomRPS = function () {
+  var numPossibleWords = 3;
+  var randomNum = Math.floor(Math.random() * numPossibleWords) + 1;
+  console.log(randomNum);
+  if (randomNum == 1) return "rock";
+  else if (randomNum == 2) return "paper";
+  else if (randomNum == 3) return "scissors";
+};
+
+var conditionList = function (input) {
+  var randomAns = generateRandomRPS();
+  if (input == randomAns)
+    return (
+      "Draw!" + input + "vs" + randomAns + "(your choice vs computer choice)"
+    );
+  else if (input == "rock" && randomAns == "paper")
+    return (
+      "You Lose! Computer Wins" +
+      input +
+      "vs" +
+      randomAns +
+      "(your choice vs computer choice)"
+    );
+  else if (input == "rock" && randomAns == "scissors")
+    return (
+      "You Win!" + input + "vs" + randomAns + "(your choice vs computer choice)"
+    );
+  else if (input == "paper" && randomAns == "rock")
+    return (
+      "You Win" + input + "and" + randomAns + "(your choice vs computer choice)"
+    );
+  else if (input == "paper" && randomAns == "scissors")
+    return (
+      "You Lose! Computer Wins" +
+      input +
+      "vs" +
+      randomAns +
+      "(your choice vs computer choice)"
+    );
+  else if (input == "scissors" && randomAns == "rock")
+    return (
+      "You Lose! Computer Wins" +
+      input +
+      "vs" +
+      randomAns +
+      "(your choice vs computer choice)"
+    );
+  else if (input == "scissors" && randomAns == "paper")
+    return (
+      "You Win!" + input + "vs" + randomAns + "(your choice vs computer choice)"
+    );
 };
