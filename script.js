ed7faa0b3c8164c82d560fa7c30e4737e5105a7a
hWeitian <huangweitian1@hotmail.com>
2021-08-22 18:29:53 +0800

    add input validation

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var main = function (input) {
+  // Input validation - Check if user input only scissors, paper or stone. Prompt users if required input is not found.
+  if (!(input == "scissors" || input == "paper" || input == "stone")) {
+    var myOutputValue = `Please input only scissors, paper or stone`;
+    return myOutputValue;
+  }
+  // Generate random option of either scissors, paper or stone
   var outputByProgram = pickOption();
   var myOutputValue = `The random option selected by the program is ${outputByProgram}.`;
   return myOutputValue;
@@ -12,9 +18,11 @@ var optionsArray = ["scissors", "paper", "stone"];
 var pickRandomNumber = function () {
   // Generate random decimal between 0 to 3, excluding 3.
   var randomDecimal = Math.random() * 3;
+  console.log(`Random Decimal is ${randomDecimal}`);
 
   // Generate random integer between 0 to 3, exlucing 3.
   var randomInteger = Math.floor(randomDecimal);
+  console.log(`Random Integer is ${randomInteger}`);
 
   return randomInteger;
 };
