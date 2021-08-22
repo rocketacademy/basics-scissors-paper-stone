98e8e043906eda7daec08c51a5046e6e97fbd97a
hWeitian <huangweitian1@hotmail.com>
2021-08-22 18:17:36 +0800

    add code for computer to randomly pick scissors, paper or stone

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var outputByProgram = pickOption();
+  var myOutputValue = `The random option selected by the program is ${outputByProgram}.`;
   return myOutputValue;
+};
+
+// Set the game options into an array
+var optionsArray = ["scissors", "paper", "stone"];
+
+// Generate random integer between 0 to 2.
+
+var pickRandomNumber = function () {
+  // Generate random decimal between 0 to 3, excluding 3.
+  var randomDecimal = Math.random() * 3;
+
+  // Generate random integer between 0 to 3, exlucing 3.
+  var randomInteger = Math.floor(randomDecimal);
+
+  return randomInteger;
+};
+
+var pickOption = function () {
+  // Generate the random integer to be used to select the item in the array
+  var randomOptionNum = pickRandomNumber();
+
+  // Use the random integer to select the random item in the array
+  var selectedOption = optionsArray[randomOptionNum];
+
+  return selectedOption;
 };
