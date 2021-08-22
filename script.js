c6ad5138cea67fd3238ccfa02833ad060685ab3a
hWeitian <huangweitian1@hotmail.com>
2021-08-22 21:05:30 +0800

    Add game rules - check user input against program output to determine winner

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-var main = function (input) {
+var main = function (userInput) {
   // Input validation - Check if user input only scissors, paper or stone. Prompt users if required input is not found.
-  if (!(input == "scissors" || input == "paper" || input == "stone")) {
+  if (
+    !(userInput == "scissors" || userInput == "paper" || userInput == "stone")
+  ) {
     var myOutputValue = `Please input only scissors, paper or stone`;
     return myOutputValue;
   }
   // Generate random option of either scissors, paper or stone
   var outputByProgram = pickOption();
-  var myOutputValue = `The random option selected by the program is ${outputByProgram}.`;
-  return myOutputValue;
+
+  var myOutputValueWin = `The computer chose ${outputByProgram}. You won!`;
+  var myOutputValueLose = `The computer chose ${outputByProgram}. You lose!`;
+
+  // Check user input against program output to determine winner (game rules)
+  if (userInput == outputByProgram) {
+    var myOutputValue = `The computer chose ${outputByProgram}. It's a draw!`;
+    return myOutputValue;
+  }
+
+  if (userInput == "scissors" && outputByProgram == "paper") {
+    return myOutputValueWin;
+  }
+
+  if (userInput == "scissors" && outputByProgram == "stone") {
+    return myOutputValueLose;
+  }
+
+  if (userInput == "paper" && outputByProgram == "scissors") {
+    return myOutputValueLose;
+  }
+
+  if (userInput == "paper" && outputByProgram == "stone") {
+    return myOutputValueWin;
+  }
+
+  if (userInput == "stone" && outputByProgram == "scissors") {
+    return myOutputValueWin;
+  }
+
+  if (userInput == "stone" && outputByProgram == "paper") {
+    return myOutputValueLose;
+  }
 };
 
 // Set the game options into an array
