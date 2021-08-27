34f946ef3ac297a7d5679c37b81843f604ccf06a
eggcodes <helloeuer@gmail.com>
2021-08-28 02:05:10 +0800

    - resolved issues mentioned on codeclimate bot - reduced complexity within  code block; shifted  out of  and made into a helper function - renamed some function variables for clarity (checkResults,checkReversedResults) to (generateNormalResults,generateReversedResults)

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
 var main = function (userInput) {
-  // GENERATE RANDOM COMPUTER OUTPUT: SCISSORS, PAPER, STONE //
+  // GENERATE RANDOM COMPUTER OUTPUT: SCISSORS, PAPER, STONE
   var programOutput = generateProgramOutput();
 
-  // CHECK IF USER INPUT IS VALID
-  const inputOptions = [
-    "scissors",
-    "paper",
-    "stone",
-    "reversed scissors",
-    "reversed paper",
-    "reversed stone",
-  ];
-  // if does not contain any object in array, generate error message.
-  if (!inputOptions.includes(userInput)) {
-    return `Please input "scissors", "paper" or "stone". <br><br>
-    <div> Easter Egg: try adding "reversed" in front of your input. E.g "reversed stone".
-    `;
+  // CHECK IF USER INPUT IS VALID; IF NOT, GENERATE ERROR MESSAGE.
+  if (!checkUserInputValidity(userInput)) {
+    return `Please input "scissors", "paper" or "stone".`;
   }
 
-  // CHECK IF USER INPUT CONTAINS "reversed"
-  // if contains "reversed", check if user win, lose or draw. and return outcome string.
+  // CHECK IF USER INPUT CONTAINS "reversed"; IF YES, GENERATE REVERSE OUTCOME. IF NO, GENERATE NORMAL OUTCOME.
+  // if yes, check if user win, lose or draw. and return outcome string. (reversed version)
   if (userInput.includes("reversed")) {
-    var userOutcome = checkReversedResult(userInput, programOutput);
-
-    // if normal version, check if user win, lose or draw. and return outcome string.
+    var userOutcome = generateReversedResults(userInput, programOutput);
+    // if no, check if user win, lose or draw. and return outcome string. (normal version)
   } else {
-    var userOutcome = checkResult(userInput, programOutput);
+    var userOutcome = generateNormalResults(userInput, programOutput);
   }
 
   // GENERATE NICER INPUT: ADD EMOJIS
@@ -37,28 +25,45 @@ var main = function (userInput) {
   var myOutputValue = `The computer chose ${programOutput}. <br>
   You chose ${userInput}. <br><br>
   You ${userOutcome} <br><br>
-  Now you can type "scissors", "paper" or "stone" to play another round!
-  `;
+  Now you can type "scissors", "paper" or "stone" to play another round!<br><br>
+  <div> Easter Egg: try adding "reversed" in front of your input. E.g "reversed stone".`;
   return myOutputValue;
 };
 
-// HELPER FUNCTIONS BELOW:
-// GENERATE RANDOM COMPUTER OUTPUT: SCISSORS, PAPER, STONE //
+// HELPER FUNCTIONS BELOW: //
+
+// GENERATE RANDOM COMPUTER OUTPUT: SCISSORS, PAPER, STONE
 // generate a number from 0 to 2, to be used by computer to select output from array
 var generateRandomNumber = function () {
   var randomDecimal = Math.random() * 3;
   return Math.floor(randomDecimal);
 };
-
-// computer randomly chooses scissors, paper or stone
+// generate computer output: randomly choose scissors, paper or stone
 var generateProgramOutput = function () {
   const outputOptions = ["scissors", "paper", "stone"];
-  return (programOutput = outputOptions[generateRandomNumber()]);
+  return outputOptions[generateRandomNumber()];
 };
 
-// TO CHECK IF USER WINS, LOSES OR DRAWS. //
+// CHECK IF USER INPUT IS VALID
+var checkUserInputValidity = function (userInput) {
+  const inputOptions = [
+    "scissors",
+    "paper",
+    "stone",
+    "reversed scissors",
+    "reversed paper",
+    "reversed stone",
+  ];
+  // check if user input is one of array objects. if yes, return true. if no, return false.
+  if (inputOptions.includes(userInput)) {
+    return true;
+  }
+  return false;
+};
+
+// TO GENERATE OUTPUT MESSAGES WHEN USER WINS, LOSES OR DRAWS.
 // #1: normal version
-var checkResult = function (userInput, programOutput) {
+var generateNormalResults = function (userInput, programOutput) {
   // if user wins, output message
   if (
     (userInput.includes("scissors") && programOutput == "paper") ||
@@ -66,40 +71,43 @@ var checkResult = function (userInput, programOutput) {
     (userInput.includes("stone") && programOutput == "scissors")
   ) {
     return `win! Congrats.`;
-    // if user loses, output message
-  } else if (
+  }
+  // if user loses, output message
+  if (
     (userInput.includes("scissors") && programOutput == "stone") ||
     (userInput.includes("paper") && programOutput == "scissors") ||
     (userInput.includes("stone") && programOutput == "paper")
   ) {
     return `lose! Bummer.`;
     // if anything else (user draws), output message
-  } else {
-    return `draw. Try again?`;
   }
+  return `draw. Try again?`;
 };
 
 // #2: reversed version
-var checkReversedResult = function (userInput, programOutput) {
+var generateReversedResults = function (userInput, programOutput) {
   // if user wins normally, reverse output message
-  if (checkResult(userInput, programOutput) == "win! Congrats.") {
+  if (generateNormalResults(userInput, programOutput) == "win! Congrats.") {
     return `lose! Bummer.`;
-    // if user loses normally, reverse output message
-  } else if (checkResult(userInput, programOutput) == "lose! Bummer.") {
-    return `win! Congrats.`;
-  } else {
-    // if anything else (user draws), output message
-    return `draw. Try again?`;
   }
+  // if user loses normally, reverse output message
+  if (generateNormalResults(userInput, programOutput) == "lose! Bummer.") {
+    return `win! Congrats.`;
+  }
+  // if anything else (user draws), output message
+  return `draw. Try again?`;
 };
 
-// GENERATE NICER INPUT: ADD-ON EMOJIS//
+// GENERATE NICER INPUT: ADD-ON EMOJIS
 var addEmojis = function (input) {
+  // if user input includes "scissors", return knife emoji
   if (input.includes("scissors")) {
     return ` 🔪`;
-  } else if (input.includes("stone")) {
-    return ` 🗿`;
-  } else {
+  }
+  // if user input includes "paper", return paper
+  if (input.includes("paper")) {
     return ` 📄`;
   }
+  // if anything else ("stone"), return easter island emoji
+  return ` 🗿`;
 };
