de58a529be6ddbb466dd9e5f1479d74812a5d8cd
anthearosechy <anthearosechy@outlook.com>
2021-08-27 16:26:11 +0800

    Added helper function to randomise computer's choice of R/P/S

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
+};
+
+var rollDice = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal);
+  var diceNumber = randomInteger + 1;
+  return diceNumber;
 };
