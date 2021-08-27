1bd5de4f90d2dc6128c6c6d6f373945d3e9602d1
anthearosechy <anthearosechy@outlook.com>
2021-08-27 16:38:22 +0800

    Assign computer's random number pick to Scissors, Paper or Stone

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 var main = function (input) {
+  var progsChoice = rollDice();
+  if (progsChoice == 1) {
+    progsChoice = "Scissors";
+  }
+  if (progsChoice == 2) {
+    progsChoice = "Paper";
+  }
+  if (progsChoice == 3) {
+    progsChoice = "Stone";
+  }
+  console.log(progsChoice);
   var myOutputValue = "hello world";
   return myOutputValue;
 };
