08448865cadccb3158f0584845d388d632b20697
anthearosechy <anthearosechy@outlook.com>
2021-08-27 17:33:54 +0800

    Included reverse game but I'm afraid it's very inefficient

--- script.js
+++ script.js
@@ -10,21 +10,18 @@ var main = function (input) {
     progsChoice = "Stone";
   }
   console.log(progsChoice);
-  var myOutputValue = "You can only type 'Scissors', 'Paper' or 'Stone'.";
-  if (input == progsChoice) {
+  var myOutputValue =
+    "You can only type 'Scissors', 'Paper' or 'Stone'. <br> Or try including the word 'Reverse' before your choice to play the reverse scissors, paper, stone game.";
+  if (input.includes(progsChoice)) {
     myOutputValue = "It's a draw!";
   }
   if (
     (input == "Scissors" && progsChoice == "Paper") ||
     (input == "Paper" && progsChoice == "Stone") ||
-    (input == "Stone" && progsChoice == "Scissors")
-  ) {
-    myOutputValue = "You win!";
-  }
-  if (
-    (input == "Scissors" && progsChoice == "Paper") ||
-    (input == "Paper" && progsChoice == "Stone") ||
-    (input == "Stone" && progsChoice == "Scissors")
+    (input == "Stone" && progsChoice == "Scissors") ||
+    (input == "Reverse Scissors" && progsChoice == "Stone") ||
+    (input == "Reverse Stone" && progsChoice == "Paper") ||
+    (input == "Reverse Paper" && progsChoice == "Scissors")
   ) {
     myOutputValue = "You win! Yay!";
   }
@@ -34,6 +31,13 @@ var main = function (input) {
     (progsChoice == "Stone" && input == "Scissors")
   ) {
     myOutputValue = "You lose! Sobbles.";
+  }
+  if (
+    (input == "Reverse Scissors" && progsChoice == "Paper") ||
+    (input == "Reverse Stone" && progsChoice == "Scissors") ||
+    (input == "Reverse Paper" && progsChoice == "Stone")
+  ) {
+    myOutputValue = "You would've won if this was the normal game.";
   }
   return `${myOutputValue} <br> The computer chose ${progsChoice} and you chose ${input}. <br> Feel free to try again!`;
 };
