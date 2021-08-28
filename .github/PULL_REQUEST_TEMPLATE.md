5284f05f120b26b78d7359309b04d7e24126f49f
bryanhandc <bryan.han.2016@business.smu.edu.sg>
2021-08-28 23:05:33 +0800

    2nd part of project

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -3,15 +3,22 @@ Please fill out the survey before submitting the pull request. Thanks!
 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 
 How many hours did you spend on this assignment?
+//1hr
 
 Please fill in one error and/or error message you received while working on this assignment.
+//Uncaught ReferenceError: input is not defined
 
 What part of the assignment did you spend the most time on?
+//Linking the various functions together such that the variables are defined.
 
 Comfort Level (1-5):
+//4
 
 Completeness Level (1-5):
+//4
 
 What did you think of this deliverable?
+//It was fun and actually allowed us to use the different operators we learnt.
 
 Is there anything in this code that you feel pleased about?
+//-nil-
--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// setting the states to keep track of the wins for user and computer, as well as the counting of rounds and storing of username
+var userWin = 0;
+var computerWin = 0;
+var roundsCount = 0;
+var userName = "";
+
 // Create a basic version of Scissors Paper Stone where the user inputs one of "scissors", "paper", or "stone", the program internally randomly chooses scissors, paper, or stone, and the program outputs whether the user won, the program won, or it's a draw.
 var main = function (input) {
   var letsPlay = gameRules(input);
@@ -7,14 +13,34 @@ var main = function (input) {
 // Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
 var gameRules = function (input) {
   var systemThrow = systemGuess();
-
+  var userWinPercent = (userWin / roundsCount) * 100;
+  var computerWinPercent = (computerWin / roundsCount) * 100;
+  if (userName == "") {
+    // assumption is user wont type or keyed in 1 space
+    if (
+      input == "" ||
+      input == " " ||
+      input == "scissors" ||
+      input == "paper" ||
+      input == "stone"
+    ) {
+      return `Please set a username first by typing your username in the input box.`;
+    }
+    userName = input;
+    return `You have set your username as "${input}". Now, to start the game, please enter either "scissors" or "paper" or "stone". Enjoy!`;
+  }
   // if what we guess is the same as what the system throws, then it is a draw
   if (input == systemThrow) {
     console.log("Input");
     console.log(input);
     console.log("System");
     console.log(systemThrow);
-    return `It's a draw! You threw ${input} and the program threw ${systemThrow}.`;
+    roundsCount = roundsCount + 1;
+    userWinPercent = (userWin / roundsCount) * 100;
+    computerWinPercent = (computerWin / roundsCount) * 100;
+    return `It's a draw! <br> ${userName} threw ${input} and the program threw ${systemThrow}. <br> It's been ${roundsCount} round(s) and the scores are as such: <br> ${userName} ${userWin} (${userWinPercent.toFixed(
+      2
+    )}%):  Computer ${computerWin} (${computerWinPercent.toFixed(2)}%)`;
   }
   // if what we guess is different from the what the system throws, and based on gamerules, we win
   if (
@@ -26,7 +52,13 @@ var gameRules = function (input) {
     console.log(input);
     console.log("System");
     console.log(systemThrow);
-    return `You won! You threw ${input} and the program threw ${systemThrow}.`;
+    userWin = userWin + 1;
+    roundsCount = roundsCount + 1;
+    userWinPercent = (userWin / roundsCount) * 100;
+    computerWinPercent = (computerWin / roundsCount) * 100;
+    return `${userName} won! <br> ${userName} threw ${input} and the program threw ${systemThrow}. <br> It's been ${roundsCount} round(s) and the scores are as such: <br> ${userName} ${userWin} (${userWinPercent.toFixed(
+      2
+    )}%):  Computer ${computerWin} (${computerWinPercent.toFixed(2)}%)`;
   }
   // if what we guess is different from the what the system throws, and based on gamerules, we lose
   if (
@@ -38,7 +70,13 @@ var gameRules = function (input) {
     console.log(input);
     console.log("System");
     console.log(systemThrow);
-    return `You lose! You threw ${input} and the program threw ${systemThrow}.`;
+    computerWin = computerWin + 1;
+    roundsCount = roundsCount + 1;
+    userWinPercent = (userWin / roundsCount) * 100;
+    computerWinPercent = (computerWin / roundsCount) * 100;
+    return `${userName} lose! <br> ${userName} threw ${input} and the program threw ${systemThrow}. <br> It's been ${roundsCount} round(s) and the scores are as such: <br> ${userName} ${userWin} (${userWinPercent.toFixed(
+      2
+    )}%):  Computer ${computerWin} (${computerWinPercent.toFixed(2)}%)`;
   }
   return `Please type either "scissors" or "paper" or "stone".`;
 };
