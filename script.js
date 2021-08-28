9f31c422be3df4d426413fb7740f2f2612f05d13
agnesia-lubuk <agnesia.lubuk@gmail.com>
2021-08-29 03:25:28 +0700

    fixing some issues and refactoring

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const compRandom = () => {
   const compRandomChoice = Math.floor(Math.random() * 3) + 1;
   if (compRandomChoice == 1) {
     return "stone";
-  } else if (compRandomChoice == 2) {
+  }
+  if (compRandomChoice == 2) {
     return "paper";
   }
   return "scissors";
 };
+
 const inputChecking = (input) => {
   if (
     input == "stone" ||
@@ -21,25 +23,31 @@ const inputChecking = (input) => {
   return false;
 };
 
+const checkResult = (input, comp) => {
+  let result = "";
+  if (input == comp || input == "reversed " + comp) {
+    result = "It's a draw!";
+  } else if (
+    (input == "stone" && comp == "scissors") ||
+    (input == "paper" && comp == "stone") ||
+    (input == "scissors" && comp == "paper") ||
+    (input == "reversed stone" && comp == "paper") ||
+    (input == "reversed paper" && comp == "scissors") ||
+    (input == "reversed scissors" && comp == "stone")
+  ) {
+    result = "You win! ";
+  } else {
+    result = "You lose! Bummer.";
+  }
+  return result;
+};
+
 const main = (input) => {
   const comp = compRandom();
-  let result = "";
   const validInput = inputChecking(input);
+  const result = checkResult(input, comp);
+
   if (validInput) {
-    if (input == comp || input == "reversed " + comp) {
-      result = "It's a draw!";
-    } else if (
-      (input == "stone" && comp == "scissors") ||
-      (input == "paper" && comp == "stone") ||
-      (input == "scissors" && comp == "paper") ||
-      (input == "reversed stone" && comp == "paper") ||
-      (input == "reversed paper" && comp == "scissors") ||
-      (input == "reversed scissors" && comp == "stone")
-    ) {
-      result = "You win! ";
-    } else {
-      result = "You lose! Bummer.";
-    }
     return `The computer chose ${comp}.<br>You chose ${input}.<br><br>${result}<br><br>Now you can type "scissors" "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
   }
   return `Try again. <br>Now you can type "scissors" "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
