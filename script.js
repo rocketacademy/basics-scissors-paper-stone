987aec29e0a358f519c3c2c02e117e7d167326b8
agnesia-lubuk <agnesia.lubuk@gmail.com>
2021-08-28 17:10:49 +0700

    create SPS game

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  const compRandom = () => {
+    const compRandomChoice = Math.floor(Math.random() * 3) + 1;
+    if (compRandomChoice == 1) {
+      return "stone";
+    } else if (compRandomChoice == 2) {
+      return "paper";
+    } else {
+      return "scissors";
+    }
+  };
+  comp = compRandom();
+
+  if (
+    input == "stone" ||
+    input == "paper" ||
+    input == "scissors" ||
+    input == "reversed stone" ||
+    input == "reversed paper" ||
+    input == "reversed scissors"
+  ) {
+    if (input == comp || input == "reversed " + comp) {
+      result = "It's a draw!";
+    } else if (
+      (input == "stone" && comp == "scissors") ||
+      (input == "paper" && comp == "stone") ||
+      (input == "scissors" && comp == "paper") ||
+      (input == "reversed stone" && comp == "paper") ||
+      (input == "reversed paper" && comp == "scissors") ||
+      (input == "reversed scissors" && comp == "stone")
+    ) {
+      result = "You win! ";
+    } else {
+      result = "You lose! Bummer.";
+    }
+    return `The computer chose ${comp}.<br>
+            You chose ${input}.<br><br>
+            ${result}<br><br>
+            Now you can type "scissors" "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
+  }
+  return `Try again. <br>
+          Now you can type "scissors" "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
 };
