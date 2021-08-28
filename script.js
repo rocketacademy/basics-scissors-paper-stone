f210e63f508aa6190ad8b337195e0ab5ac81fe23
agnesia-lubuk <agnesia.lubuk@gmail.com>
2021-08-29 02:25:04 +0700

    fixing some issues

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-var main = function (input) {
-  const compRandom = () => {
-    const compRandomChoice = Math.floor(Math.random() * 3) + 1;
-    if (compRandomChoice == 1) {
-      return "stone";
-    } else if (compRandomChoice == 2) {
-      return "paper";
-    } else {
-      return "scissors";
-    }
-  };
-  comp = compRandom();
-
+const compRandom = () => {
+  const compRandomChoice = Math.floor(Math.random() * 3) + 1;
+  if (compRandomChoice == 1) {
+    return "stone";
+  } else if (compRandomChoice == 2) {
+    return "paper";
+  }
+  return "scissors";
+};
+const inputChecking = (input) => {
   if (
     input == "stone" ||
     input == "paper" ||
@@ -19,6 +16,16 @@ var main = function (input) {
     input == "reversed paper" ||
     input == "reversed scissors"
   ) {
+    return true;
+  }
+  return false;
+};
+
+const main = (input) => {
+  const comp = compRandom();
+  let result = "";
+  const validInput = inputChecking(input);
+  if (validInput) {
     if (input == comp || input == "reversed " + comp) {
       result = "It's a draw!";
     } else if (
@@ -33,11 +40,7 @@ var main = function (input) {
     } else {
       result = "You lose! Bummer.";
     }
-    return `The computer chose ${comp}.<br>
-            You chose ${input}.<br><br>
-            ${result}<br><br>
-            Now you can type "scissors" "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
+    return `The computer chose ${comp}.<br>You chose ${input}.<br><br>${result}<br><br>Now you can type "scissors" "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
   }
-  return `Try again. <br>
-          Now you can type "scissors" "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
+  return `Try again. <br>Now you can type "scissors" "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
 };
