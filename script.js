41bf221d3756d3c451d6e8b0bf99cb8cddd5c93c
Hedylyt <hedylimyt@gmail.com>
2021-08-29 20:58:34 +0800

    completed part 2 base

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+var currentGameMode = "Please key in your name.";
+var userName = "";
+var numberOfWins = 0;
+var numberOfLose = 0;
+
 var main = function (input) {
-  if (input != "Scissors" && input != "Stone" && input != "Paper") {
-    return "Invalid input. Please select either 'Scissors', 'Paper', or 'Stone'.";
-  }
-  var myOutputValue = "The program won!";
-  var gameOutput = randomSPS();
-  console.log(gameOutput);
-  if (input == gameOutput) {
-    myOutputValue = "It is a draw!";
-  }
-  if (
-    (input == "Scissors" && gameOutput == "Paper") ||
-    (input == "Paper" && gameOutput == "Stone") ||
-    (input == "Stone" && gameOutput == "Scissors")
-  ) {
-    myOutputValue = "You won!";
+  var myOutputValue = "";
+  if (currentGameMode == "Please key in your name.") {
+    userName = input;
+    currentGameMode = "dice game";
+    myOutputValue = "Hello " + userName + "!";
+  } else if (currentGameMode == "dice game") {
+    if (input != "Scissors" && input != "Stone" && input != "Paper") {
+      return "Invalid input. Please select either 'Scissors', 'Paper', or 'Stone'.";
+    }
+    var gameOutput = randomSPS();
+    console.log(gameOutput);
+    if (input == gameOutput) {
+      myOutputValue = "It is a draw!";
+    } else if (
+      (input == "Scissors" && gameOutput == "Paper") ||
+      (input == "Paper" && gameOutput == "Stone") ||
+      (input == "Stone" && gameOutput == "Scissors")
+    ) {
+      numberOfWins = numberOfWins + 1;
+      myOutputValue = "You won!";
+    } else {
+      numberOfLose = numberOfLose + 1;
+      myOutputValue = "The program won!";
+    }
+    myOutputValue = `The computer chose ${gameOutput}.<br> You chose ${input}. <br><br> ${myOutputValue}<br><br> Computer: ${numberOfLose} <br> ${userName}: ${numberOfWins}`;
   }
   return myOutputValue;
 };
-
 var randomSPS = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal);
