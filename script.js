6e8f90c15ed05de0eb6c3bbe0fe9f55f2fbbe448
lim-jiahao <lim.jiahaoo@gmail.com>
2021-08-31 01:23:07 +0800

    added user name feature

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var timesUserWon = 0;
 var timesTied = 0;
 var timesPlayed = 0;
+var curGameMode = "name";
+var userName = "";
 
 var generateRandomNum = function (range) {
   // generates random integer from 0 to range - 1, inclusive
@@ -46,45 +48,51 @@ var getGameStats = function () {
 };
 
 var main = function (input) {
-  // store possible options
-  var options = ["stone", "paper", "scissors"];
-
-  // set input to lower case first and trim leading/ending whitespace
-  var userChoice = input.toLowerCase().trim();
-
-  // create variable for the reversed word to look for, and set reversed mode as off initially
-  var reversedModeCheck = "reversed";
-  var reversedMode = false;
-
-  // check if the input starts with reversed
-  if (userChoice.startsWith(reversedModeCheck)) {
-    // set reversed mode to on and replace the "reversed" word in the input
-    reversedMode = true;
-    userChoice = userChoice.replace(reversedModeCheck, "").trim();
+  if (curGameMode == "name") {
+    userName = input;
+    curGameMode = "sps";
+    return `Hello, ${userName}!<br><br>The game is Scissors Paper Stone. Enter one of 'scissors', 'paper' or 'stone' to begin. ✌✋✊`;
   }
 
-  // check if input is valid
-  if (!options.includes(userChoice)) {
-    return `You didn't enter a valid choice. The game is Scissors Paper Stone!<br><br>
-    Don't be nervous! Enter one of 'scissors', 'paper' or 'stone'. ✌✋✊<br><br>
-    Or if you're feeling adventurous, try reverse mode by adding 'reversed' in front of your choice! (e.g. reversed scissors)`;
+  if (curGameMode == "sps") {
+    // store possible options
+    var options = ["stone", "paper", "scissors"];
+
+    // set input to lower case first and trim leading/ending whitespace
+    var userChoice = input.toLowerCase().trim();
+
+    // create variable for the reversed word to look for, and set reversed mode as off initially
+    var reversedModeCheck = "reversed";
+    var reversedMode = false;
+
+    // check if the input starts with reversed
+    if (userChoice.startsWith(reversedModeCheck)) {
+      // set reversed mode to on and replace the "reversed" word in the input
+      reversedMode = true;
+      userChoice = userChoice.replace(reversedModeCheck, "").trim();
+    }
+
+    // check if input is valid
+    if (!options.includes(userChoice)) {
+      return `You didn't enter a valid choice. The game is Scissors Paper Stone!<br><br>Don't be nervous! Enter one of 'scissors', 'paper' or 'stone'. ✌✋✊<br><br>Or if you're feeling adventurous, try reverse mode by adding 'reversed' in front of your choice! (e.g. reversed scissors)`;
+    }
+
+    // generate computer choice using random num: 0 = stone, 1 = paper, 2 = scissors
+    // get user's choice number based on the input and the corresponding index in the array
+    var computerChoiceNum = generateRandomNum(options.length);
+    var userChoiceNum = options.indexOf(userChoice);
+
+    var computerChoice = options[computerChoiceNum];
+    var output = `SCISSORS! ✌ PAPER! ✋ STONE! ✊<br><br>${userName}: I choose ${userChoice}!<br>Computer: I choose ${computerChoice}!<br><br>`;
+
+    if (reversedMode) {
+      output += "REVERSED MODE!<br><br>";
+    }
+
+    // add to output based on game outcome
+    output += playGame(reversedMode, userChoiceNum, computerChoiceNum);
+    output += " Play again?<br><br>";
+    output += getGameStats();
+    return output;
   }
-
-  // generate computer choice using random num: 0 = stone, 1 = paper, 2 = scissors
-  // get user's choice number based on the input and the corresponding index in the array
-  var computerChoiceNum = generateRandomNum(options.length);
-  var userChoiceNum = options.indexOf(userChoice);
-
-  var computerChoice = options[computerChoiceNum];
-  var output = `SCISSORS! ✌ PAPER! ✋ STONE! ✊<br><br>You chose ${userChoice}.<br>The computer chose ${computerChoice}.<br><br>`;
-
-  if (reversedMode) {
-    output += "REVERSED MODE!<br><br>";
-  }
-
-  // add to output based on game outcome
-  output += playGame(reversedMode, userChoiceNum, computerChoiceNum);
-  output += " Play again?<br><br>";
-  output += getGameStats();
-  return output;
 };
