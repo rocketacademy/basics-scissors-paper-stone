4bc87169b044fe1820cc34d642558ff8ffd89c06
VIVIENLOKE <VIVIENNLOKE@GMAIL.COM>
2021-08-30 16:49:02 +0800

    edited script to record win-loss and username

--- script.js
+++ script.js
@@ -2,72 +2,99 @@
 var getNumber = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal);
-  var number123 = randomInteger + 1;
-
-  console.log("RANDOM 1-3 FUNCTION WORKS");
-  console.log(number123);
-
-  return number123;
+  return randomInteger;
 };
 
 //PEG THE RANDOM 123 TO GET COM CHOICE OF SPS
 var pegNumToSps = function () {
   var comNum = getNumber();
-
-  if (comNum == 1) {
+  if (comNum == 0) {
     return "scissors";
   }
-  if (comNum == 2) {
+  if (comNum == 1) {
     return "paper";
   }
-  if (comNum == 3) {
+  if (comNum == 2) {
     return "stone";
   }
 };
 
+//ADDING WIN-LOSS RECORD
+var userWins = 0;
+var comWins = 0;
+var draw = 0;
+var totalPlay = 0;
+var numClicks = 0;
+
 var main = function (userAnswer) {
-  console.log("WHAT IS USER CHOICE?");
-  console.log(userAnswer);
+  console.log(`WHAT IS USER CHOICE: ${userAnswer}`);
 
+  if (numClicks == "0") {
+    numClicks = +1;
+    //CURRENTLY SHORTCUT BY NOT 'VAR'-ING userName BUT POTENTIALLY BUGGY
+    userName = userAnswer;
+    return `Hello ${userAnswer}! Let's play scissors ✂️ , paper 📄 , stone 🗿 !<br><br>Input your choice (case sensitive!) in the box above to play :D`;
+  }
+
+  console.log(`NUMBER OF CLICKS: ${numClicks}`);
   var comSps = pegNumToSps();
-
-  console.log("WHAT IS COMPUTER CHOICE?");
-  console.log(comSps);
+  console.log(`WHAT IS COMPUTER CHOICE: ${comSps}`);
 
   //CASE OF INVALID INPUT
   if (
-    userAnswer != "paper" ||
+    (numClicks != 0 && userAnswer != "paper") ||
     userAnswer != "scissors" ||
     userAnswer != "stone"
   ) {
     outputValue =
-      "Please only key in 'scissors', 'paper' or 'stone' (case sensitive)!";
+      "Input not valid! 🙅🏻‍♀️<br>Please only key in 'scissors', 'paper' or 'stone' (case sensitive)!";
   }
 
   //CASE OF A DRAW
   if (userAnswer == comSps) {
-    return `It was a draw!<br>We both picked ${comSps}!`;
+    draw += 1;
+    totalPlay += 1;
+    var percentWins = ((userWins / totalPlay) * 100).toFixed(1);
+    return `Good guess ${userName}, it was a draw! 🤷🏻‍♀️<br>We both picked ${comSps}!<br><br>You win ${userWins}/${totalPlay} times (${percentWins}%) with ${draw} draw(s)!`;
   }
   //USER CHOSE SCISSORS
   if (userAnswer == "scissors" && comSps == "stone") {
-    outputValue = "You lose!<br>Your scissors lost to my stone!";
+    comWins += 1;
+    totalPlay += 1;
+    var percentWins = ((userWins / totalPlay) * 100).toFixed(1);
+    outputValue = `You lose, try again ${userName}! 😰<br>Your scissors ✂️ lost to my stone 🗿 !<br><br>You win ${userWins}/${totalPlay} times (${percentWins}%) with ${draw} draw(s)!`;
   }
   if (userAnswer == "scissors" && comSps == "paper") {
-    outputValue = "You win!<br>Your scissors won my paper!";
+    userWins += 1;
+    totalPlay += 1;
+    var percentWins = ((userWins / totalPlay) * 100).toFixed(1);
+    outputValue = `Congratulations ${userName}, YOU WIN! 🥳<br>Your scissors ✂️ won my paper 📄 !<br><br>You win ${userWins}/${totalPlay} times (${percentWins}%) with ${draw} draw(s)!`;
   }
   //USER CHOSE STONE
   if (userAnswer == "stone" && comSps == "paper") {
-    outputValue = "You lose! Your stone lost to my paper!";
+    comWins += 1;
+    totalPlay += 1;
+    var percentWins = ((userWins / totalPlay) * 100).toFixed(1);
+    outputValue = `You lose, try again ${userName}! 😰<br>Your stone 🗿 lost to my paper 📄 !<br><br>You win ${userWins}/${totalPlay}times (${percentWins}%) with ${draw} draw(s)!`;
   }
   if (userAnswer == "stone" && comSps == "scissors") {
-    outputValue = "You win! Your stone won my scissors!";
+    userWins += 1;
+    totalPlay += 1;
+    var percentWins = ((userWins / totalPlay) * 100).toFixed(1);
+    outputValue = `Congratulations ${userName}, YOU WIN! 🥳<br>Your stone 🗿 won my scissors ✂️ !<br><br>You win ${userWins}/${totalPlay} times (${percentWins}%) with ${draw} draw(s)!`;
   }
   //USER CHOSE PAPER
   if (userAnswer == "paper" && comSps == "scissors") {
-    outputValue = "You lose! Your paper lost to my scissors!";
+    comWins += 1;
+    totalPlay += 1;
+    var percentWins = ((userWins / totalPlay) * 100).toFixed(1);
+    outputValue = `You lose, try again ${userName}! 😰<br>Your paper 📄 lost to my scissors ✂️ !<br><br>You win ${userWins}/${totalPlay} times (${percentWins}%) with ${draw} draw(s)!`;
   }
   if (userAnswer == "paper" && comSps == "stone") {
-    outputValue = "You win! Your paper won my stone!";
+    userWins += 1;
+    totalPlay += 1;
+    var percentWins = ((userWins / totalPlay) * 100).toFixed(1);
+    outputValue = `Congratulations ${userName}, YOU WIN! 🥳<br>Your paper 📄 won my stone 🗿 !<br><br>You win ${userWins}/${totalPlay} times (${percentWins}%)  with ${draw} draw(s)!`;
   }
 
   return outputValue;
