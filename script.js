0e6a8d0a37dd76cdf971d20fdcab673b125e3160
hWeitian <huangweitian1@hotmail.com>
2021-08-30 11:57:37 +0800

    refactor code

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ var calPcWinningPercent = function () {
   return pcWinPercent;
 };
 
-// ===== Function to check user input against program output to determine winner (game rules) =====
+// ===== Function to check user input against program output to determine winner =====
 
 var generateWinner = function (userInput) {
   var outputByProgram = pickOption(); // Generate random option of either scissors, paper or stone
@@ -111,10 +111,19 @@ var generateWinner = function (userInput) {
   numOfGamesPlayed = numOfGamesPlayed + 1;
   // console.log(outputByProgram);
 
+  if (gameMode == "Original Mode") {
+    myOutputValue = originalRules(userInput, outputByProgram);
+  } else if (gameMode == "Reversed Mode") {
+    myOutputValue = reversedRules(userInput, outputByProgram);
+  }
+  return myOutputValue;
+};
+
+// ===== Function to determine winner with original rules =====
+
+var originalRules = function (userInput, outputByProgram) {
   var choiceStatement = `Hey ${userName}, you chose ${userInput} and the computer chose ${outputByProgram}.`;
   var originalInstructionStatement = `Submit "scissors" "paper", "stone", "reversed scissors", "reversed paper" or "reversed stone" to play another round!<br ><br >You can also submit "reverse" to reverse the game rules.`;
-  var reversedInstructionStatement = `Submit "scissors" "paper" or "stone" to play another round!<br ><br >You can also submit "reverse" to reverse the game rules.`;
-
   if (
     userInput == outputByProgram ||
     (userInput == "reversed scissors" && outputByProgram == "scissors") ||
@@ -122,11 +131,15 @@ var generateWinner = function (userInput) {
     (userInput == "reversed stone" && outputByProgram == "stone")
   ) {
     numOfDraws = numOfDraws + 1;
-    if (gameMode == "Original Mode") {
-      myOutputValue = `${choiceStatement} <br ><br > It's a draw! 🤷<br ><br >${originalInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)`;
-    } else if (gameMode == "Reversed Mode") {
-      myOutputValue = `${choiceStatement} <br ><br > It's a draw! 🤷<br ><br >${reversedInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)`;
-    }
+    myOutputValue = `${choiceStatement} <br ><br > It's a draw! 🤷<br ><br >${originalInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)`;
+  } else if (
+    userInput == outputByProgram ||
+    (userInput == "reversed scissors" && outputByProgram == "scissors") ||
+    (userInput == "reversed paper" && outputByProgram == "paper") ||
+    (userInput == "reversed stone" && outputByProgram == "stone")
+  ) {
+    numOfDraws = numOfDraws + 1;
+    myOutputValue = `${choiceStatement} <br ><br > It's a draw! 🤷<br ><br >${originalInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)`;
   } else if (
     (userInput == "scissors" && outputByProgram == "paper") ||
     (userInput == "paper" && outputByProgram == "stone") ||
@@ -135,15 +148,9 @@ var generateWinner = function (userInput) {
     (userInput == "reversed paper" && outputByProgram == "scissors") ||
     (userInput == "reversed stone" && outputByProgram == "paper")
   ) {
-    if (gameMode == "Original Mode") {
-      numOfTimesUserWon = numOfTimesUserWon + 1;
-      var userWinningPercent = calUserWinningPercent().toFixed(0);
-      myOutputValue = `${choiceStatement} <br ><br > You won! 🙆 <br ><br >${originalInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)<br ><br >You are winning ${userWinningPercent}% of the game! Keep it up!!`;
-    } else if (gameMode == "Reversed Mode") {
-      numOfTimesPcWon = numOfTimesPcWon + 1;
-      var pcWinningPercent = calPcWinningPercent().toFixed(0);
-      myOutputValue = `${choiceStatement}<br ><br > You lose! 🙅<br ><br >${reversedInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)<br ><br >The computer is winning ${pcWinningPercent}% of the game. Try harder!`;
-    }
+    numOfTimesUserWon = numOfTimesUserWon + 1;
+    var userWinningPercent = calUserWinningPercent().toFixed(0);
+    myOutputValue = `${choiceStatement} <br ><br > You won! 🙆 <br ><br >${originalInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)<br ><br >You are winning ${userWinningPercent}% of the game! Keep it up!!`;
   } else if (
     (userInput == "scissors" && outputByProgram == "stone") ||
     (userInput == "paper" && outputByProgram == "scissors") ||
@@ -152,15 +159,38 @@ var generateWinner = function (userInput) {
     (userInput == "reversed paper" && outputByProgram == "stone") ||
     (userInput == "reversed stone" && outputByProgram == "scissors")
   ) {
-    if (gameMode == "Original Mode") {
-      numOfTimesPcWon = numOfTimesPcWon + 1;
-      var pcWinningPercent = calPcWinningPercent().toFixed(0);
-      myOutputValue = `${choiceStatement}<br ><br > You lose! 🙅<br ><br >${originalInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)<br ><br >The computer is winning ${pcWinningPercent}% of the game. Try harder!`;
-    } else if (gameMode == "Reversed Mode") {
-      numOfTimesUserWon = numOfTimesUserWon + 1;
-      var userWinningPercent = calUserWinningPercent().toFixed(0);
-      myOutputValue = `${choiceStatement} <br ><br > You won! 🙆 <br ><br >${reversedInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)<br ><br >You are winning ${userWinningPercent}% of the game! Keep it up!!`;
-    }
+    numOfTimesPcWon = numOfTimesPcWon + 1;
+    var pcWinningPercent = calPcWinningPercent().toFixed(0);
+    myOutputValue = `${choiceStatement}<br ><br > You lose! 🙅<br ><br >${originalInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)<br ><br >The computer is winning ${pcWinningPercent}% of the game. Try harder!`;
+  }
+
+  return myOutputValue;
+};
+
+// ===== Function to determine winner with reversed rules =====
+
+var reversedRules = function (userInput, outputByProgram) {
+  var choiceStatement = `Hey ${userName}, you chose ${userInput} and the computer chose ${outputByProgram}.`;
+  var reversedInstructionStatement = `Submit "scissors" "paper" or "stone" to play another round!<br ><br >You can also submit "reverse" to reverse the game rules.`;
+  if (userInput == outputByProgram) {
+    numOfDraws = numOfDraws + 1;
+    myOutputValue = `${choiceStatement} <br ><br > It's a draw! 🤷<br ><br >${reversedInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)`;
+  } else if (
+    (userInput == "scissors" && outputByProgram == "paper") ||
+    (userInput == "paper" && outputByProgram == "stone") ||
+    (userInput == "stone" && outputByProgram == "scissors")
+  ) {
+    numOfTimesPcWon = numOfTimesPcWon + 1;
+    var pcWinningPercent = calPcWinningPercent().toFixed(0);
+    myOutputValue = `${choiceStatement}<br ><br > You lose! 🙅<br ><br >${reversedInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)<br ><br >The computer is winning ${pcWinningPercent}% of the game. Try harder!`;
+  } else if (
+    (userInput == "scissors" && outputByProgram == "stone") ||
+    (userInput == "paper" && outputByProgram == "scissors") ||
+    (userInput == "stone" && outputByProgram == "paper")
+  ) {
+    numOfTimesUserWon = numOfTimesUserWon + 1;
+    var userWinningPercent = calUserWinningPercent().toFixed(0);
+    myOutputValue = `${choiceStatement} <br ><br > You won! 🙆 <br ><br >${reversedInstructionStatement}<br ><br ><u>Game Record:</u><br>You won: ${numOfTimesUserWon} round(s)<br >Computer won: ${numOfTimesPcWon} round(s)<br >Number of draws: ${numOfDraws} round(s)<br ><br >You are winning ${userWinningPercent}% of the game! Keep it up!!`;
   }
   return myOutputValue;
 };
