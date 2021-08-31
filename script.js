de6895293732d194be436694e9527031bf3265c0
penguinorigami <jane.superikigai@gmail.com>
2021-08-31 21:37:02 +0800

    finished SPS, input validation, win-loss record

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-//Create a basic version of Scissors Paper Stone where the user inputs one of "scissors", "paper", or "stone", the program internally randomly chooses scissors, paper, or stone, and the program outputs whether the user won, the program won, or it's a draw.
+//Create a basic version of Scissors Paper Stone where the user inputs one of "scissors", "paper", or "stone", the program internally randomly chooses scissors, paper, or stone, and the program outputs whether the user won, the program won, or it's a draw. Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
 
-//Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
-
+var numWin = 0
+var numLoss = 0
 var main = function (input) {
+
   console.log(input)
-  var SPSChoice = SPSChoiceGenerator()
+  var randomNumvariable = randomNumGenerator ()
+  var SPSChoice = SPSChoiceGenerator(randomNumvariable)
   var outcome = ShowHands (input, SPSChoice)
-  
+  console.log (numWin)
+  console.log (numLoss)
+
   // default response if input is not recognised.
   var myOutputValue = "Please enter either 'rock', 'paper', or 'scissors' then click Submit to play";
  
   // the program outputs whether the user won, the program won, or it's a draw.
+
+  //optional: remove if blocks from line 16 out of main body. 
   if (outcome == "draw") {
     myOutputValue = 
     "You entered " + 
     input + 
     " and the program selected " +
     SPSChoice +
-    " so it's a draw. Try again."
+    " so it's a draw. Try again. Your win-lose record is " +
+    numWin +
+    "-" +
+    numLoss;
   }
 
   if (outcome == "lose"){
@@ -26,7 +35,22 @@ var main = function (input) {
     input + 
     " and the program selected " +
     SPSChoice +
-    ", so you lose. Try again."
+    ", so you lose. Try again. Your win-lose record is " +
+    numWin +
+    "-" +
+    numLoss;
+  }
+
+    if (outcome == "win"){
+    myOutputValue =
+    "You entered " + 
+    input + 
+    " and the program selected " +
+    SPSChoice +
+    ", so you win! Your win-lose record is " +
+    numWin +
+    "-" +
+    numLoss;
   }
   
   return myOutputValue;
@@ -41,25 +65,30 @@ var randomNumGenerator = function () {
 } 
 
 // output choice: rock (default), paper, scissors 
-var SPSChoiceGenerator = function () {
+var SPSChoiceGenerator = function (randomNum) {
   var randomNum = randomNumGenerator()
   console.log ("computer chose " + randomNum) 
   
   // default SPS choice is rock
-  var SPSChoice = "rock"
+ if (randomNum == 1) {
+   SPSChoice = "rock"
+   return "rock"
+ }
 
   // if random number is 2, SPS choice (reasigned) is paper
   if (randomNum == 2) {
     SPSChoice =  "paper"
+    return "paper"
   }
   
   // if random number is 3, SPS choice (reasigned) is scissors
   if (randomNum == 3) {
     SPSChoice = "scissors"
+    return "scissors"
   }
     
   console.log ("which matches to " + SPSChoice)
-  return SPSChoice 
+
 }
 
 // HELPER FUNCTION
@@ -67,51 +96,40 @@ var SPSChoiceGenerator = function () {
 // Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
 
 var ShowHands = function (input, SPSChoice){
-if (input == "rock" && SPSChoice == "rock") {
-  myOutputValue = "it's a draw!"
+  var input
+  var SPSChoice
+if (input == SPSChoice) {
+  return "draw"
 }
 
 if (input == "rock" && SPSChoice == "paper") {
-  myOutputValue = "you lose!"
+  numLoss = numLoss + 1
+  return "lose"
 }
 
 if (input == "rock" && SPSChoice == "scissors") {
-  myOutputValue = "you win!"
+  numWin = numWin +1
+  return "win"
 }
 
 if (input == "paper" && SPSChoice == "rock") {
-  myOutputValue = "you win!"
-}
-
-if (input == "paper" && SPSChoice == "paper") {
-  myOutputValue = "it's a draw!"
+   numWin = numWin +1
+  return "win"
 }
 
 if (input == "paper" && SPSChoice == "scissors") {
-  myOutputValue = "you lose!"
+  numLoss = numLoss + 1
+  return "lose"
 }
 
 if (input == "scissors" && SPSChoice == "rock") {
-  myOutputValue = "you lose!"
+  numLoss = numLoss + 1
+  return "lose"
 }
 
 if (input == "scissors" && SPSChoice == "paper" ) {
-  myOutputValue = "you win!"
+   numWin = numWin +1
+  return "win"
 }
 
-if (input == "scissors" && SPSChoice == "scissors" ) {
-  myOutputValue = "it's a draw!"
-}
-
-
-// Determines if player won or not
-//var winorLose = function (input, SPSChoice) {
-  // Default outcome is "lose"
-  //var outcome = "lose"  
-  // If input is same as SPS choice then it's a draw
-  //if (input == SPSChoice) {
-  //  var outcome = "draw"
-  //}
-  //console.log (outcome)
- // return outcome
 }
