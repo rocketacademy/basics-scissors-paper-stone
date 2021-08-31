c3546b3f530e9009e982a998d2d196a01832b423
averygan <averygan19@gmail.com>
2021-08-31 11:56:46 +0800

    added username function, game modes and edited formatting for output message

--- script.js
+++ script.js
@@ -2,43 +2,59 @@ var computerWins = 0;
 var userWins = 0;
 //keeps track of total number of games played
 var gameCount = 0;
+//stores username
+var userName = `0`;
+var myOutputValue = ``;
+//current game mode
+var gameMode = `waiting for userName`;
 
 var main = function (input) {
-  //generate choice
-  var computerChoice = generateRandomOption();
-  //console.log(computerChoice);
-  //assign number to computer's play
-  //computerScissors = 0;
-  //computerPaper = 1;
-  //computerStone = 2;
-  var computersPlay = assignComputerChoice(computerChoice);
-  //console.log(computersPlay);
-  gameCount += 1;
+  if (gameMode == `waiting for userName`) {
+    userName = input;
+    //console.log(userName);
+    gameMode = `spsGame`;
+    //console.log(gameMode);
+    myOutputValue = `Hi ${userName}, input scissors, paper or stone to play the game!`;
+  } else if (gameMode == `spsGame`) {
+    //console.log(input);
+    //generate choice
+    var computerChoice = generateRandomOption();
+    //console.log(`computerChoice`);
+    //console.log(computerChoice);
+    //assign number to computer's play; computerScissors = 0; computerPaper = 1; computerStone = 2;
+    var computersPlay = assignComputerChoice(computerChoice);
+    //console.log(`computersPlay`);
+    //console.log(computersPlay);
 
-  //player wins, add to userWins count
-  if ((input == `scissors` && computersPlay == `paper`) || (input == `paper` && computersPlay == `stone`) || (input == `stone` && computersPlay == `scissors`)) {
-    userWins += 1;
-    var winPercentage = calculatePercentageOfUserWins(userWins, gameCount);
-    return `You win! <br><br>You chose ${input} and the computer played ${computersPlay}. <br><br>You won ${userWins} times and the computer won ${computerWins} times. You win ${winPercentage} of the game you've played!`;
-  }
+    //player wins, add to userWins count
+    if ((input == `scissors` && computersPlay == `paper`) || (input == `paper` && computersPlay == `stone`) || (input == `stone` && computersPlay == `scissors`)) {
+      userWins += 1;
+      gameCount += 1;
+      var winPercentage = calculatePercentageOfUserWins(userWins, gameCount);
+      myOutputValue = `Yay ${userName}, You win! <br><br>You chose ${input} and the computer played ${computersPlay}. <br><br>You won ${userWins} times and the computer won ${computerWins} times. ${winPercentage}`;
+    }
 
-  //player loses, add to computerWins count
-  if ((input == `scissors` && computersPlay == `stone`) || (input == `paper` && computersPlay == `scissors`) || (input == `stone` && computersPlay == `paper`)) {
-    computerWins += 1;
-    var winPercentage = calculatePercentageOfUserWins(userWins, gameCount);
-    return `You lost :( <br><br>You chose ${input} and the computer played ${computersPlay}. <br><br>You won ${userWins} times and the computer won ${computerWins} times. You win ${winPercentage} of the game you've played!`;
-  }
+    //player loses, add to computerWins count
+    else if ((input == `scissors` && computersPlay == `stone`) || (input == `paper` && computersPlay == `scissors`) || (input == `stone` && computersPlay == `paper`)) {
+      computerWins += 1;
+      gameCount += 1;
+      var winPercentage = calculatePercentageOfUserWins(userWins, gameCount);
+      myOutputValue = `Aww ${userName}, You lost :( <br><br>You chose ${input} and the computer played ${computersPlay}. <br><br>You won ${userWins} times and the computer won ${computerWins} times. ${winPercentage}`;
+    }
 
-  //tie
-  if (input == computersPlay) {
-    var winPercentage = calculatePercentageOfUserWins(userWins, gameCount);
-    return `It's a tie! <br><br>You chose ${input} and the computer played ${computersPlay} <br><br>You won ${userWins} times and the computer won ${computerWins} times. You win ${winPercentage} of the game you've played!`;
-  }
+    //tie
+    else if (input == computersPlay) {
+      gameCount += 1;
+      var winPercentage = calculatePercentageOfUserWins(userWins, gameCount);
+      myOutputValue = `${userName}, It's a tie! <br><br>You chose ${input} and the computer played ${computersPlay} <br><br>You won ${userWins} times and the computer won ${computerWins} times. ${winPercentage}`;
+    }
 
-  //player does not input scissors, paper or stone
-  else {
-    return `Please type in scissors, paper or stone to play game :)`;
+    //player does not input scissors, paper or stone
+    else {
+      myOutputValue = `Please type in scissors, paper or stone to play :)`;
+    }
   }
+  return myOutputValue;
 };
 
 //generate random number 0 to 2, to be assigned to scissors, paper and stone
@@ -57,8 +73,13 @@ var assignComputerChoice = function (computerChoice) {
 
 //function to calculate percentage of wins
 var calculatePercentageOfUserWins = function (userWins, gameCount) {
-  var winPercentage = (userWins / gameCount) * 100;
+  var winPercentage = Math.round((userWins / gameCount) * 100);
   //console.log(`winPercentage`);
   //console.log(winPercentage);
-  return `${Math.round(winPercentage)}%`;
+  if (winPercentage < 60) {
+    return `You have won ${userWins} of ${gameCount} you played - winning ${winPercentage}% of games. Don't give up!`;
+  }
+  if (winPercentage >= 60) {
+    return `You have won ${userWins} of ${gameCount} you played - winning ${winPercentage}% of games. Awesome job, game master!`;
+  }
 };
