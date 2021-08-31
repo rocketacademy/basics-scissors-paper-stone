26cbe6a97c85d57c5e6117f1bedc8d9271ced652
agnesia-lubuk <agnesia.lubuk@gmail.com>
2021-08-31 19:07:53 +0700

    SPS part II

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ const scissors = "scissors";
 const reversedStone = "reversed stone";
 const reversedPaper = "reversed paper";
 const reversedScissors = "reversed scissors";
-
+let playerScores = 0;
+let compScores = 0;
+let draws = 0;
+const userName = prompt("Please enter your name here :");
+if (userName) {
+  alert(
+    "Start playing Scissors Paper Stone by entering one of the 3 objects or reverse it by adding the word 'reversed' to your choice!"
+  );
+}
 // define a function that randomly choosesstone, paper, scissors
 const compRandom = () => {
   const compRandomChoice = Math.floor(Math.random() * 3) + 1;
@@ -44,29 +52,54 @@ const doesPlayerWin = function (player, comp) {
     (player == reversedPaper && comp == scissors) ||
     (player == reversedScissors && comp == stone)
   );
-};
+}; // return win and lost, make it as a variables
 
 // define a function to check the result
 const checkResult = (player, comp) => {
   let result = "";
   if (player == comp || player == "reversed " + comp) {
     result = "It's a draw!";
+    draws += 1;
   } else if (doesPlayerWin(player, comp)) {
+    //make this win and los a var
     result = "You won! ";
+    playerScores += 1;
   } else {
     result = "You lose! Bummer.";
+    compScores += 1;
   }
   return result;
 };
+
+const generateScoreMessage = (playerScores, compScores) => {
+  if (playerScores < compScores) {
+    return "Keep going.";
+  } else {
+    return "Pretty good!";
+  }
+};
+
+// generate object's icon
+const objectIcon = {
+  scissors: "✌🏻",
+  paper: "🤚🏻",
+  stone: "✊🏻",
+  "reversed paper": "🤚🏻",
+  "reversed scissors": "✌🏻",
+  "reversed stone": "✊🏻",
+};
+git;
 
 // play SPS with user input, return game result.
 const main = (input) => {
   const comp = compRandom();
   const player = input;
   const result = checkResult(player, comp);
+  let turns = playerScores + compScores + draws;
+  let scoreMessage = generateScoreMessage(playerScores, compScores);
 
   if (inputChecking(input)) {
-    return `The computer chose ${comp}.<br>You chose ${player}.<br><br>${result}<br><br>Now you can type "scissors", "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
+    return `The computer chose ${comp} ${objectIcon[comp]} .<br>You chose ${player} ${objectIcon[player]}.<br><br>${result}<br><br>So far ${userName}, you've been winning ${playerScores}/${turns} turns and ${draws} draws. ${scoreMessage}<br><br>Now you can type "scissors", "paper" or "stone" to play another round, or reverse it by adding the word "reversed" to your choice!`;
   }
   return `Please input 1 of "scissors", "paper", or "stone" to play the game, or reverse it by adding the word "reversed" to your choice!`;
 };
