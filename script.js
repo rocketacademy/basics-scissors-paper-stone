a56c6674d0115500e368f615a845a3809939a234
sharlearn <liewsharlen@gmail.com>
2021-08-31 23:55:59 +0800

    project part 2, base

--- script.js
+++ script.js
@@ -1,72 +1,81 @@
-//Associate random number to an output of Scissors Paper or Stone
-var scissorsPaperorStone = function () {
-  //produce random number from 1 to 3
-  var randomNumber = Math.random() * 3;
-  var randomInteger = Math.floor(randomNumber) + 1;
-  console.log("random integer", randomInteger);
+var userWins = 0; //number of times user wins
+var compWins = 0; //number of time computer wins
+var noOfPlays = 0; //number of rounds in total
 
-  //Number to scissors paper or stone
-  if (randomInteger == 1) {
+var userName = "";
+var gameStateOne = "Enter Username";
+var gameStateTwo = "SPS Game";
+var currentGameState = gameStateOne;
+
+//this is so that i can use the tab function
+var scissors = "scissors";
+var paper = "paper";
+var stone = "stone";
+
+//Random scissors paper or stone
+var scissorsPaperorStone = function () {
+  var randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber == 0) {
     return "scissors";
   }
-
-  if (randomInteger == 2) {
+  if (randomNumber == 1) {
     return "paper";
   }
-
-  if (randomInteger == 3) {
+  if (randomNumber == 2) {
     return "stone";
   }
 };
 
+//Win lose draw conditions for normal SPS
+var playerWinOrLose = function (userGuess, compGuess) {
+  if (userGuess == compGuess) {
+    return "draw";
+  }
+  if (
+    (userGuess == scissors && compGuess == paper) ||
+    (userGuess == paper && compGuess == stone) ||
+    (userGuess == stone && compGuess == scissors)
+  ) {
+    return "win";
+  }
+  if (
+    (userGuess == scissors && compGuess == stone) ||
+    (userGuess == paper && compGuess == scissors) ||
+    (userGuess == stone && compGuess == paper)
+  ) {
+    return "lose";
+  }
+};
+
 var main = function (input) {
-  //if input is anything other than scissors paper or stone, to get an invalid response
-  var myOutputValue = `Please type 'scissors', 'paper' or 'stone' to play. <br> Or add 'reversed' infront and try you luck at reversed mode.`;
+  userName = input;
+  if (userName == "" && currentGameState == gameStateOne) {
+    return `Please enter a user name to begin.`;
+  }
+
+  if ((currentGameState == gameStateOne) & (input != "")) {
+    currentGameState = gameStateTwo;
+    return `Hi ${userName}, please enter "scissors" "paper" or "stone" to play.`;
+  }
+
   var compSPS = scissorsPaperorStone();
-  console.log("scissors paper or stone", compSPS);
+  console.log(compSPS);
+  var winOrLose = playerWinOrLose(input, compSPS);
 
-  //Input does not match winning conditions
-  //Add "reversed xxx" as valid input
-  if (
-    input == "scissors" ||
-    input == "paper" ||
-    input == "stone" ||
-    input == "reversed scissors" ||
-    input == "reversed paper" ||
-    input == "reversed stone"
-  ) {
-    myOutputValue = `You chose ${input}. <br>
-    The computer chose ${compSPS}. <br> <br>
-    You lose! Try again.`;
+  if (winOrLose == "win") {
+    userWins += 1;
+    noOfPlays += 1;
+    return `You Win! <br> You chose ${input} & the computer chose ${compSPS}. <br> <br> ${userName} wins: ${userWins} over ${noOfPlays} rounds. <br> Computer wins: ${compWins} over ${noOfPlays} rounds.`;
   }
 
-  //If user inputs Scissors or reversed stone and comp = paper, user wins
-  if (
-    (input == "scissors" || input == "reversed paper") &&
-    compSPS == "paper"
-  ) {
-    myOutputValue = `You chose ${input}. <br> The computer chose ${compSPS}. <br> <br> You win, happy days.`;
+  if (winOrLose == "lose") {
+    noOfPlays += 1;
+    compWins += 1;
+    return `You lost! <br> You chose ${input} & the computer chose ${compSPS}. <br> <br> ${userName} wins: ${userWins} over ${noOfPlays} rounds. <br> Computer wins: ${compWins} over ${noOfPlays} rounds.`;
   }
 
-  //Paper and reversed scissors beats stone
-  if (
-    (input == "paper" || input == "reversed scissors") &&
-    compSPS == "stone"
-  ) {
-    myOutputValue = `You chose ${input}. <br> The Computer chose ${compSPS}, you win, happy days.`;
+  if (winOrLose == "draw") {
+    noOfPlays += 1;
+    return `Both You and the Computer chose ${compSPS}. <br> <br> It is a draw. <br> ${userName} wins: ${userWins} over ${noOfPlays} rounds. <br> Computer wins: ${compWins} over ${noOfPlays} rounds.`;
   }
-  //Stone and reversed paper beats scissors
-  if (
-    (input == "stone" || input == "reversed paper") &&
-    compSPS == "scissors"
-  ) {
-    myOutputValue = `You chose ${input}. <br> The Computer chose ${compSPS}, you win, happy days.`;
-  }
-
-  //if same object, draw
-  if (input == compSPS || input == "reversed " + compSPS) {
-    myOutputValue = `Both You and the Computer chose ${compSPS}. <br> <br> It is a draw.`;
-  }
-
-  return myOutputValue;
 };
