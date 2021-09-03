48bb2a87b770229d91a818b8d700668b4cceefb5
hell0kj <hoh_kia_jin@hotmail.com>
2021-09-03 21:33:51 +0800

    Lets Play SPS part 2

--- script.js
+++ script.js
@@ -8,17 +8,26 @@ var currentGameMode = "Waiting for User Name";
 var main = function (input) {
   var myOutputValue = "";
   if (currentGameMode == "Waiting for User Name") {
-    userName = input; // allow user to input userName
-    currentGameMode = "diceGame"; // toggle game mode to play game
-    myOutputValue = "Hello " + userName + ". Lets Play!";
+    // allow user to input userName
+    userName = input;
+    // toggle game mode to play game
+    currentGameMode = "Undecided";
+    myOutputValue =
+      "Hello " + userName + ". Lets Play SPS, Reverse SPS or Computer!";
     return myOutputValue;
-  } else if (currentGameMode == "diceGame") {
-    if (input != "Scissors" || input != "Paper" || input != "Stone") {
-      // Validating the input
-      var myOutputValue = "Only Scissors, Paper or Stone! <br> Try again";
-      var computerchose = computermove();
-      totalgames = totalgames + 1;
+  }
+  if (currentGameMode == "Undecided") {
+    currentGameMode = input;
+    myOutputValue = "Welcome, " + userName + ". Lets play " + input;
+    return myOutputValue;
+  }
+
+  if (currentGameMode == "SPS") {
+    if (input != "Scissors" && input != "Paper" && input != "Stone") {
+      return "Only Scissors, Paper or Stone! <br> Try again";
     }
+    var computerchose = computermove();
+    totalgames = totalgames + 1;
     // Tied Grouping
     if (input == computerchose) {
       tiedgames = tiedgames + 1;
@@ -60,6 +69,97 @@ var main = function (input) {
         ". <br> You Lose :(";
     }
   }
+  if (currentGameMode == "Reverse SPS") {
+    if (input != "Scissors" && input != "Paper" && input != "Stone") {
+      return "Only Scissors, Paper or Stone! <br> Try again";
+    }
+    var computerchose = computermove();
+    totalgames = totalgames + 1;
+
+    // Tied Grouping
+    if (input == computerchose) {
+      tiedgames = tiedgames + 1;
+      myOutputValue =
+        "You used " +
+        input +
+        " and Computer used " +
+        computerchose +
+        ". <br> Its a tie!";
+    }
+
+    // Lose Grouping
+    if (
+      (input == "Scissors" && computerchose == "Paper") ||
+      (input == "Paper" && computerchose == "Stone") ||
+      (input == "Stone" && computerchose == "Scissors")
+    ) {
+      computerwins = computerwins + 1;
+      myOutputValue =
+        "You used " +
+        input +
+        " and Computer used " +
+        computerchose +
+        ". <br> You Lose :(";
+    }
+
+    // Win Grouping
+    if (
+      (input == "Scissors" && computerchose == "Stone") ||
+      (input == "Paper" && computerchose == "Scissors") ||
+      (input == "Stone" && computerchose == "Paper")
+    ) {
+      userwins = userwins + 1;
+      myOutputValue =
+        "You used " +
+        input +
+        " and Computer used " +
+        computerchose +
+        ". <br> You WIN!!!";
+    }
+  }
+  // Computer vs Computer game mode
+  if (currentGameMode == "Computer") {
+    // Generate 2 computer output
+    var computerAchose = computermove();
+    var computerBchose = computermove();
+    // compare 2 computer output
+    //Tied Grouping
+    if (computerAchose == computerBchose) {
+      myOutputValue =
+        "Computer A used " +
+        computerAchose +
+        " and Computer B used " +
+        computerBchose +
+        ".<br> Its a tie!";
+    }
+    // Computer A wins
+    if (
+      (computerAchose == "Scissors" && computerBchose == "Paper") ||
+      (computerAchose == "Paper" && computerBchose == "Stone") ||
+      (computerAchose == "Stone" && computerBchose == "Scissors")
+    ) {
+      myOutputValue =
+        "Computer A used " +
+        computerAchose +
+        "and Computer B used " +
+        computerBchose +
+        ". <br> Computer A won!";
+    }
+    //Computer B wins
+    if (
+      (computerAchose == "Scissors" && computerBchose == "Stone") ||
+      (computerAchose == "Paper" && computerBchose == "Scissors") ||
+      (computerAchose == "Stone" && computerBchose == "Paper")
+    ) {
+      myOutputValue =
+        "Computer A used " +
+        computerAchose +
+        " and Computer B used " +
+        computerBchose +
+        ". <br> Computer B won!";
+    }
+  }
+
   userWinPercentage = Math.floor((userwins / totalgames) * 100);
   tiedGamesPercentage = Math.floor((tiedgames / totalgames) * 100);
   computerWinPercentage = Math.floor((computerwins / totalgames) * 100);
