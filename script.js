1ce41f15e4b0d3abe58ccc7d55c70036a32f2c16
kocs <kenneth.ongcs@gmail.com>
2021-10-10 14:50:49 +0800

    added random number generator & computer selection

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
+'use strict';
+
+const inputField = document.getElementById('input-field');
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = input;
   return myOutputValue;
 };
+
+// add 'enter' function to input-field
+inputField.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    document.getElementById('submit-button').click();
+  }
+});
+
+// random number generator
+const randomNumberGen = function () {
+  return Math.trunc(Math.random() * 3);
+};
+
+// computer's selection
+const computerSelect = function () {
+  let randomNumber = randomNumberGen();
+  switch (randomNumber) {
+    case 0:
+      return 'scissors ✂';
+      break;
+    case 1:
+      return 'paper 🧻';
+      break;
+    case 2:
+      return 'stone 🥌';
+      break;
+  }
+};
+// console.log(computerSelect());
+
+// get player choice
+
+// game code
