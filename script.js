34d22680c8b41a9bf996c982abf67b518acfa27c
kennethongcs <kenneth.ongcs@gmail.com>
2021-10-10 16:14:18 +0800

    added full functionality for game into script

--- script.js
+++ script.js
@@ -1,56 +1,106 @@
 'use strict';
 
-const inputField = document.getElementById('input-field');
+// const main = function (input) {
+//   let myOutputValue = input;
+//   return myOutputValue;
+// };
 
-var main = function (input) {
-  var myOutputValue = input;
-  return myOutputValue;
-};
+////////////////////
 
-var button = document.querySelector('#submit-button');
-button.addEventListener('click', function () {
-  // Set result to input value
-  var input = document.querySelector('#input-field');
-  var result = main(input.value);
+// game variables
+let playerChoice = '';
+let computerChoice = '';
 
-  // Display result in output element
-  var output = document.querySelector('#output-div');
-
-  output.textContent = result;
-
-  // Reset input value
-  input.value = '';
-});
+// elements
+const inputFieldEl = document.getElementById('input-field');
+const button = document.querySelector('#submit-button');
 
 // add 'enter' function to input-field
-inputField.addEventListener('keydown', function (e) {
+inputFieldEl.addEventListener('keydown', function (e) {
   if (e.key === 'Enter') {
     document.getElementById('submit-button').click();
   }
 });
+
+////////////////////
+
+// GAME FUNCTIONALITY
 
 // random number generator
 const randomNumberGen = function () {
   return Math.trunc(Math.random() * 3);
 };
 
-// computer's selection
-const computerSelect = function () {
+// get player choice
+button.addEventListener('click', function () {
+  // Set result to input value
+  let input = document.querySelector('#input-field');
+  playerChoice = input.value;
+  // Display result in output element
+  let output = document.querySelector('#output-div');
+  // ensures output = result
+  output.textContent = playerChoice;
+  // Reset input value
+  input.value = '';
+  // get computer choice
+  computerChoice = getComputerChoice();
+  // get player choice
+  let choice = playerChoice.toLowerCase();
+
+  // play game
+  if (choice === 'scissors' || choice === 'paper' || choice === 'stone') {
+    // determine winner
+    output.textContent = `You chose ${playerChoice} and the computer chose ${computerChoice}. ${determineWinner(
+      computerChoice,
+      playerChoice
+    )}`;
+  } else {
+    alert('⚠ please input correctly');
+    output.textContent = '';
+  }
+});
+
+// get computer choice
+const getComputerChoice = function () {
   let randomNumber = randomNumberGen();
   switch (randomNumber) {
     case 0:
-      return 'scissors ✂';
+      return 'scissors';
       break;
     case 1:
-      return 'paper 🧻';
+      return 'paper';
       break;
     case 2:
-      return 'stone 🥌';
+      return 'stone';
       break;
   }
 };
 // console.log(computerSelect());
 
-// get player choice
-
-// game code
+// game logic
+const determineWinner = function (computerChoice, playerChoice) {
+  if (computerChoice === playerChoice) {
+    return 'The game is a draw, try again! 💪';
+  }
+  if (playerChoice === 'stone') {
+    if (computerChoice === 'paper') {
+      return 'The computer won! 😢';
+    } else {
+      return 'You won! 🎉🍾';
+    }
+  }
+  if (playerChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'The computer won! 😢';
+    } else {
+      return 'You won! 🎉🍾';
+    }
+  }
+  if (playerChoice === 'scissors') {
+    if (computerChoice === 'stone') {
+      return 'The computer won! 😢';
+    } else {
+      return 'You won! 🎉🍾';
+    }
+  }
+};
