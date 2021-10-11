4e8e16590943d5355167c3a681d8870997c0bd9a
tabbiho <tabbiho@gmail.com>
2021-10-11 17:06:49 +0800

    Basic Scissors Paper Stone completed

--- script.js
+++ script.js
@@ -1,4 +1,68 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  input = input.toLowerCase();
+  // Variable to capture opponent's random pick (scissors, paper or stone)
+  var opponentPick = choicePick(randomOpponentNum());
+
+  if (input == 'scissors' || input == 'paper' || input == 'stone'){
+    // Draw condition
+    if (input == opponentPick){
+      var score = `It's a draw!`
+    };
+
+    //Scissors
+    if (input == 'scissors'){
+    if (opponentPick == 'paper'){
+        var score = `You won!`
+      };
+    if (opponentPick == 'stone'){
+        var score = `You lost!`
+      }
+    };
+
+    //Paper
+    if (input == 'paper'){
+    if (opponentPick == 'stone'){
+        var score = `You won!`
+      };
+    if (opponentPick == 'scissors'){
+        var score = `You lost!`
+      }
+    };
+
+    //Stone
+    if (input == 'stone'){
+    if (opponentPick == 'scissors'){
+        var score = `You won!`
+      };
+    if (opponentPick == 'paper'){
+        var score = `You lost!`
+      }
+    };
+
+    var myOutputValue = `${score}`;
+    myOutputValue += `<br>You picked ${input}, while your opponent picked ${opponentPick}.`;
+  };
+  myOutputValue += `<br><br>Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.`;
+return myOutputValue;
 };
+
+// Function: Returns random integer (0, 1 or 2)
+var randomOpponentNum = function(){
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal);
+  return randomInteger;
+  };
+
+// Function: Returns pick from number input (scissors, paper or stone)
+var choicePick = function(num){
+  if (num == 0){
+    choice = 'scissors';
+  };
+  if (num == 1){
+    choice = 'paper';
+  };
+  if (num == 2){
+    choice = 'stone';
+  };
+  return choice;
+  };
