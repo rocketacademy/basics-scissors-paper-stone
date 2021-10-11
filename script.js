424fd4fc98a464ec6a06181b179975b813393b3b
kennethongcs <kenneth.ongcs@gmail.com>
2021-10-11 15:57:13 +0800

    refactored playGame()

--- script.js
+++ script.js
@@ -10,10 +10,14 @@
 // game variables
 let playerChoice = '';
 let computerChoice = '';
+let state = 0;
 
-// elements
+// DOM elements
 const inputFieldEl = document.getElementById('input-field');
-const button = document.querySelector('#submit-button');
+const submitButtonEl = document.querySelector('#submit-button');
+const reverseButtonEl = document.getElementById('btn-reverse');
+const output = document.querySelector('#output-div');
+const input = document.querySelector('#input-field');
 
 // add 'enter' function to input-field
 inputFieldEl.addEventListener('keydown', function (e) {
@@ -31,33 +35,38 @@ const randomNumberGen = function () {
   return Math.trunc(Math.random() * 3);
 };
 
+// play game function
+const playGame = function (choice, computerChoice) {
+  if (choice === 'scissors' || choice === 'paper' || choice === 'stone') {
+    // determine winner
+    output.textContent = `You chose ${choice} and the computer chose ${computerChoice}. ${determineWinner(
+      computerChoice,
+      choice
+    )}`;
+  } else {
+    alert('⚠ please input correctly');
+    output.textContent = '';
+  }
+};
 // get player choice
-button.addEventListener('click', function () {
+
+submitButtonEl.addEventListener('click', function () {
   // Set result to input value
-  let input = document.querySelector('#input-field');
+  // add below in a function TODO
+
+  // start of function ----->
   playerChoice = input.value;
   // Display result in output element
-  let output = document.querySelector('#output-div');
-  // ensures output = result
-  output.textContent = playerChoice;
   // Reset input value
   input.value = '';
   // get computer choice
   computerChoice = getComputerChoice();
   // get player choice
   let choice = playerChoice.toLowerCase();
+  //end of function <-----
 
   // play game
-  if (choice === 'scissors' || choice === 'paper' || choice === 'stone') {
-    // determine winner
-    output.textContent = `You chose ${playerChoice} and the computer chose ${computerChoice}. ${determineWinner(
-      computerChoice,
-      playerChoice
-    )}`;
-  } else {
-    alert('⚠ please input correctly');
-    output.textContent = '';
-  }
+  playGame(choice, computerChoice);
 });
 
 // get computer choice
@@ -77,7 +86,7 @@ const getComputerChoice = function () {
 };
 // console.log(computerSelect());
 
-// game logic
+// game logic normal
 const determineWinner = function (computerChoice, playerChoice) {
   if (computerChoice === playerChoice) {
     return 'The game is a draw, try again! 💪';
@@ -101,6 +110,34 @@ const determineWinner = function (computerChoice, playerChoice) {
       return 'The computer won! 😢';
     } else {
       return 'You won! 🎉🍾';
+    }
+  }
+};
+
+// game logic reversed
+const determineWinnerReverse = function (computerChoice, playerChoice) {
+  if (computerChoice === playerChoice) {
+    return 'The game is a draw, try again! 💪';
+  }
+  if (playerChoice === 'stone') {
+    if (computerChoice === 'paper') {
+      return 'You won! 🎉🍾';
+    } else {
+      return 'The computer won! 😢';
+    }
+  }
+  if (playerChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'You won! 🎉🍾';
+    } else {
+      return 'The computer won! 😢';
+    }
+  }
+  if (playerChoice === 'scissors') {
+    if (computerChoice === 'stone') {
+      return 'You won! 🎉🍾';
+    } else {
+      return 'The computer won! 😢';
     }
   }
 };
