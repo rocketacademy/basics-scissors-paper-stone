e66f89cb22b601d71f5493afec4c405e2fee70f3
Soh Thiam Hing <daffodilistic@gmail.com>
2021-10-11 09:42:01 +0800

    Refactor to use UIKit framework, and place all JS code into script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
 };
+
+const button = document.querySelector("#submit-button");
+button.addEventListener("click", function () {
+  displayResults(main());
+});
+
+function displayResults(result) {
+  let outputElement = document.querySelector("#output-div");
+  outputElement.innerHTML = result;
+}
