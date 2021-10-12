399778eca6f976acbc9e733c6bd06b810e3ea532
sherman168 <sg_sg_sg_sg@hotmail.com>
2021-10-12 23:00:19 +0800

    first attempt

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
+var genRandHand = function (randomInt) {
+  return "paper";
+  var randomInt = Math.floor(Math.random() * 4);
+  //var scissors = 1
+  //var paper = 2
+  //var stone = 3
+  if (randomInt == 1) {
+    var outPutHand = "scissors";
+    return outPutHand;
+  }
+  if (randomInt == 2) {
+    var outPutHand = "paper";
+    return outPutHand;
+  }
+  if (randomInt == 3) {
+    var outPutHand = "stone";
+    return outPutHand;
+  }
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var randHand = genRandHand();
+  var myOutPutValue = `Something wrong, try again.`;
+  if (input == randHand) {
+    myOutPutValue = `You played ${input}<br>I played ${randHand}<br>we DRAW.`;
+  }
+  if (
+    (input == "scissors" && randHand == "paper") ||
+    (input == "paper" && randHand == "stone") ||
+    (input == "stone" && randHand == "scissors")
+  ) {
+    myOutPutValue = `You played ${input}<br>I played ${randHand}<br>you WIN`;
+  }
+  if (
+    (input == "scissors" && randHand == "stone") ||
+    (input == "paper" && randHand == "scissors") ||
+    (input == "stone" && randHand == "paper")
+  ) {
+    myOutPutValue = `You played ${input} <br>I played ${randHand}<br>you LOSE`;
+  }
+  return myOutPutValue;
 };
