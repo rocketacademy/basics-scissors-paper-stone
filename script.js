73381861a7caaf5ef2bbfebf376f9a384bd71d31
DLCT13 <daryllct13@gmail.com>
2021-10-14 02:05:24 +0800

    did base game but error undefined var so upload first

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+var main = function (userInputChoice) {
+  var compFinalChoice = progChoice();
+  var gameOutcome = "Please choose Rock or Scissors or Paper and try again!";
+  if (userInputChoice == compChoice) {
+    gameOutcome = "The game ends in a draw";
+  }
+  if (
+    (userInputChoice == "Rock" && compChoice == "Scissors") ||
+    (userInputChoice == "Scissors" && compChoice == "Paper") ||
+    (userInputChoice == "Paper" && compChoice == "Rock")
+  ) {
+    gameOutcome = "You win the game!";
+  }
+  if (
+    (userInputChoice == "Paper" && compChoice == "Scissors") ||
+    (userInputChoice == "Scissors" && compChoice == "Rock") ||
+    (userInputChoice == "Rock" && compChoice == "Paper")
+  ) {
+    gameOutcome = "You lose the game";
+  }
+  return gameOutcome;
 };
+
+//Computer inputs choices
+var progChoice = function () {
+  var randomNumber = Math.random() * 3;
+  console.log(randomNumber);
+  var randomNumberRounded = Math.floor(randomNumber);
+  console.log(randomNumberRounded);
+  if (randomNumberRounded == 0) {
+    var compChoice = "Rock";
+    console.log(compChoice);
+  }
+  if (randomNumberRounded == 1) {
+    var compChoice = "Scissors";
+    console.log(compChoice);
+  }
+  if (randomNumberRounded == 2) {
+    var compChoice = "Paper";
+    console.log(compChoice);
+  }
+  return compChoice;
+};
+
+// Scissors beats paper, paper beats stone, and stone beats scissors
+// User win , lose or drwa
+//
