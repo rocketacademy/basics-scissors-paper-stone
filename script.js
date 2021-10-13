51104c1149d36b271c7d9f575b5da50da2554fc2
batrashivani <bshivani@gmail.com>
2021-10-13 14:29:15 +0800

    changed logic

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 var main = function (input) {
-  var myOutputValue = 'hellos world';
+  var myOutputValue = getResult(input);
   return myOutputValue;
 };
+
+var getResult = function(input){
+  console.log ('you have input ' + input);
+  var arrayItems = ['stone','paper','scissors'];
+  var random = Math.floor(Math.random()*3);
+  var value = arrayItems[random];
+  console.log ('system has chosen ' + value);
+  if (input==value) {
+    return ('system choose ' + value  +' its a Draw');
+  }
+  if ((input=='scissors') && (value=='paper')){
+    return 'system choose ' + value + ' you won';
+  }
+  if ((input=='paper') && (value=='stone')){
+    return 'system choose ' + value + ' you won';
+  }
+  if ((input=='stone') && (value=='scissors')){
+    return 'system choose ' + value + ' you won';
+  }
+  
+  return 'system choose ' + value + ' you lose';
+}
