8ff1c4047e3bd0cb0ac102762d4092fbc5a28eba
jun <junjie@keijet.com>
2021-10-14 18:47:37 +0800

    Implement SPS

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  const scissors = "scissors";
+  const paper = "paper";
+  const stone = "stone";
+
+  
+  if (input != scissors && input != paper && input != stone) {
+    return `You keyed in an invalid option; choose from '${scissors}', '${paper}' or '${stone}'.`;
+  }
+
+  var roll = rollDice();
+  let guess;
+
+  if (roll == 0) {
+    guess = scissors
+  } 
+  if (roll == 1) {
+    guess = paper
+  }
+  if (roll == 2) {
+    guess = stone
+  }
+
+  if (input == guess) {
+    return `The computer chose ${guess}, you chose ${input}. You win!`
+  } else {
+    return `The computer chose ${guess}, you chose ${input}. You lose!`
+  }
+  
+};
+
+var rollDice = function () {  
+  // produces a float between 0 and 3
+  var randomFloat = Math.random() * 3;  
+  // take off the decimal  
+  var resultInteger = Math.floor(randomFloat);  
+  return resultInteger;
 };
