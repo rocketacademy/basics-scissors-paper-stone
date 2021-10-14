d625e6cbab5ae2742e98fbf81600ac71dbb3f2a2
hst2709 <trishst27@gmail.com>
2021-10-14 19:59:03 +0800

    wrote code for scissors paper stone

--- script.js
+++ script.js
@@ -1,4 +1,62 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  myOutputValue =
+    "You can only choose scissors, paper, or stone. Please try again!";
+  var computerHand = randomHandSign();
+  if (
+    (input == "scissors" && computerHand == "stone") ||
+    (input == "paper" && computerHand == "scissors") ||
+    (input == "stone" && computerHand == "paper")
+  ) {
+    myOutputValue =
+      "You lose! You chose " +
+      input +
+      " and the computer chose " +
+      computerHand +
+      "<br><br> Type 'scissors', 'paper', or 'stone' to play another round!";
+  }
+  if (
+    (input == "scissors" && computerHand == "paper") ||
+    (input == "paper" && computerHand == "stone") ||
+    (input == "stone" && computerHand == "scissors")
+  ) {
+    myOutputValue =
+      "You win! You chose " +
+      input +
+      " and the computer chose " +
+      computerHand +
+      "<br><br> Type 'scissors', 'paper', or 'stone' to play another round!";
+  }
+  if (
+    (input == "scissors" && computerHand == "scissors") ||
+    (input == "paper" && computerHand == "paper") ||
+    (input == "stone" && computerHand == "stone")
+  ) {
+    myOutputValue =
+      "It's a draw! You chose " +
+      input +
+      " and the computer chose " +
+      computerHand +
+      "<br><br> Type 'scissors', 'paper', or 'stone' to play another round!";
+  }
   return myOutputValue;
+};
+
+var generateRandomInteger = function (max) {
+  var randomDecimal = Math.random() * max;
+  var randomInteger = Math.floor(randomDecimal) + 1;
+  return randomInteger;
+};
+
+var randomHandSign = function () {
+  var numHandSigns = 3;
+  var randomNum = generateRandomInteger(numHandSigns);
+  if (randomNum == 1) {
+    return "scissors";
+  }
+  if (randomNum == 2) {
+    return "paper";
+  }
+  if (randomNum == 3) {
+    return "stone";
+  }
 };
