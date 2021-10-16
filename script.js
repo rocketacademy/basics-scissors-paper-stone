1374e20dda767bb312fc0af03b72197b727b7c57
wmlam123 <wmlam@msn.com>
2021-10-16 22:24:34 +0800

    Submission 1 for Basics Project 1 Part 2

--- script.js
+++ script.js
@@ -5,43 +5,81 @@ var stone = "stone";
 var reversed_scissors = "reversed scissors";
 var reversed_paper = "reversed paper";
 var reversed_stone = "reversed stone";
+var totalPlayCount = 0;
+var totalDrawCount = 0;
+var userWinCount = 0;
+var computerWinCount = 0;
+var playerName = "";
 
 // Main function to execute the output to browser
-var main = function (input) {  
+var main = function (input) {
+  if (!playerName) {       
+   
+    if (input == "") {         
+      return "Please enter your name to continue."  
+    }  
+  
+    playerName = input; 
+    console.log(playerName);
+    return `Hello ${playerName}, you can start game by entering "scissors", "paper" & "stone".`         
+  }    
+  
+  return playSPS (input);    
+};
+
+// Play SPS function
+var playSPS = function (playerInput) {  
   var randomComputerSPS = numberToSPS ();
   console.log (randomComputerSPS); // For debugging on output value of randomComputerSPS
-  var getIcon = objectIcon (input);
+  var getIcon = objectIcon (playerInput);
   console.log (getIcon); // For debugging on output value of getIcon
   var getComputerIcon = objectIcon (randomComputerSPS);
   console.log (getComputerIcon); // For debugging on output value of getComputerIcon
-
+  
   // Check and perform input validation
-  if (input != scissors && input != reversed_scissors &&
-    input != paper && input != reversed_paper &&
-    input != stone && input != reversed_stone) {
+  if (playerInput != scissors && playerInput != reversed_scissors &&
+    playerInput != paper && playerInput != reversed_paper &&
+    playerInput != stone && playerInput != reversed_stone) {
       var myOutputValue = 'Please enter "scissors", "paper" or "stone" to start.'
   }
-  
+    
   // Check input condition for draw
-  else if (conditionForDraw (input,randomComputerSPS)) {
-    myOutputValue = `The computer chose ${randomComputerSPS} ${getComputerIcon}, you chose ${input} ${getIcon}, it is a draw.`
+  else if (conditionForDraw (playerInput,randomComputerSPS)) {
+    totalPlayCount += 1;
+    totalDrawCount += 1;
+    myOutputValue = `The computer chose ${randomComputerSPS} ${getComputerIcon}, you chose ${playerInput} ${getIcon}, it is a draw.`
   }
   
   // Check input condition for win
-  else if (conditionForWin (input,randomComputerSPS)) {
-    myOutputValue = `The computer chose ${randomComputerSPS} ${getComputerIcon}, you chose ${input} ${getIcon}, you win!.`
+  else if (conditionForWin (playerInput,randomComputerSPS)) {
+    totalPlayCount += 1;
+    userWinCount += 1;
+    myOutputValue = `The computer chose ${randomComputerSPS} ${getComputerIcon}, you chose ${playerInput} ${getIcon}, you win!.`
   }
   
   // All other non-defined conditions will lose
   else {
+    totalPlayCount += 1;
+    computerWinCount += 1;
     myOutputValue = `The computer chose ${randomComputerSPS} ${getComputerIcon}.<br><br> 
-    You chose ${input} ${getIcon}.<br><br>
+    You chose ${playerInput} ${getIcon}.<br><br>
     You lose! Bummer.<br><br>
     Now you can type "scissors" "paper" or "stone" to play another round!`
   }
+  
+  var winPercentageComputer = computerWinCount/totalPlayCount * 100;
+  winPercentageComputer = winPercentageComputer.toFixed(0);
+  var winPercentageUser = userWinCount/totalPlayCount * 100;
+  winPercentageUser = winPercentageUser.toFixed(0);
 
-  return myOutputValue;
-};
+  var callUserName = `Hello ${playerName}!<br><br>`
+  var stdMessage = `<br><br>Summary: Total play count is ${totalPlayCount}, 
+  total draw count is ${totalDrawCount},
+  computer win count is ${computerWinCount}, percentage at ${winPercentageComputer}%, 
+  your win count is ${userWinCount}, percentage at ${winPercentageUser}%.`
+
+  return callUserName + myOutputValue + stdMessage;
+}
 
 // Function to check draw condition for SPS between Player and Computer
 var conditionForDraw = function (player, computer) {
