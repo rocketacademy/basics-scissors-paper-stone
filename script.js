d790b2b6c11c97608416dff232a186b7764c3936
davemeng <davejmeng@gmail.com>
2021-10-16 15:17:02 +0800

    Revert "secret word"
    This reverts commit 66405357438d0797f752f91c85613ca0edea4f17.

--- script.js
+++ script.js
@@ -1,79 +1,191 @@
-var winRecord = 0;
+// What is the input going to be? Stone or Paper or Scissors
+// What should the output be? You played {input} and the computer played {stone/paper/scissors} so you {won/lost/drew}
+// How will the computer's option be randomly generated? Using math.random to generate a number between 0-2
+// How many different cases are there? 3 (win, lose, draw)
+
 var main = function (input) {
   // Generate a random dice number
   var randomDiceNumber = rollDice();
   // Assign a play to each dice number
   if (randomDiceNumber == 0) {
-    var computerTurn = "banana";
-    console.log("computer played 0 - banana");
+    var computerTurn = "stone";
+    console.log("computer played 0 - stone");
     console.log(randomDiceNumber == 0);
   }
 
   if (randomDiceNumber == 1) {
-    console.log("computer played 1 - chiesel");
+    console.log("computer played 1 - paper");
     console.log(randomDiceNumber == 1);
-    var computerTurn = "chiesel";
+    var computerTurn = "paper";
   }
 
   if (randomDiceNumber == 2) {
-    console.log("computer played 2 - faucet");
+    console.log("computer played 2 - scissors");
     console.log(randomDiceNumber == 2);
-    var computerTurn = "faucet";
+    var computerTurn = "scissors";
   }
 
   // Assign an outcome based on each input
   // Input Validation
-  if (input != "chiesel" && input != "banana" && input != "faucet") {
+  if (
+    input != "stone" &&
+    input != "paper" &&
+    input != "scissors" &&
+    input != "reversed stone" &&
+    input != "reversed paper" &&
+    input != "reversed scissors"
+  ) {
     myOutputValue =
       "You played " +
       input +
-      " which is invalid, please only play chiesel, banana or faucet.";
+      " which is invalid, please only play stone, paper or scissors.<br> You can also try playing reversed stone or reversed paper or reversed scissors.";
     // Return output.
     console.log("input validation: ", input);
     return myOutputValue;
   }
 
-  // If user guesses incorrect
-  if (input != computerTurn) {
+  // stone
+  // stone vs Scissors (win) / reversed stone vs paper (win)
+  if (
+    (input == "stone" && computerTurn == "scissors") ||
+    (input == "reversed stone" && computerTurn == "paper")
+  ) {
     myOutputValue =
       "You played " +
       input +
       " and the computer played " +
       computerTurn +
-      " so you lose and your win record is " +
-      winRecord;
+      " so you win";
     // Return output.
     console.log("you played ", input, " and computer played ", computerTurn);
     return myOutputValue;
   }
 
-  // If user guesses correctly twice
-  if (input == computerTurn && winRecord == 1) {
-    winRecord = winRecord + 1;
+  // stone vs stone (draw)
+  if (
+    (input == "stone" || input == "reversed stone") &&
+    computerTurn == "stone"
+  ) {
     myOutputValue =
       "You played " +
       input +
       " and the computer played " +
       computerTurn +
-      " so you win because your winRecord is " +
-      winRecord;
+      " so you drew";
     // Return output.
     console.log("you played ", input, " and computer played ", computerTurn);
-    winRecord = winRecord - 2;
     return myOutputValue;
   }
 
-  // If user guesses correctly once
-  if (input == computerTurn && winRecord == 0) {
-    winRecord = winRecord + 1;
+  // stone vs Paper (lose) / reversed stone vs scissors (lose)
+  if (
+    (input == "stone" && computerTurn == "paper") ||
+    (input == "reversed stone" && computerTurn == "scissors")
+  ) {
     myOutputValue =
       "You played " +
       input +
       " and the computer played " +
       computerTurn +
-      " and you need " +
-      (2 - winRecord) +
-      " more correct guess to win";
+      " so you lose";
+    // Return output.
+    console.log("you played ", input, " and computer played ", computerTurn);
+    return myOutputValue;
+  }
+
+  // Paper
+  // Paper vs Scissors (lose) / reversed paper vs stone (lose)
+  if (
+    (input == "paper" && computerTurn == "scissors") ||
+    (input == "reversed paper" && computerTurn == "stone")
+  ) {
+    myOutputValue =
+      "You played " +
+      input +
+      " and the computer played " +
+      computerTurn +
+      " so you lose";
+    // Return output.
+    console.log("you played ", input, " and computer played ", computerTurn);
+    return myOutputValue;
+  }
+
+  // Paper vs stone (win) / reversed paper vs scissors (win)
+  if (
+    (input == "paper" && computerTurn == "stone") ||
+    (input == "reversed paper" && computerTurn == "scissors")
+  ) {
+    myOutputValue =
+      "You played " +
+      input +
+      " and the computer played " +
+      computerTurn +
+      " so you win";
+    // Return output.
+    console.log("you played ", input, " and computer played ", computerTurn);
+    return myOutputValue;
+  }
+
+  // Paper vs paper (draw)
+  if (
+    (input == "paper" || input == "reversed paper") &&
+    computerTurn == "paper"
+  ) {
+    myOutputValue =
+      "You played " +
+      input +
+      " and the computer played " +
+      computerTurn +
+      " so you drew";
+    // Return output.
+    console.log("you played ", input, " and computer played ", computerTurn);
+    return myOutputValue;
+  }
+
+  // Scissors
+  // Scissors vs Scissors (draw)
+  if (
+    (input == "scissors" || input == "reversed scissors") &&
+    computerTurn == "scissors"
+  ) {
+    myOutputValue =
+      "You played " +
+      input +
+      " and the computer played " +
+      computerTurn +
+      " so you drew";
+    // Return output.
+    console.log("you played ", input, " and computer played ", computerTurn);
+    return myOutputValue;
+  }
+
+  // Scissors vs stone (lose) or reversed scissors vs paper (lose)
+  if (
+    (input == "scissors" && computerTurn == "stone") ||
+    (input == "reversed scissors" && computerTurn == "paper")
+  ) {
+    myOutputValue =
+      "You played " +
+      input +
+      " and the computer played " +
+      computerTurn +
+      " so you lose";
+    // Return output.
+    console.log("you played ", input, " and computer played ", computerTurn);
+    return myOutputValue;
+  }
+
+  // Scissors vs paper (win) or reversed scissors vs stone (win)
+  if (
+    (input == "scissors" && computerTurn == "paper") ||
+    (input == "reversed scissors" && computerTurn == "stone")
+  ) {
+    myOutputValue =
+      "You played " +
+      input +
+      " and the computer played " +
+      computerTurn +
+      " so you win";
     // Return output.
     console.log("you played ", input, " and computer played ", computerTurn);
     return myOutputValue;
