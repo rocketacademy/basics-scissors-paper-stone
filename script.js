2ba5588583790f504fef8052aef7bf61ac77aba4
berniceyeo <berniceyeosuping@gmail.com>
2021-10-16 14:59:47 +0800

    added reversed scissors

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
 var scissors = "scissors";
 var paper = "paper";
 var stone = "stone";
-
-var main = function (input) {
-  var player = input;
-  var computer = computerChoice();
-  console.log("Computer Choice");
-  console.log(computer);
-  console.log("User Choice");
-  console.log(input);
-  var myOutputValue = `You have played ${input} and you have lost to the computer's choice ${computer}! <br> you lose! <br> Please type scissors, paper or stone to start new game `;
-
-  //check for invalid inputs
-  if (player != scissors && player != stone && player != paper) {
-    myOutputValue =
-      "you have entered a incorrect input <br> Please enter either scissors, paper or stone <br>";
-  }
-
-  //check for win
-  if (
-    (player == scissors && computer == paper) ||
-    (player == stone && computer == scissors) ||
-    (player == paper && computer == stone)
-  ) {
-    myOutputValue = `You have played ${input} and you have won the computer's choice ${computer}! <br> good job! <br> Now you can type "scissors, paper or stone to start new game <br>`;
-  }
-
-  //check for draw
-  if (
-    (player == scissors && computer == scissors) ||
-    (player == stone && computer == stone) ||
-    (player == paper && computer == paper)
-  ) {
-    myOutputValue = `You have played ${input} and you have won the computer's choice ${computer}! <br> Its a draw!!`;
-  }
-
-  return myOutputValue;
-};
+var reversedScissors = "reversed scissors";
+var reversedPaper = "reversed paper";
+var reversedStone = "reversed stone";
 
 var randomNumber = function () {
   var randomDecimal = Math.random() * 3;
@@ -50,12 +17,54 @@ var computerChoice = function () {
   var number = randomNumber();
   var choice = scissors;
 
-  if (randomNumber == 0) {
+  if (number == 0) {
     choice = stone;
   }
 
-  if (randomNumber == 1) {
+  if (number == 1) {
     choice = paper;
   }
   return choice;
+};
+
+var main = function (input) {
+  var computer = computerChoice();
+  var player = input;
+  console.log("Computer Choice");
+  console.log(computer);
+  console.log("User Choice");
+  console.log(input);
+  var myOutputValue = `You have played ${input} and you have lost to the computer's choice ${computer}! <br> you lose! <br> Please type scissors, paper or stone to start a new game `;
+
+  //check for invalid inputs
+  if (
+    player != scissors &&
+    player != stone &&
+    player != paper &&
+    player != reversedScissors &&
+    player != reversedStone &&
+    player != reversedPaper
+  ) {
+    myOutputValue =
+      "you have entered a incorrect input <br> Please enter either scissors, paper or stone <br>";
+  }
+
+  //check for win
+  if (
+    (player == scissors && computer == paper) ||
+    (player == stone && computer == scissors) ||
+    (player == paper && computer == stone) ||
+    (player == reversedPaper && computer == scissors) ||
+    (player == reversedStone && computer == paper) ||
+    (player == reversedScissors && computer == stone)
+  ) {
+    myOutputValue = `You have played ${input} and you have won the computer's choice ${computer}! <br> good job! <br> Now you can type "scissors, paper or stone to start new game <br>`;
+  }
+
+  //check for draw
+  if (player == computer) {
+    myOutputValue = `You have played ${input} and its the same as the computer's choice ${computer}! <br> Its a draw!!`;
+  }
+
+  return myOutputValue;
 };
