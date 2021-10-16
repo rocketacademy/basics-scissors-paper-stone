94e37bed4f37e2f5a5a720f83601d3e21c4ef90c
honeyroastedturkey <wong1046@e.ntu.edu.sg>
2021-10-16 13:48:21 +0800

    scissors paper stone ver1

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var results = scissorsPaperStone(input);
+  return results;
+};
+
+var scissorsPaperStone = function (input) {
+  var listOfChoice = ["scissors", "paper", "stone"];
+  var computerChoice = Math.floor(Math.random() * listOfChoice.length);
+  //var computerChoice = listOfChoice[randomChoice];
+  var userChoice = listOfChoice.indexOf(input);
+  var results = userChoice - computerChoice;
+  var winningCases = [-1, 2];
+  var losingCases = [1, -2];
+  if (winningCases.includes(results)) {
+    output = "Win";
+  }
+  if (losingCases.includes(results)) {
+    output = "Lose";
+  }
+  if (results == 0) {
+    output = "Draw";
+  }
+  return "The computer chose " + listOfChoice[computerChoice] + "the " + output;
 };
