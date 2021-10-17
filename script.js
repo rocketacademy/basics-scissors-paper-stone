260077b201b1335a816759c95de108ec8c82af1a
jason1015 <jianxiong.ong@gmail.com>
2021-10-17 12:44:20 +0800

    simplefied codes

--- script.js
+++ script.js
@@ -1,115 +1,66 @@
+var countGameTurn = 0;
+var countWinTurn = 0;
+
 var generateRandomInteger = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal);
-  console.log("randomInteger = " + randomInteger);
+  console.log(`randomInteger =${randomInteger}`);
+  return randomInteger;
+};
+
+var generateProgamInput = function () {
+  var randomInteger = generateRandomInteger();
   if (randomInteger == 0) progInput = "Scissors";
   if (randomInteger == 1) progInput = "Paper";
   if (randomInteger == 2) progInput = "Stone";
-  console.log("progam input = " + progInput);
+  console.log(`progam input =${progInput}`);
   return progInput;
 };
 
+var generateWinLossRecord = function () {
+  var record = countWinTurn / countGameTurn;
+  console.log(`game record =${record}`);
+  if (record >= 0.5) {
+    return `Lucky!`;
+  }
+  if (record < 0.5) {
+    return `Bummer!`;
+  }
+};
+
 var main = function (input) {
-  var progInput = generateRandomInteger();
+  var progInput = generateProgamInput();
+  console.log(`your input = ${input}`);
   // default output
-  var myOutputValue =
-    "Error, please try again. Input options are 'Scissors', 'Paper' or 'Stone'";
-  if (input == progInput)
-    myOutputValue =
-      "Its a draw!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Scissors" && progInput == "Paper")
-    myOutputValue =
-      "You Win!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Scissors" && progInput == "Stone")
-    myOutputValue =
-      "You Lose!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Paper" && progInput == "Scissors")
-    myOutputValue =
-      "You Lose!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Paper" && progInput == "Stone")
-    myOutputValue =
-      "You Win!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Stone" && progInput == "Scissors")
-    myOutputValue =
-      "You Win!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Stone" && progInput == "Paper")
-    myOutputValue =
-      "You Lose!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Reversed Stone" && progInput == "Paper")
-    myOutputValue =
-      "You Win!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Reversed Stone" && progInput == "Scissors")
-    myOutputValue =
-      "You Lose!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Reversed Stone" && progInput == "Stone")
-    myOutputValue =
-      "Its a draw!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Reversed Scissors" && progInput == "Paper")
-    myOutputValue =
-      "You Lose!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Reversed Scissors" && progInput == "Stone")
-    myOutputValue =
-      "You Win!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Reversed Scissors" && progInput == "Scissors")
-    myOutputValue =
-      "Its a draw!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Reversed Paper" && progInput == "Scissors")
-    myOutputValue =
-      "You Win!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Reversed Paper" && progInput == "Stone")
-    myOutputValue =
-      "You Lose!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
-  if (input == "Reversed Paper" && progInput == "Paper")
-    myOutputValue =
-      "Its a draw!, <br>you choose " +
-      input +
-      " <br>and program choose " +
-      progInput;
+  if (input != `Scissors` || input != `Paper` || input != `Stone`) {
+    myOutputValue = `Error, <br>Please try again. <br>Input options are 'Scissors', 'Paper' or 'Stone'`;
+  }
+  // draw condition
+  if (input == progInput) {
+    countGameTurn += 1;
+    myOutputValue = `Draw game! <br>Computer chose: ${progInput}. <br>You chose${input}. <br>You win ${countWinTurn}/${countGameTurn}. ${generateWinLossRecord()}. `;
+  }
+  // win condition
+  if (
+    (input == `Scissors` && progInput == "Paper") ||
+    (input == "Paper" && progInput == "Stone") ||
+    (input == "Stone" && progInput == "Scissors")
+  ) {
+    countGameTurn += 1;
+    countWinTurn += 1;
+    myOutputValue = `Win game! <br>Computer chose ${progInput}. <br>You chose ${input}. <br>You win ${countWinTurn}/${countGameTurn}. ${generateWinLossRecord()}. `;
+  }
+  // lose condition
+  if (
+    (input == `Scissors` && progInput == "Stone") ||
+    (input == "Paper" && progInput == "Scissors") ||
+    (input == "Stone" && progInput == "Paper")
+  ) {
+    countGameTurn += 1;
+    myOutputValue = `Lose game! <br>Computer chose ${progInput}. <br>You chose ${input}. <br>You win ${countWinTurn}/${countGameTurn}. ${generateWinLossRecord()}. `;
+  }
+  console.log(`Total turns =${countGameTurn}`);
+  console.log(`Total wins =${countWinTurn}`);
 
   return myOutputValue;
 };
