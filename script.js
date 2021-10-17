5fd44482fd6b61221f2cd203d371158fbbd36194
darientan <darienjacobtan@gmail.com>
2021-10-17 22:43:11 +0800

    added User Name

--- script.js
+++ script.js
@@ -1,103 +1,110 @@
 var winRecord = 0;
 var loseRecord = 0;
+var currentGameMode = "collect user name";
+var userName = "";
 
 var main = function (input) {
-  var randomSPS = random();
   var myOutputValue = "";
+  if (currentGameMode == "collect user name") {
+    userName = input;
+    myOutputValue = `Welcome ${userName}! Guess "scissors", "paper" or "stone" to play!`;
+    currentGameMode = "play";
+  } else if (currentGameMode == "play") {
+    var randomSPS = random();
 
-  //input validation
-  if (
-    input !== "scissors" ||
-    input !== "paper" ||
-    input !== "stone" ||
-    input !== "reversed scissors" ||
-    input !== "reversed paper" ||
-    input !== "reversed stone"
-  ) {
-    myOutputValue =
-      "Try again. Please enter either 'scissors', 'paper', or 'stone'";
-  }
+    // input validation
+    if (
+      input !== "scissors" ||
+      input !== "paper" ||
+      input !== "stone" ||
+      input !== "reversed scissors" ||
+      input !== "reversed paper" ||
+      input !== "reversed stone"
+    ) {
+      myOutputValue = `${userName}, try again! Please enter either 'scissors', 'paper', or 'stone'`;
+    }
 
-  //assign randomly generated integer to either SPS value
-  if (randomSPS == 1) {
-    randomSPS = "scissors";
-  }
+    //assign randomly generated integer to either SPS value
+    if (randomSPS == 1) {
+      randomSPS = "scissors";
+    }
 
-  if (randomSPS == 2) {
-    randomSPS = "paper";
-  }
+    if (randomSPS == 2) {
+      randomSPS = "paper";
+    }
 
-  if (randomSPS == 3) {
-    randomSPS = "stone";
-  }
+    if (randomSPS == 3) {
+      randomSPS = "stone";
+    }
 
-  console.log(randomSPS);
+    console.log(randomSPS);
 
-  //DRAW
-  if (input == randomSPS) {
-    myOutputValue = `The computer chose ${randomSPS}.<br>
+    //DRAW
+    if (input == randomSPS) {
+      myOutputValue = `The computer chose ${randomSPS}.<br>
       You chose ${input}.<br><br>
       It's a draw.<br><br>
       Now you can type "scissors" "paper" or "stone" to play another round!<br>
-      Your record: ${winRecord} Wins, ${loseRecord} Losses.`;
-  }
+      ${userName}'s record: ${winRecord} Wins, ${loseRecord} Losses.`;
+    }
 
-  //WIN
-  if (
-    (input == "scissors" && randomSPS == "paper") ||
-    (input == "paper" && randomSPS == "stone") ||
-    (input == "stone" && randomSPS == "scissors")
-  ) {
-    winRecord = winRecord + 1;
-    myOutputValue = `The computer chose ${randomSPS}.<br>
+    //WIN
+    if (
+      (input == "scissors" && randomSPS == "paper") ||
+      (input == "paper" && randomSPS == "stone") ||
+      (input == "stone" && randomSPS == "scissors")
+    ) {
+      winRecord = winRecord + 1;
+      myOutputValue = `The computer chose ${randomSPS}.<br>
       You chose ${input}.<br><br>
       You win! Congrats!<br><br>
-      Now you can type "scissors" "paper" or "stone" to play another round!
-      Your record: ${winRecord} Wins, ${loseRecord} Losses.`;
-  }
+      Now you can type "scissors" "paper" or "stone" to play another round!<br>
+      ${userName}'s record: ${winRecord} Wins, ${loseRecord} Losses.`;
+    }
 
-  //LOSE
-  if (
-    (randomSPS == "scissors" && input == "paper") ||
-    (randomSPS == "paper" && input == "stone") ||
-    (randomSPS == "stone" && input == "scissors")
-  ) {
-    loseRecord = loseRecord + 1;
-    myOutputValue = `The computer chose ${randomSPS}.<br>
+    //LOSE
+    if (
+      (randomSPS == "scissors" && input == "paper") ||
+      (randomSPS == "paper" && input == "stone") ||
+      (randomSPS == "stone" && input == "scissors")
+    ) {
+      loseRecord = loseRecord + 1;
+      myOutputValue = `The computer chose ${randomSPS}.<br>
       You chose ${input}.<br><br>
       You lose! Bummer.<br><br>
-      Now you can type "scissors" "paper" or "stone" to play another round!
-      Your record: ${winRecord} Wins, ${loseRecord} Losses.`;
-  }
+      Now you can type "scissors" "paper" or "stone" to play another round!<br>
+      ${userName}'s record: ${winRecord} Wins, ${loseRecord} Losses.`;
+    }
 
-  //REVERSED WIN
-  if (
-    (randomSPS == "scissors" && input == "reversed paper") ||
-    (randomSPS == "paper" && input == "reversed stone") ||
-    (randomSPS == "stone" && input == "reversed scissors")
-  ) {
-    winRecord = winRecord + 1;
-    myOutputValue = `REVERSE! <br>
+    //REVERSED WIN
+    if (
+      (randomSPS == "scissors" && input == "reversed paper") ||
+      (randomSPS == "paper" && input == "reversed stone") ||
+      (randomSPS == "stone" && input == "reversed scissors")
+    ) {
+      winRecord = winRecord + 1;
+      myOutputValue = `REVERSE! <br>
       The computer chose ${randomSPS}.<br>
       You chose ${input}.<br><br>
       You win! Congrats!<br><br>
       Now you can type "scissors" "paper" or "stone" to play another round!<br>
-      Your record: ${winRecord} Wins, ${loseRecord} Losses.`;
-  }
+      ${userName}'s record: ${winRecord} Wins, ${loseRecord} Losses.`;
+    }
 
-  //REVERSED LOSE
-  if (
-    (input == "reversed scissors" && randomSPS == "paper") ||
-    (input == "reversed paper" && randomSPS == "stone") ||
-    (input == "reversed stone" && randomSPS == "scissors")
-  ) {
-    loseRecord = loseRecord + 1;
-    myOutputValue = `REVERSE! <br>
+    //REVERSED LOSE
+    if (
+      (input == "reversed scissors" && randomSPS == "paper") ||
+      (input == "reversed paper" && randomSPS == "stone") ||
+      (input == "reversed stone" && randomSPS == "scissors")
+    ) {
+      loseRecord = loseRecord + 1;
+      myOutputValue = `REVERSE! <br>
       The computer chose ${randomSPS}.<br>
       You chose ${input}.<br><br>
       You lose! Bummer.<br><br>
       Now you can type "scissors" "paper" or "stone" to play another round!<br>
-      Your record: ${winRecord} Wins, ${loseRecord} Losses.`;
+      ${userName}'s record: ${winRecord} Wins, ${loseRecord} Losses.`;
+    }
   }
 
   return myOutputValue;
