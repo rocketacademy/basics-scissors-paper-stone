b3a880e91b61602b537522e89bbed7fa8c37b6dc
EmilySinML <02October2021!>
2021-10-17 17:43:47 +0800

    changes made based on (Part 1) feedback

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-//Create a basic version of Scissors Paper Stone where the user inputs one of "scissors", "paper", or "stone", the program internally randomly chooses scissors, paper, or stone, and the program outputs whether the user won, the program won, or it's a draw.
-//Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
+//Create a basic version of Scissors Paper Stone where the user inputs one of "scissors", "paper", or "stone", the program internally randomly chooses scissors, paper, or stone, and the program outputs whether the user won, the program won, or it's a draw. Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
+//Add state to your program such that it keeps track of the number of times the user has won and the number of times the computer has won. Output this win-loss record in a format you like in the program output. You can also output the number of draws and/or each party's winning percentage if you'd like.
 
 var main = function (input) {
-  console.log("User Guess");
-  console.log(input);
   var canContinue = validateUserInput(input);
-  console.log(canContinue);
+  console.log("User Guess:", input);
+  console.log("canContinue:", canContinue);
+
+  //Validate User's input - cannot continue
   if (!canContinue) {
-    myOutputValue = "Try again by inputting either: Scissors, Paper, Stone.";
+    var myOutputValue =
+      "Try again by inputting either: Scissors, Paper, Stone.";
   }
+  //Validate User's input - can continue
   if (canContinue) {
     var ComputerNumber = generateNumber();
-    console.log("Computer Generated Number");
-    console.log(ComputerNumber);
     var ComputerGuess = assignSPS(ComputerNumber);
-    console.log("Computer Guess");
-    console.log(ComputerGuess);
-    var myOutputValue = compareSPS(input, ComputerGuess);
+    myOutputValue = compareSPS(input, ComputerGuess);
+    console.log("Computer Generated Number:", ComputerNumber);
+    console.log("Computer Guess:", ComputerGuess);
   }
   return myOutputValue;
 };
@@ -45,33 +46,36 @@ var assignSPS = function (Number) {
 
 //Compare User Input & Computer Guess
 var compareSPS = function (UserInput, ComputerInput) {
+  //Draw
   if (UserInput == ComputerInput) {
     var gameOutput =
-      "Draw! You chose " +
+      "Draw!<br > You chose " +
       UserInput +
       " and the Computer chose " +
       ComputerInput +
       ".";
   }
+  //Lose
   if (
     (UserInput == "Scissors" && ComputerInput == "Stone") ||
     (UserInput == "Paper" && ComputerInput == "Scissors") ||
     (UserInput == "Stone" && ComputerInput == "Paper")
   ) {
-    var gameOutput =
-      "You lose! You chose " +
+    gameOutput =
+      "You lose!<br > You chose " +
       UserInput +
       " and the Computer chose " +
       ComputerInput +
       ".";
   }
+  //Win
   if (
     (UserInput == "Scissors" && ComputerInput == "Paper") ||
     (UserInput == "Paper" && ComputerInput == "Stone") ||
     (UserInput == "Stone" && ComputerInput == "Scissors")
   ) {
-    var gameOutput =
-      "You win! You chose " +
+    gameOutput =
+      "You win! <br > You chose " +
       UserInput +
       " and the Computer chose " +
       ComputerInput +
@@ -82,7 +86,7 @@ var compareSPS = function (UserInput, ComputerInput) {
 
 //Validate User Input
 var validateUserInput = function (UserInput) {
-  flag =
+  var flag =
     UserInput == "Scissors" || UserInput == "Paper" || UserInput == "Stone";
   return flag;
 };
