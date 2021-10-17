e9d3ec75a56267e07d56e0b9f56b4189a4f91fcc
Nateliew <weeli.tn@gmail.com>
2021-10-17 18:43:51 +0800

    added functions for part 2

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var userWin = 0;
+var computerWin = 0;
+var totalPlay = 0;
+var currentGameMode = "Waiting for username";
+var userName = "";
+
 // Computer generates random Number
 var randomNumber = function () {
   var randomDecimal = Math.random() * 3;
@@ -25,25 +31,60 @@ var computerGuesses = function () {
 // if user input beats computer's, user win
 var main = function (input) {
   var userInput = input.toLowerCase();
-  var computersGuess = computerGuesses();
-  if (
-    (computersGuess == "paper" && userInput == "scissors") ||
-    (computersGuess == "stone" && userInput == "paper") ||
-    (computersGuess == "scissors" && userInput == "stone")
-  ) {
-    return (
-      "The Computer chose " +
-      computersGuess +
-      " and you chose " +
-      userInput +
-      ". You won! Now you can type scissors, paper or stone to play another round!."
-    );
+  //Waiting for user input
+  if (currentGameMode == "Waiting for username") {
+    //if the input's value for not a number is false, the user has to type again
+    if (Number.isNaN(Number(input)) == false) {
+      myOutputValue = "Sorry please enter a Name.";
+    } else {
+      userName = input;
+      currentGameMode = "SPS game";
+      myOutputValue = "Hello " + userName;
+    }
   }
-  return (
-    "The Computer chose " +
-    computersGuess +
-    " and you chose " +
-    userInput +
-    ". You lose! Bummer. Now you can type scissors, paper or stone to play another round!."
-  );
+
+  //When user input and the SPS game mode is switched on
+  else if (currentGameMode == "SPS game") {
+    var computersGuess = computerGuesses();
+    var myOutputValue = "";
+    //if we beat the computer, we win
+    if (
+      (computersGuess == "paper" && userInput == "scissors") ||
+      (computersGuess == "stone" && userInput == "paper") ||
+      (computersGuess == "scissors" && userInput == "stone")
+    ) {
+      userWin = userWin + 1;
+      totalPlay = totalPlay + 1;
+      myOutputValue =
+        "The Computer chose " +
+        computersGuess +
+        "<br><br> You chose " +
+        userInput +
+        ".<br><br> You won! <br><br> Now you can type scissors, paper or stone to play another round! <br><br> You won: " +
+        userWin;
+      //else if the computer is the same as the user's input, it's a draw
+    } else {
+      if (computersGuess == userInput) {
+        totalPlay = totalPlay + 1;
+        myOutputValue =
+          "The Computer chose " +
+          computersGuess +
+          "<br><br> You chose " +
+          userInput +
+          ".<br><br> It's a draw! <br><br> Now you can type scissors, paper or stone to play another round!";
+        //if the 2 conditions are not met, the player lose
+      } else {
+        computerWin = computerWin + 1;
+        totalPlay = totalPlay + 1;
+        myOutputValue =
+          "The Computer chose " +
+          computersGuess +
+          "<br><br> You chose " +
+          userInput +
+          ".<br><br> You lose! Bummer. <br><br> Now you can type scissors, paper or stone to play another round! <br><br> The computer won: " +
+          computerWin;
+      }
+    }
+  }
+  return myOutputValue;
 };
