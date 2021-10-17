41610dd7f24471b6bd99676a5075f70a239ab419
darientan <darienjacobtan@gmail.com>
2021-10-17 18:44:08 +0800

    Base SPS, More Comfortable

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 var main = function (input) {
   var randomSPS = random();
+  var myOutputValue = "";
 
-  var myOutputValue = "Please enter either 'scissors', 'paper', or 'stone'";
+  //input validation
+  if (
+    input !== "scissors" ||
+    input !== "paper" ||
+    input !== "stone" ||
+    input !== "reversed scissors" ||
+    input !== "reversed paper" ||
+    input !== "reversed stone"
+  ) {
+    myOutputValue =
+      "Try again. Please enter either 'scissors', 'paper', or 'stone'";
+  }
 
+  //assign randomly generated integer to either SPS value
   if (randomSPS == 1) {
     randomSPS = "scissors";
   }
@@ -17,23 +30,62 @@ var main = function (input) {
 
   console.log(randomSPS);
 
+  //DRAW
   if (input == randomSPS) {
-    myOutputValue = `It's a draw! Your choice is ${input}, program's choice was ${randomSPS}`;
+    myOutputValue = `The computer chose ${randomSPS}.<br>
+      You chose ${input}.<br><br>
+      It's a draw.<br><br>
+      Now you can type "scissors" "paper" or "stone" to play another round!`;
   }
+
+  //WIN
   if (
     (input == "scissors" && randomSPS == "paper") ||
     (input == "paper" && randomSPS == "stone") ||
     (input == "stone" && randomSPS == "scissors")
   ) {
-    myOutputValue = `You win! Your choice is ${input}, program's choice was ${randomSPS}`;
+    myOutputValue = `The computer chose ${randomSPS}.<br>
+      You chose ${input}.<br><br>
+      You win! Congrats!<br><br>
+      Now you can type "scissors" "paper" or "stone" to play another round!`;
   }
 
+  //LOSE
   if (
     (randomSPS == "scissors" && input == "paper") ||
     (randomSPS == "paper" && input == "stone") ||
     (randomSPS == "stone" && input == "scissors")
   ) {
-    myOutputValue = `You lose! Your choice is ${input}, program's choice was ${randomSPS}`;
+    myOutputValue = `The computer chose ${randomSPS}.<br>
+      You chose ${input}.<br><br>
+      You lose! Bummer.<br><br>
+      Now you can type "scissors" "paper" or "stone" to play another round!`;
+  }
+
+  //REVERSED WIN
+  if (
+    (randomSPS == "scissors" && input == "reversed paper") ||
+    (randomSPS == "paper" && input == "reversed stone") ||
+    (randomSPS == "stone" && input == "reversed scissors")
+  ) {
+    myOutputValue = `REVERSE! <br>
+      The computer chose ${randomSPS}.<br>
+      You chose ${input}.<br><br>
+      You win! Congrats!<br><br>
+      Now you can type "scissors" "paper" or "stone" to play another round!`;
+  }
+
+  //REVERSED LOSE
+  if (
+    (input == "reversed scissors" && randomSPS == "paper") ||
+    (input == "reversed paper" && randomSPS == "stone") ||
+    (input == "reversed stone" && randomSPS == "scissors")
+  ) {
+    myOutputValue = `REVERSE! <br>
+      The computer chose ${randomSPS}.<br>
+      You chose ${input}.<br><br>
+      You lose! Bummer.<br><br>
+      Now you can type "scissors" "paper" or "stone" to play another round!`;
   }
 
   return myOutputValue;
