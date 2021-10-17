cd0bade09898fa2c822c466262989d0f7061b108
formula3 <lionelbx123@gmail.com>
2021-10-17 17:55:43 +0800

    Add stats & username

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Counters to keep track of scores
+var totalAttempts = 0;
+var userWinCount = 0;
+var computerWinCount = 0;
+var drawCount = 0;
+// Player's name
+var userName = "";
+var emptyUserName = true;
+
 var getRandomOutput = function () {
   // Get random integer from 1 to 3
   var randomDecimal = Math.random() * 3;
@@ -36,21 +45,16 @@ var checkBattleOutcome = function (userInput, programInput) {
   var reversedGame = false;
   var finalBattleOutcome = "Something went wrong.";
   var userInputForGame = userInput;
-
   var userWon = true;
-  var winMessage = "You win! =D";
-  var loseMessage = "You lose :(";
-
   if (userInput.startsWith("Reversed")) {
     userInputForGame = userInputForGame.replace("Reversed ", "");
     reversedGame = true;
   }
-
   if (userInputForGame == programInput) {
+    drawCount += 1;
     finalBattleOutcome = "Draw";
     return finalBattleOutcome;
   }
-
   if (userInputForGame == "Scissors") {
     if (programInput == "Stone") {
       userWon = false;
@@ -70,37 +74,79 @@ var checkBattleOutcome = function (userInput, programInput) {
       userWon = true;
     }
   }
-
   if (reversedGame == true) {
     userWon = !userWon;
   }
-
   if (userWon == true) {
-    finalBattleOutcome = winMessage;
+    userWinCount += 1;
+    finalBattleOutcome = `You win! =D`;
   } else {
-    finalBattleOutcome = loseMessage;
+    computerWinCount += 1;
+    finalBattleOutcome = `You lose :(`;
   }
-
   return finalBattleOutcome;
 };
 
-var main = function (input) {
-  var check = checkInput(input);
+var scissorsPaperStoneGame = function (input) {
+  var errorMessage = checkInput(input);
 
-  if (check != "") {
-    return check;
+  if (errorMessage != "") {
+    return errorMessage;
   }
+
+  totalAttempts += 1;
 
   var computerDecision = getRandomOutput();
   console.log(computerDecision);
 
   var battleResult = checkBattleOutcome(input, computerDecision);
 
-  return `Result: ${battleResult}`;
+  // if ("win" in battleResult) {
+  //   userWinCount += 1;
+  // } else if ("lose" in battleResult) {
+  //   computerWinCount += 1;
+  // } else {
+  //   drawCount += 1;
+  // }
+
+  console.log("user count ---> " + userWinCount);
+  console.log("com count ---> " + computerWinCount);
+  var userWinPercentage = (userWinCount / totalAttempts) * 100;
+  var computerWinPercentage = (computerWinCount / totalAttempts) * 100;
+
+  return `Computer chose ${computerDecision}, you chose ${input}
+  <br> Current Result: ${battleResult}
+  <br> OVERALL
+  <br> ${userName} Won: ${userWinCount} time(s), ${userName} Win Percentage: ${userWinPercentage}%
+  <br> Computer Won: ${computerWinCount} time(s), Computer Win Percentage: ${computerWinPercentage}%
+  <br> Draw: ${drawCount} time(s)
+  `;
 };
 
-console.log("program decision --> " + getRandomOutput());
-console.log("checking input --> " + checkInput("Paper"));
-console.log(
-  "battle outcome --> " + checkBattleOutcome("Reversed Stone", "Paper")
-);
+var checkUserName = function (input) {
+  console.log(input);
+  userName = input;
+  if (userName == "") {
+    return "Pls input a username";
+  }
+  emptyUserName = false;
+  return `Hi ${userName}!
+  <br> To start the game, input 'Scissors' / 'Paper' / 'Stone'
+  <br> If you want to play the reversed game mode, insert 'Reversed ' before your input
+  <br> Good luck!`;
+};
+
+var main = function (input) {
+  var mainOutputValue = "";
+
+  if (emptyUserName == true) {
+    mainOutputValue = checkUserName(input);
+    return mainOutputValue;
+  }
+
+  mainOutputValue = scissorsPaperStoneGame(input);
+
+  return mainOutputValue;
+};
+
+console.log();
