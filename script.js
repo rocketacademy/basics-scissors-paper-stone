d38bf360637b93a0211a098bff8ed41709d98d33
Explore135 <ktpicho@yahoo.co.nz>
2021-10-17 20:48:13 +0800

    i just added this

--- script.js
+++ script.js
@@ -1,57 +1,130 @@
-// Basic Scissors Paper Stone (reversed)
+var numOne = 0;
+var numTwo = 0;
+var numThree = 0;
+var numFour = 0;
+var numFive = 0;
+var numSix = 0;
+var prevRoll = 0;
+var numOfWin = 0;
+var numOfLoss = 0;
+var numOfGame = 0;
+var points = 0;
+var result = "";
+var lossRate = 0;
+var winRate = 0;
+var currentGameMode = "waiting for user name";
+var userName = "";
+var userGuess = 0;
 
-var guess_scissors = `scissors`;
-var guess_paper = `paper`;
-var guess_stone = `stone`;
-
-var generateRandomInteger = function (max) {
-  var randomDecimal = Math.random() * max;
-  var randomInteger = Math.floor(randomDecimal) + 1;
-  return randomInteger;
+var rollDice = function () {
+  var randomDecimal = Math.random() * 6;
+  var randomInteger = Math.floor(randomDecimal);
+  var diceNumber = randomInteger + 1;
+  return diceNumber;
 };
 
-var generateRandomGuess = function () {
-  var totalNumGuess = 3;
-  var randomGuessNum = generateRandomInteger(totalNumGuess);
-  if (randomGuessNum == 1) {
-    return guess_scissors;
+var diceGame = function (userName, userGuess) {
+  var message = "";
+  var randomDiceNumber = rollDice();
+  numOfGame = numOfGame + 1;
+  // determine if this is the first roll
+  if (prevRoll == 0) {
+    message = `Hi ${userName}! This is your first roll! `;
+    console.log(message);
+  } else {
+    message = `Hi ${userName}! Your last roll was ${prevRoll}.`;
   }
-  if (randomGuessNum == 2) {
-    return guess_paper;
+  // determine it is a win/loss
+  if (randomDiceNumber == userGuess) {
+    numOfWin = numOfWin + 1;
+    result = "win";
+  } else {
+    numOfLoss = numOfLoss + 1;
+    result = "lose";
   }
-  if (randomGuessNum == 3) {
-    return guess_stone;
+  winRate = ((numOfWin / numOfGame) * 100).toFixed(0) + `%`;
+  lossRate = ((numOfLoss / numOfGame) * 100).toFixed(0) + `%`;
+  message += `You just rolled a ${randomDiceNumber}. You guessed ${userGuess}. You ${result}! <br>
+  You win ${numOfWin} out of ${numOfGame}. Win rate = ${winRate}.<br>
+  You lose ${numOfLoss} out of ${numOfGame}. Loss rate = ${lossRate}.<br>`;
+  console.log(message);
+  prevRoll = randomDiceNumber;
+  // determine which is the most rolled number
+
+  if (randomDiceNumber == 1) {
+    numOne = numOne + 1;
   }
+
+  if (randomDiceNumber == 2) {
+    numTwo = numTwo + 1;
+  }
+
+  if (randomDiceNumber == 3) {
+    numThree = numThree + 1;
+  }
+
+  if (randomDiceNumber == 4) {
+    numFour = numFour + 1;
+  }
+
+  if (randomDiceNumber == 5) {
+    numFive = numFive + 1;
+  }
+
+  if (randomDiceNumber == 6) {
+    numSix = numSix + 1;
+  }
+
+  var rollCountNum = [numOne, numTwo, numThree, numFour, numFive, numSix];
+  var winnerNum = -1,
+    winners = []; // define winners as an array
+  for (var i = 0; i < rollCountNum.length; i++) {
+    if (rollCountNum[i] > winnerNum) {
+      winners = [i + 1];
+      winnerNum = rollCountNum[i];
+    } else if (rollCountNum[i] == winnerNum) {
+      //Use else if to specify a new condition to test, if the first condition is false
+      winners.push(i + 1);
+    }
+  }
+  if (winners.length == 1) {
+    message += `The most rolled number is number ${winners[0]}! <br>`;
+  } else {
+    message += `The most rolled numbers are  numbers ${winners.join(
+      ", "
+    )}! <br>`;
+  }
+
+  // calculate the total points
+  if (randomDiceNumber - userGuess == 1 || userGuess - randomDiceNumber == 1) {
+    points = points + 1;
+  } else if (randomDiceNumber == userGuess) {
+    points = points + 2;
+  }
+  return (message += `Total winning points = ${points}.`);
 };
 
 var main = function (input) {
-  var programHas = generateRandomGuess();
+  var myOutputValue = "";
 
-  var myOutputValue = `try again! do "scissors", "paper", or "stone" only.`;
-
-  if (
-    (input == "scissors" && guess_scissors == programHas) ||
-    (input == "paper" && guess_paper == programHas) ||
-    (input == "stone" && guess_stone == programHas)
-  ) {
-    myOutputValue = `you have ${input} & the program has ${programHas} so a draw.`;
+  if (currentGameMode == "waiting for user name") {
+    userName = input;
+    //validation
+    if (Number.isNaN(Number(input)) == false) {
+      myOutputValue = "sorry please enter a name.";
+      //validation
+    } else {
+      currentGameMode = "dice game";
+      myOutputValue = `Hello ${userName}`;
+    }
+  } else if (currentGameMode == "dice game") {
+    //validation
+    if (Number.isNaN(Number(input)) == true || input > 6 || input < 1) {
+      myOutputValue = "sorry please enter a number between 1 - 6.";
+      //validation
+    } else {
+      myOutputValue = diceGame(userName, input);
+    }
   }
-
-  if (
-    (input == "scissors" && guess_paper == programHas) ||
-    (input == "paper" && guess_stone == programHas) ||
-    (input == "stone" && guess_scissors == programHas)
-  ) {
-    myOutputValue = `you have ${input} & the program has ${programHas} so you lose.`;
-  }
-
-  if (
-    (input == "scissors" && guess_stone == programHas) ||
-    (input == "paper" && guess_scissors == programHas) ||
-    (input == "stone" && guess_paper == programHas)
-  ) {
-    myOutputValue = `you have ${input} & the program has ${programHas} so you win.`;
-  }
-
   return myOutputValue;
 };
