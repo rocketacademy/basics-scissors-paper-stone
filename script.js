7926bee4de1ecca6be26d3fd6f5de5a215abd14b
Jermsery <jeremyhzj.pc@gmail.com>
2021-10-18 13:38:28 +0800

    first commit

--- script.js
+++ script.js
@@ -9,18 +9,15 @@ var main = function (input) {
   if (input == 'scissors' && randomInteger == 0){
     myOutputValue = 'The computer also chose scissors, its a draw'  
   }
-
   if (input == 'scissors' && randomInteger == 1){
     myOutputValue = 'The computer chose paper, you won'  
   }
   if (input == 'scissors' && randomInteger == 2){
     myOutputValue = 'The computer chose stone, you lost'  
   }
-
   if (input == 'paper' && randomInteger == 0){
     myOutputValue = 'The computer chose scissors, you lost'  
   }
-
   if (input == 'paper' && randomInteger == 1){
     myOutputValue = 'The computer also chose paper, its a draw'  
   }
@@ -30,13 +27,11 @@ var main = function (input) {
   if (input == 'stone' && randomInteger == 0){
     myOutputValue = 'The computer chose scissors, you won'  
   }
-
   if (input == 'stone' && randomInteger == 1){
     myOutputValue = 'The computer chose paper, you lost'  
   }
   if (input == 'stone' && randomInteger == 2){
     myOutputValue = 'The computer also chose stone, its a draw'  
   }
-
  return myOutputValue 
 }
