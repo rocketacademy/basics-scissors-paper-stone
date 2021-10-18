61902fe7b0204f8a3256a7e17ee63fbba444c934
tbunnag <tara.kate.b@gmail.com>
2021-10-18 19:03:17 +0800

    I fixed my input validation that was not working

--- script.js
+++ script.js
@@ -8,21 +8,17 @@ var playerHand = ``;
 // ======================================MAIN FUNCTION STARTS BELOW ====================================
 
 var main = function (input) {
+  myOutputValue = ``;
   //Username needed to start the game
   if (gameMode == `waiting for username`) {
     userName = input;
     gameMode = `SPS`;
-    myOutputValue = `Hello ${userName}! <br> You are now ready to play. Paper, scissors, or stone?`;
+    myOutputValue = `Hello ${userName}! <br> <br> You are now ready to play. <br> <br> Paper, scissors, or stone?`;
   } else if (gameMode == `SPS`) {
     myOutputValue = spsGame(input);
-  } else {
-    // input validation
-    myOutputValue = `not working`;
-
-    //`Sorry, ${input} was not recognized. You can only enter scissors, paper, or stone. Please try again.`;
   }
 
-  return `${myOutputValue} <br> GAME MODE: ${gameMode} <br> Rounds played: ${roundCount}`;
+  return `${myOutputValue} <br> <br> GAME MODE: ${gameMode} <br> <br> Rounds played: ${roundCount}`;
 };
 
 // ===============================MAIN FUNCITON ENDS HERE==================================================
@@ -76,26 +72,22 @@ var spsGame = function (playerHand) {
 
   var WinnerResult = WinnerChecker(playerHand, computerhand);
 
-  console.log(WinnerResult);
-
   // This is what the player sees if they lose against the computer
   if (WinnerResult == `Player loses`) {
     roundCount = roundCount + 1;
     var msg = `${userName} you lose! You showed ${playerHand} and the computer showed ${computerhand}. Your total wins are ${winCount}.`;
-  }
-
-  // This is what the player sees if they win against the computer
-  if (WinnerResult == `Player win`) {
+  } else if (WinnerResult == `Player win`) {
     winCount = winCount + 1;
     roundCount = roundCount + 1;
     console.log(winCount);
-    var msg = `${userName} you win! You showed ${playerHand} and the computer showed ${computerhand}. Your total wins are ${winCount}`;
-  }
-
-  // This is what the player sees if there is a tie
-  if (WinnerResult == `Tie`) {
+    var msg = `${userName} you win! You showed ${playerHand} and the computer showed ${computerhand}. Your total wins are ${winCount}.`;
+  } // This is what the player sees if there is a tie
+  else if (WinnerResult == `Tie`) {
     roundCount = roundCount + 1;
-    var msg = `${userName} It is a tie! You showed ${playerHand} and the computer showed ${computerhand}. Your total wins are ${winCount}`;
+    var msg = `${userName} It is a tie! You showed ${playerHand} and the computer showed ${computerhand}. Your total wins are ${winCount}.`;
+  } else {
+    // input validation
+    msg = `Sorry, your input was not recognized. You can only enter scissors, paper, or stone. Please try again.`;
   }
 
   console.log(`=======CHECK=======`);
@@ -103,7 +95,6 @@ var spsGame = function (playerHand) {
   console.log(playerHand);
   console.log(`computer showed:`);
   console.log(computerhand);
-  console.log(WinnerResult);
 
   return msg;
 };
