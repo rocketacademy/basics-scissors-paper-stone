cbe2204abb01beb8825e06d6689c590cb5be65a9
weijiepoh <weijie.poh@gmail.com>
2021-10-18 16:30:08 +0800

    fixed unexpected end input

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //output "you chose" +input+ ". and the program chose" + randomchoice+ ". you win/lose."
 //generate randomchoice using Math.random() and rounddown with Math.floor
 
-var computerguess = function (){
+var computerguess = function () {
   var randomNum = Math.floor(Math.random() * 3);
   console.log(randomNum);
   if (randomNum == 0) {
@@ -11,43 +11,55 @@ var computerguess = function (){
   if (randomNum == 1) {
     return "paper";
   }
-    return "stone";
+  return "stone";
 };
 
 var playerwins = function (playerSPS, computerSPS) {
   return (
-        (playerSPS == "scissors" && computerSPS == "paper") ||
-        (playerSPS == "paper" && computerSPS == "stone") ||
-        (playerSPS == "stone" && computerSPS == "scissor") ||
+    (playerSPS == "scissors" && computerSPS == "paper") ||
+    (playerSPS == "paper" && computerSPS == "stone") ||
+    (playerSPS == "stone" && computerSPS == "scissor")
   );
 };
 
 var draw = function (playerSPS, computerSPS) {
-  return (
-    (playerSPS == computerSPS) 
-  );
+  return playerSPS == computerSPS;
 };
 
-var main = function (input) { 
+var main = function (input) {
   var playerSPS = input;
-  var computerSPS = computerguess ();
-  if (playerwins(playerSPS, computerSPS)){
-    return showplayercomputerSPS + "you entered "+ playerSPS + ", the computer has " + computerSPS + ". you win";
-  }
-
-  if (draw (playerSPS, computerSPS)){
-    return showplayercomputerSPS + "you entered "+ playerSPS + ", the computer has " + computerSPS + ". it's a draw";
-  }
-
-var showplayercomputerSPS = function (playerSPS, computerSPS) {
-  return (
-    'You picked ' +
-    playerSPS +
-    '. The computer chose ' +
-    computerSPS
+  var computerSPS = computerguess();
+  if (playerwins(playerSPS, computerSPS)) {
+    return (
+      showplayercomputerSPS +
+      "you entered " +
+      playerSPS +
+      ", the computer has " +
+      computerSPS +
+      ". you win"
     );
+  }
 
+  if (draw(playerSPS, computerSPS)) {
+    return (
+      showplayercomputerSPS +
+      "you entered " +
+      playerSPS +
+      ", the computer has " +
+      computerSPS +
+      ". it's a draw"
+    );
+  }
+
+  var showplayercomputerSPS = function (playerSPS, computerSPS) {
+    return "You picked " + playerSPS + ". The computer chose " + computerSPS;
+  };
   return (
-    showplayercomputerSPS + "you entered "+ playerSPS + ", the computer has " + computerSPS + ". you lost";
+    showplayercomputerSPS +
+    "you entered " +
+    playerSPS +
+    ", the computer has " +
+    computerSPS +
+    ". you lost"
   );
 };
