38b8a102aa7f573ae9c7a2144f2a1217f5cde73e
antinglimmy <antinglim@gmail.com>
2021-10-19 00:22:34 +0800

    I added in user names, win/lose records, and made the output  nice

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-//generate a number from 1-3
-// 1 = scissors, 2 = stone, 3 = paper
-// programme outputs either scissors, paper or stone
-// if user inputs scissors and programme outputs scissors = draw
-// if user inputs scissors and programme outputs paper = win
-// if user inputs scissors and programme outputs stone = lose
-// if user inputs paper and programme outputs paper = draw
-// if user inputs paper and programme outputs scissors = lose
-// if user inputs paper and programme outputs stone = win
-// if user inputs stone and programme outputs stone = draw
-// if user inputs stone and programme outputs paper = lose
-// if user inputs stone and programme outputs scissors = win
+// user types in their name and programme says hello
+// user types in scissors/paper/stone
+// Computer generates a number from 1-3
+// Assign 1 = scissors, 2 = stone, 3 = paper
+// Computer outputs either scissors, paper or stone
+// Output result and add to the win/lose/total game count
+
+//Global variables
+var winCount = 0;
+var loseCount = 0;
+var drawCount = 0;
+var totalGameCount = 0;
+var userName = "no user name";
+var myOutputValue = "";
 
 //generating a random number from 0-2
 var randomOutput = function () {
@@ -17,64 +19,114 @@ var randomOutput = function () {
   return randomNumber;
 };
 
+//Generating a function for when player wins
+var playerWins = function (userInput, computerInput) {
+  return (
+    (userInput == "scissors" && computerInput == "paper") ||
+    (userInput == "paper" && computerInput == "stone") ||
+    (userInput == "stone" && computerInput == "scissors")
+  );
+};
+
+//Generating a function for when player loses
+var playerLoses = function (userInput, computerInput) {
+  return (
+    (userInput == "scissors" && computerInput == "stone") ||
+    (userInput == "stone" && computerInput == "paper") ||
+    (userInput == "paper" && computerInput == "scissors")
+  );
+};
+
+//Generating a function for when player wins
+var playerDraws = function (userInput, computerInput) {
+  return (
+    (userInput == "scissors" && computerInput == "scissors") ||
+    (userInput == "paper" && computerInput == "paper") ||
+    (userInput == "stone" && computerInput == "stone")
+  );
+};
+
+//Main function
 var main = function (input) {
-  // if the user types in an invalid input
-  //if the computer generates 0, his guess is scissors
-  var randomAttack = randomOutput();
-  if (randomAttack == 0) {
-    var finalAttack = "scissors";
-    console.log(finalAttack);
+  // Set the user name
+  if (userName == "no user name") {
+    userName = input;
+    return `Hello ${userName}!`;
+
+    // Carry out the sps game
+    // I tried to put this whole block as a separate function above but it didnt work/i didnt know how to do it...so i had to add it back here
+  } else {
+    // Generate a random number from 0-2
+    randomAttack = randomOutput();
+
+    //Assign 0 to scissors, which means computer chose scissors
+    if (randomAttack == 0) {
+      var finalAttack = "scissors";
+      console.log("This is the computer choice:");
+      console.log(finalAttack);
+    }
+
+    //Assign 1 to stone, which means computer chose stone
+    if (randomAttack == 1) {
+      var finalAttack = "stone";
+      console.log("This is the computer choice:");
+      console.log(finalAttack);
+    }
+
+    //Assign 2 to paper, which means computer chose paper
+    if (randomAttack == 2) {
+      var finalAttack = "paper";
+      console.log("This is the computer choice:");
+      console.log(finalAttack);
+    }
+
+    // I don't know where this block should fit in...should it be an additional if function nested on top or??
+    //User validation
+    var words = ["scissors", "paper", "stone"];
+    if (!words.includes(input)) {
+      return "Please type scissors, paper or stone!!";
+    }
+
+    //Situation if player wins
+    if (playerWins(input, finalAttack)) {
+      winCount += 1;
+      totalGameCount += 1;
+      var winPercentage = Math.floor((winCount / totalGameCount) * 100);
+
+      //Customise output message depending on win percentage
+      if (winPercentage < 60) {
+        myOutputValue = `${userName}, you won! <br> You chose ${input} but the computer chose ${finalAttack} <br> Your current win count is ${winCount} and your win percentage is ${winPercentage}% You need to improve!`;
+      } else {
+        myOutputValue = `${userName}, you won! <br> You chose ${input} but the computer chose ${finalAttack} <br> Your current win count is ${winCount} and your win percentage is ${winPercentage}%`;
+      }
+    }
+
+    //Situation if player loses
+    if (playerLoses(input, finalAttack)) {
+      loseCount += 1;
+      totalGameCount += 1;
+      var winPercentage = Math.floor((winCount / totalGameCount) * 100);
+
+      //Customise output message depending on win percentage
+      if (winPercentage < 60) {
+        myOutputValue = `${userName}, you lose! <br> You chose ${input} but the computer chose ${finalAttack} <br> Your current lose count is ${loseCount} and your win percentage is ${winPercentage}% You need to improve!`;
+      } else {
+        myOutputValue = `${userName}, you lose! <br> You chose ${input} but the computer chose ${finalAttack} <br> Your current lose count is ${loseCount} and your win percentage is ${winPercentage}%`;
+      }
+    }
+
+    //Situation if player draws
+    if (playerDraws(input, finalAttack)) {
+      totalGameCount += 1;
+      var winPercentage = Math.floor((winCount / totalGameCount) * 100);
+
+      //Customise output message depending on win percentage
+      if (winPercentage < 60) {
+        myOutputValue = `${userName}, it's a draw! <br> You chose ${input}, and the computer too. <br> Your current win count is ${winCount} and your win percentage is ${winPercentage}% You need to improve!`;
+      } else {
+        myOutputValue = `${userName}, it's a draw! <br> You chose ${input}, and the computer too. <br> Your current win count is ${winCount} and your win percentage is ${winPercentage}%`;
+      }
+    }
   }
-
-  //if the computer generates 1, his guess is stone
-  if (randomAttack == 1) {
-    var finalAttack = "stone";
-    console.log(finalAttack);
-  }
-
-  //if the computer generates 2, his guess is paper
-  if (randomAttack == 2) {
-    var finalAttack = "paper";
-    console.log(finalAttack);
-  }
-
-  var myOutputValue = "Please type in either scissors, paper or stone";
-
-  if (input == "scissors" && finalAttack == "scissors") {
-    var myOutputValue = "Its a draw";
-  }
-
-  if (input == "scissors" && finalAttack == "paper") {
-    var myOutputValue = "You win";
-  }
-
-  if (input == "scissors" && finalAttack == "stone") {
-    var myOutputValue = "You lose";
-  }
-
-  if (input == "paper" && finalAttack == "scissors") {
-    var myOutputValue = "You lose";
-  }
-
-  if (input == "paper" && finalAttack == "paper") {
-    var myOutputValue = "It's a draw";
-  }
-
-  if (input == "paper" && finalAttack == "stone") {
-    var myOutputValue = "You win";
-  }
-
-  if (input == "stone" && finalAttack == "scissors") {
-    var myOutputValue = "You win";
-  }
-
-  if (input == "stone" && finalAttack == "paper") {
-    var myOutputValue = "You lose";
-  }
-
-  if (input == "stone" && finalAttack == "stone") {
-    var myOutputValue = "It's a draw";
-  }
-
-  return `${myOutputValue}! <br> You guessed ${input} but the computer chose ${finalAttack}. <br> Now you can type in scissors, paper or stone to play another round.`;
+  return myOutputValue;
 };
