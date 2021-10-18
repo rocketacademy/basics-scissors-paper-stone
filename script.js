35608b2a5d4961c28bb5eb3de545eb45bc011be8
jun <junjie@keijet.com>
2021-10-18 20:09:27 +0800

    Refactored

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-var main = function (input) {
-  const scissors = "scissors";
-  const paper = "paper";
-  const stone = "stone";
+var totalWins = 0;
+var totalLoss = 0;
+const scissors = "scissors";
+const paper = "paper";
+const stone = "stone";
 
-  
+var main = function (input) {
   if (input != scissors && input != paper && input != stone) {
     return `You keyed in an invalid option; choose from '${scissors}', '${paper}' or '${stone}'.`;
   }
 
-  var roll = rollDice();
-  let guess;
-
-  if (roll == 0) {
-    guess = scissors
-  } 
-  if (roll == 1) {
-    guess = paper
-  }
-  if (roll == 2) {
-    guess = stone
-  }
-
-  if (input == guess) {
-    return `The computer chose ${guess}, you chose ${input}. You win!`
+  var computerChoice = setWinningChoice();
+  if (input == computerChoice) {
+    totalWins = totalWins + 1;
+    return `
+    You have ${totalWins} wins and ${totalLoss} losses.
+    The computer chose ${computerChoice}, you chose ${input}. You win!
+    `
   } else {
-    return `The computer chose ${guess}, you chose ${input}. You lose!`
+    totalLoss = totalLoss + 1;
+    return `
+    You have ${totalWins} wins and ${totalLoss} losses.
+    The computer chose ${computerChoice}, you chose ${input}. You lose!
+    `
   }
   
 };
@@ -36,3 +33,17 @@ var rollDice = function () {
   var resultInteger = Math.floor(randomFloat);  
   return resultInteger;
 };
+
+var setWinningChoice = function() {
+  var roll = rollDice();
+
+  if (roll == 0) {
+    return scissors;
+  } 
+  if (roll == 1) {
+    return paper;
+  }
+  if (roll == 2) {
+    return stone;
+  }
+}
