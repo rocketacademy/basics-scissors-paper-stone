a45025bd62667bfd896a3c98155c79d8425dd919
jovlst <sheanthieng@gmail.com>
2021-10-19 20:28:36 +0800

    I did the 2nd part of sps

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 //Create a game of scissors, paper, and stone.
-// rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
+
+// Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
+
+// Keeping track of the number of times the user has won or loss
+var winCount = 0;
+// Keeping trakc the number of games played
+var gamesPlayed = 0;
+
+// Waiting for username
+var currentGameMode = "waiting for user name";
+var UserName = "";
 
 // Design system to generate numbers which will be assigned to scissors, paper and stone
 var systemGenerator = function (input) {
@@ -11,13 +21,19 @@ var systemGenerator = function (input) {
   console.log("The system chose " + systemChoice);
 
   // creating a default response for losing
-  var myOutputValue = `The computer chose ${systemChoice}. You chose ${input}. Boohoo! You lose! Now you can type "scissors" "paper" or "stone" to play another round!`;
+  // minus one win count for losers
+  winCount = winCount - 1;
+  gamesPlayed = gamesPlayed + 1;
+  // what users will see if they lose
+  var myOutputValue = `The computer chose ${systemChoice}. You chose ${input}. You lose! Bummer! <br> So far ${userName}, your score is ${winCount}/${gamesPlayed}! <br> Keep playing!`;
   console.log("losing response");
 
   //create a response for draw where input and system generated results are the same
   if (input == systemChoice) {
     console.log("draw response");
-    myOutputValue = `The computer chose ${systemChoice}. You chose ${input}. Draw! Bummer. Now you can type "scissors" "paper" or "stone" to play another round!`;
+    // no change to the winning count
+    winCount = winCount + 1;
+    myOutputValue = `The computer chose ${systemChoice}. You chose ${input}. Draw! Bummer. <br> So far ${userName}, your score is ${winCount}/${gamesPlayed}! <br> Keep playing!`;
   }
 
   // Create a sequence to win when scissors beat paper and paper beat stone and stone beat scissors
@@ -28,13 +44,29 @@ var systemGenerator = function (input) {
   ) {
     console.log("winning response");
     // create a response for winning
-    myOutputValue = `The computer chose ${systemChoice}. You chose ${input}. You win! Now you can type "scissors" "paper" or "stone" to play another round!`;
+    // to add win count for users
+    winCount = winCount + 2;
+    myOutputValue = `The computer chose ${systemChoice}. You chose ${input}. You win! <br> So far ${userName}, your score is ${winCount}/${gamesPlayed}. Pretty good! <br> Keep playing!`;
   }
   return myOutputValue;
 };
 
-// call out function
 var main = function (input) {
-  var myOutputValue = systemGenerator(input);
+  var myOutputValue = "";
+
+  //do this if computer is waiting for user name
+  if (currentGameMode == "waiting for user name") {
+    userName = input;
+
+    // Now we have the name, we will switch the game mode
+    currentGameMode = "Scissors, Paper, Stone";
+
+    myOutputValue = "Hello " + userName;
+    console.log("user typed in " + userName);
+  } else if (currentGameMode == "Scissors, Paper, Stone") {
+    // generating result
+    var myOutputValue = systemGenerator(input);
+  }
+
   return myOutputValue;
 };
