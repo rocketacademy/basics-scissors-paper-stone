cb9a1aba2facebbe82d0bcad18f596166ab7e12d
hoyj08 <hoyj08@gmail.com>
2021-10-19 19:41:17 +0800

    part 2 changes

--- script.js
+++ script.js
@@ -1,131 +1,10 @@
-var main = function (input) {
-  if (
-    input != "scissors" ||
-    input != "paper" ||
-    input != "rock" ||
-    input != "reversed scissors" ||
-    input != "reversed paper" ||
-    input != "reversed rock"
-  ) {
-    myOutputValue =
-      "Please try again and input the following : Scissors, Paper, Rock, Reversed Scissors, Reversed Paper, Reversed Rock";
-  }
-  var randomChoice = generateRandomChoice();
-  var randomChoiceReversed = generateRandomChoiceReversed(); // for reversed game
-  console.log("randomChoice");
-  console.log(randomChoice);
-
-  // draw scenarios
-  if (input == randomChoice) {
-    myOutputValue =
-      "The computer chose " +
-      randomChoice +
-      ". <br>" +
-      "You chose " +
-      input +
-      ". <br><br>" +
-      "It's a draw! <br><br>" +
-      "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game! ";
-  }
-
-  // if input wins generateRandomChoice, output = you win
-  // input = scissors, win randomchoice = paper
-  if (
-    (input == "scissors" && randomChoice == CHOICE_PAPER) ||
-    // input paper, win randomchoice = rock
-    (input == "paper" && randomChoice == CHOICE_ROCK) ||
-    // input rock , win randomchoice = scissors
-    (input == "rock" && randomChoice == CHOICE_SCISSORS)
-  ) {
-    myOutputValue =
-      "The computer chose " +
-      randomChoice +
-      ". <br>" +
-      "You chose " +
-      input +
-      ". <br><br>" +
-      "Congratulations! It's a win! <br><br>" +
-      "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game!";
-  }
-  // lose scenarios
-  if (
-    (input == "scissors" && randomChoice == CHOICE_ROCK) ||
-    (input == "paper" && randomChoice == CHOICE_SCISSORS) ||
-    (input == "rock" && randomChoice == CHOICE_PAPER)
-  ) {
-    myOutputValue =
-      "The computer chose " +
-      randomChoice +
-      ". <br>" +
-      "You chose " +
-      input +
-      ". <br><br>" +
-      "You lose! Bummer. <br><br>" +
-      "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game!";
-  } //else
-  //var myOutputValue = "Please input the following : scissors , paper , rock";
-  console.log("myOutputValue for else");
-  console.log(myOutputValue);
-  //var randomChoice = generateRandomChoice(input);
-  //console.log("program choice");
-  //console.log(randomChoice);
-
-  // reversed choices scenarios
-  //reversed choice draw
-  if (input == randomChoiceReversed) {
-    myOutputValue =
-      "The computer chose " +
-      randomChoiceReversed +
-      ". <br>" +
-      "You chose " +
-      input +
-      ". <br><br>" +
-      "It's a draw! <br><br>" +
-      "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game! ";
-  }
-
-  // reversed win scenarios
-  // input = scissors, win randomchoice = paper
-  if (
-    (input == "reversed scissors" &&
-      randomChoiceReversed == REVERSED_CHOICE_ROCK) ||
-    // input paper, win randomchoice = rock
-    (input == "reversed paper" &&
-      randomChoiceReversed == REVERSED_CHOICE_SCISSORS) ||
-    // input rock , win randomchoice = scissors
-    (input == "reversed rock" && randomChoiceReversed == REVERSED_CHOICE_PAPER)
-  ) {
-    myOutputValue =
-      "The computer chose " +
-      randomChoiceReversed +
-      ". <br>" +
-      "You chose " +
-      input +
-      ". <br><br>" +
-      "Congratulations! It's a win! <br><br>" +
-      "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game!";
-  }
-  // reversed lose scenarios
-  if (
-    (input == "reversed scissors" &&
-      randomChoiceReversed == REVERSED_CHOICE_PAPER) ||
-    (input == "reversed paper" &&
-      randomChoiceReversed == REVERSED_CHOICE_ROCK) ||
-    (input == "reversed rock" &&
-      randomChoiceReversed == REVERSED_CHOICE_SCISSORS)
-  ) {
-    myOutputValue =
-      "The computer chose " +
-      randomChoiceReversed +
-      ". <br>" +
-      "You chose " +
-      input +
-      ". <br><br>" +
-      "You lose! Bummer. <br><br>" +
-      "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game!";
-  }
-  return myOutputValue;
-};
+//global variable
+//win-loss record
+var userWonCount = 0;
+var cpuWonCount = 0;
+//user name input
+var currentGameMode = `awaiting user name`;
+var userName = "";
 
 // Define Choices
 var CHOICE_SCISSORS = "scissors";
@@ -173,4 +52,174 @@ var generateRandomChoiceReversed = function () {
   if (randomChoiceNumberReversed == 3) {
     return REVERSED_CHOICE_ROCK;
   }
+};
+
+var main = function (input) {
+  var myOutputValue = "";
+  if (currentGameMode == `awaiting user name`) {
+    // set the name
+    if (
+      input == "scissors" ||
+      input == "paper" ||
+      input == "rock" ||
+      input == "reversed scissors" ||
+      input == "reversed paper" ||
+      input == "reversed rock"
+    ) {
+      myOutputValue = "Please input your name";
+    } else {
+      userName = input;
+      // now that we have the name, switch the mode
+      currentGameMode = "dice game";
+      myOutputValue = `Hello ` + userName;
+    }
+  } else if (currentGameMode == "dice game") {
+    //dice game logic
+    if (
+      input != "scissors" ||
+      input != "paper" ||
+      input != "rock" ||
+      input != "reversed scissors" ||
+      input != "reversed paper" ||
+      input != "reversed rock"
+    ) {
+      myOutputValue =
+        "Please try again and input the following : Scissors, Paper, Rock, Reversed Scissors, Reversed Paper, Reversed Rock";
+    }
+    var randomChoice = generateRandomChoice();
+    var randomChoiceReversed = generateRandomChoiceReversed(); // for reversed game
+    console.log("randomChoice");
+    console.log(randomChoice);
+    console.log("myoutputvalue");
+    console.log(myOutputValue);
+
+    // draw scenarios
+    if (input == randomChoice) {
+      myOutputValue =
+        "The computer chose " +
+        randomChoice +
+        ". <br>" +
+        "You chose " +
+        input +
+        ". <br><br>" +
+        "It's a draw! <br><br>" +
+        "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game! <br><br> Your win-loss record is: <br> Your win record: " +
+        userWonCount +
+        `<br> CPU win record :` +
+        cpuWonCount;
+    }
+
+    // if input wins generateRandomChoice, output = you win
+    if (
+      // input = scissors, win randomchoice = paper
+      (input == "scissors" && randomChoice == CHOICE_PAPER) ||
+      // input paper, win randomchoice = rock
+      (input == "paper" && randomChoice == CHOICE_ROCK) ||
+      // input rock , win randomchoice = scissors
+      (input == "rock" && randomChoice == CHOICE_SCISSORS)
+    ) {
+      userWonCount = userWonCount + 1; //user win + 1
+      myOutputValue =
+        "The computer chose " +
+        randomChoice +
+        ". <br>" +
+        "You chose " +
+        input +
+        ". <br><br>" +
+        "Congratulations! It's a win! <br><br>" +
+        "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game! <br><br> Your win-loss record is: <br> Your win record: " +
+        userWonCount +
+        `<br> CPU win record :` +
+        cpuWonCount;
+    }
+
+    // lose scenarios
+    if (
+      (input == "scissors" && randomChoice == CHOICE_ROCK) ||
+      (input == "paper" && randomChoice == CHOICE_SCISSORS) ||
+      (input == "rock" && randomChoice == CHOICE_PAPER)
+    ) {
+      cpuWonCount = cpuWonCount + 1; // cpu win +1
+      myOutputValue =
+        "The computer chose " +
+        randomChoice +
+        ". <br>" +
+        "You chose " +
+        input +
+        ". <br><br>" +
+        "You lose! Bummer. <br><br>" +
+        "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game! <br><br> Your win-loss record is: <br> Your win record: " +
+        userWonCount +
+        `<br> CPU win record :` +
+        cpuWonCount;
+    }
+    console.log("myOutputValue for else");
+    console.log(myOutputValue);
+
+    // reversed choices scenarios
+    //reversed choice draw
+    if (input == randomChoiceReversed) {
+      myOutputValue =
+        "The computer chose " +
+        randomChoiceReversed +
+        ". <br>" +
+        "You chose " +
+        input +
+        ". <br><br>" +
+        "It's a draw! <br><br>" +
+        "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game! Your win-loss record is: <br> Your win record: " +
+        userWonCount +
+        `<br> CPU win record :` +
+        cpuWonCount;
+    }
+
+    // reversed win scenarios
+    if (
+      (input == "reversed scissors" &&
+        randomChoiceReversed == REVERSED_CHOICE_ROCK) ||
+      (input == "reversed paper" &&
+        randomChoiceReversed == REVERSED_CHOICE_SCISSORS) ||
+      (input == "reversed rock" &&
+        randomChoiceReversed == REVERSED_CHOICE_PAPER)
+    ) {
+      userWonCount = userWonCount + 1; //user win + 1
+      myOutputValue =
+        "The computer chose " +
+        randomChoiceReversed +
+        ". <br>" +
+        "You chose " +
+        input +
+        ". <br><br>" +
+        "Congratulations! It's a win! <br><br>" +
+        "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game! Your win-loss record is: <br> Your win record: " +
+        userWonCount +
+        `<br> CPU win record :` +
+        cpuWonCount;
+    }
+
+    // reversed lose scenarios
+    if (
+      (input == "reversed scissors" &&
+        randomChoiceReversed == REVERSED_CHOICE_PAPER) ||
+      (input == "reversed paper" &&
+        randomChoiceReversed == REVERSED_CHOICE_ROCK) ||
+      (input == "reversed rock" &&
+        randomChoiceReversed == REVERSED_CHOICE_SCISSORS)
+    ) {
+      cpuWonCount = cpuWonCount + 1; //cpu win
+      myOutputValue =
+        "The computer chose " +
+        randomChoiceReversed +
+        ". <br>" +
+        "You chose " +
+        input +
+        ". <br><br>" +
+        "You lose! Bummer. <br><br>" +
+        "Now you can type scissors, paper or rock to play another round! <br> You can also type Reversed Scissors, Reversed Paper, Reversed Rock to try the Reversed Game!Your win-loss record is: <br> Your win record: " +
+        userWonCount +
+        `<br> CPU win record :` +
+        cpuWonCount;
+    }
+  }
+  return myOutputValue;
 };
