eb62b2fe6fddbf4c1839fe01e02760f4cbe93eaa
shingoh24 <oh.shiying@gmail.com>
2021-10-19 23:57:42 +0800

    help my [art 2 code has errors

--- script.js
+++ script.js
@@ -1,22 +1,67 @@
+var AWAITING_USER_NAME = "awaiting user name";
+var PSP_GAME = "psp game";
+var currentGameMode = AWAITING_USER_NAME;
+var userName = "";
+
+console.log("global");
+
+var winCount = 0;
+var loseCount = 0;
+var drawCount = 0;
+
+var totalCount = function (winCount, loseCount, drawCount) {
+  return Number(winCount) + Number(loseCount) + Number(drawCount);
+};
+console.log("stats");
+
+var winPercentage = function () {
+  return Math.floor((Number(winCount) * 100) / totalCount);
+};
+
+console.log("percent");
+
 var main = function (input) {
-  var gamePlay = "";
-  var lowercaseInput = input.toLowerCase();
-  var gameNumber = generateRandomNumber();
-  if (gameNumber == 1) {
-    gamePlay = "scissors";
+  var myOutputValue = "";
+
+  if ((currentGameMode = AWAITING_USER_NAME)) {
+    userName = input;
+    currentGameMode = PSP_GAME;
+    myOutputValue =
+      "Welcome to the SPS club, " +
+      userName +
+      "!" +
+      "<br><br>" +
+      "Please start the game by choosing either scissors, paper or stone.";
+
+    console.log(main);
+  } else if ((currentGameMode = PSP_GAME)) {
+    var gamePlay = "";
+    var lowercaseInput = input.toLowerCase();
+    var gameNumber = generateRandomNumber();
+
+    if (gameNumber == 1) {
+      gamePlay = "scissors";
+    }
+    if (gameNumber == 2) {
+      gamePlay = "paper";
+    }
+    if (gameNumber == 3) {
+      gamePlay = "stone";
+    }
+    var myOutputValue =
+      "Tough luck," +
+      userName +
+      "<br> You lost! You played: " +
+      input +
+      ", the computer played: " +
+      gamePlay +
+      "<br> Try again! <br> Wins:" +
+      Number(winCount) +
+      " <br> Losses: " +
+      Number(loseCount) +
+      "Winninig percentage: " +
+      winPercentage;
   }
-  if (gameNumber == 2) {
-    gamePlay = "paper";
-  }
-  if (gameNumber == 3) {
-    gamePlay = "stone";
-  }
-  var myOutputValue =
-    "you lose! you played: " +
-    input +
-    ", the computer played: " +
-    gamePlay +
-    ", try again! ";
 
   if (
     (gamePlay == "scissors" && lowercaseInput == "stone") ||
@@ -24,20 +69,34 @@ var main = function (input) {
     (gamePlay == "paper" && lowercaseInput == "scissors")
   ) {
     myOutputValue =
-      "you win! you played: " +
+      "Wow!" +
+      userName +
+      " <br> You won! You played: " +
       input +
       ", the computer played: " +
       gamePlay +
-      "well done!";
+      " <br> Well done! Please enter 'secret' to activate the new game mode! <br> Wins:" +
+      Number(winCount) +
+      " <br> Losses: " +
+      Number(loseCount) +
+      "Winninig percentage: " +
+      winPercentage;
   }
 
   if (lowercaseInput == gamePlay) {
     myOutputValue =
-      "it's a draw! you played: " +
+      "What are the odds?" +
+      userName +
+      "<br> It's a draw! You played: " +
       input +
       ", the computer played: " +
       gamePlay +
-      " try again! ";
+      "<br> Try a little harder! <br> Wins:" +
+      Number(winCount) +
+      " <br> Losses: " +
+      Number(loseCount) +
+      "Winninig percentage: " +
+      winPercentage;
   }
   if (
     !(
@@ -51,6 +110,7 @@ var main = function (input) {
   }
   return myOutputValue;
 };
+
 var generateRandomNumber = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal);
