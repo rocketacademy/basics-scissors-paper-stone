f08a07ca87130e5d1a0915a0b2fa96fd2ed4b9f6
ginkoka <davidlok@gmail.com>
2021-10-19 20:12:56 +0700

    Part 2 completed

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var currentGameMode = "waiting for user name";
+var userName = "";
 var computerChoice = "";
 var gamesPlayed = 0;
 var gamesWon = 0;
@@ -17,55 +19,51 @@ var computerPlay = function () {
 };
 
 var main = function (input) {
-  if (input !== "Stone" && input !== "Paper" && input !== "Scissors") {
-    var myOutputValue = "Please enter Stone, Paper, or Scissors.";
-    return myOutputValue;
+  if (currentGameMode == "waiting for user name") {
+    currentGameMode = "gamemode";
+    userName = input;
+    myOutputValue = "Hello " + userName;
+  } else if (currentGameMode == "gamemode") {
+    if (input !== "Stone" && input !== "Paper" && input !== "Scissors") {
+      var myOutputValue = "Please enter Stone, Paper, or Scissors.";
+      return myOutputValue;
+    }
+
+    gamesPlayed = gamesPlayed + 1;
+
+    computerChoice = computerPlay();
+
+    var Result = "It's a draw.";
+
+    if (
+      (input == "Stone" && computerChoice == "Paper") ||
+      (input == "Scissors" && computerChoice == "Stone") ||
+      (input == "Paper" && computerChoice == "Scissors")
+    ) {
+      var Result = "You Lost.";
+    }
+
+    if (
+      (input == "Stone" && computerChoice == "Scissors") ||
+      (input == "Paper" && computerChoice == "Stone") ||
+      (input == "Paper" && computerChoice == "Stone")
+    ) {
+      var Result = "You Won.";
+      gamesWon = gamesWon + 1;
+    }
+
+    var myOutputValue =
+      "The computer chose " +
+      computerChoice +
+      " and you chose " +
+      input +
+      ". " +
+      Result +
+      " You've won " +
+      gamesWon +
+      "/" +
+      gamesPlayed +
+      " games.";
   }
-
-  gamesPlayed = gamesPlayed + 1;
-
-  computerChoice = computerPlay();
-
-  var Result = "It's a draw.";
-
-  if (input == "Stone" && computerChoice == "Paper") {
-    var Result = "You Lost.";
-  }
-
-  if (input == "Stone" && computerChoice == "Scissors") {
-    var Result = "You Won.";
-    gamesWon = gamesWon + 1;
-  }
-
-  if (input == "Scissors" && computerChoice == "Stone") {
-    var Result = "You Lost.";
-  }
-
-  if (input == "Scissors" && computerChoice == "Paper") {
-    var Result = "You Won.";
-    gamesWon = gamesWon + 1;
-  }
-
-  if (input == "Paper" && computerChoice == "Stone") {
-    var Result = "You Won.";
-    gamesWon = gamesWon + 1;
-  }
-
-  if (input == "Paper" && computerChoice == "Scissors") {
-    var Result = "You Lost.";
-  }
-
-  var myOutputValue =
-    "The computer chose " +
-    computerChoice +
-    " and you chose " +
-    input +
-    ". " +
-    Result +
-    " You've won " +
-    gamesWon +
-    "/" +
-    gamesPlayed +
-    " games.";
   return myOutputValue;
 };
