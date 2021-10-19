b428eaed56a5a440f79339dcc79ca75672da5bf6
kathliu <ekatherinaliu@outlook.com>
2021-10-19 21:42:56 +0800

    here is part 2

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
+var currentGameMode = "waiting for user name";
+var userName = "";
+var userScore = 0;
+var compScore = 0;
+var drawScore = 0;
+
 var main = function (input) {
-  var randomChoice = randomNumber();
-  var compChoice = "";
+  var myOutputValue = "";
 
-  //computer logic
-  if (randomChoice == 1) {
-    compChoice = "stone";
-  }
-  if (randomChoice == 2) {
-    compChoice = "paper";
-  }
-  if (randomChoice == 3) {
-    compChoice = "scissors";
-  }
+  if (currentGameMode == "waiting for user name") {
+    //set the name
+    userName = input;
 
-  var myOutputValue = `Haha, lets enter only Scissors, Paper, or Stone!`;
+    //now that we have the name; switch the mode
+    currentGameMode = "SPS game";
 
-  //it's a draw
-  if (input == compChoice) {
-    myOutputValue = `it's a draw! you entered ${input} and the computer choice is ${compChoice}!`;
-  }
-  //you win
-  if (
-    (input == "scissors" && compChoice == "paper") ||
-    (input == "paper" && compChoice == "stone") ||
-    (input == "stone" && compChoice == "scissors")
-  ) {
-    myOutputValue = `you win! you entered ${input} and the computer choice is ${compChoice}!`;
-  }
-  //you lose
-  if (
-    (input == "scissors" && compChoice == "stone") ||
-    (input == "paper" && compChoice == "scissors") ||
-    (input == "stone" && compChoice == "paper")
-  ) {
-    myOutputValue = `you lose! you entered ${input} and the computer choice is ${compChoice}!`;
+    myOutputValue = `Hello ${userName}`;
+  } else if (currentGameMode == "SPS game") {
+    var randomChoice = randomNumber();
+    var compChoice = "";
+
+    //computer logic
+    if (randomChoice == 1) {
+      compChoice = "stone";
+    }
+    if (randomChoice == 2) {
+      compChoice = "paper";
+    }
+    if (randomChoice == 3) {
+      compChoice = "scissors";
+    }
+
+    var myOutputValue = `Haha ${userName}, lets enter only Scissors, Paper, or Stone!`;
+
+    //it's a draw
+    if (input == compChoice) {
+      drawScore = drawScore + 1;
+      myOutputValue = `it's a draw! you entered ${input} and the computer choice is ${compChoice}! The total amount of draw is ${drawScore}`;
+    }
+    //you win
+    if (
+      (input == "scissors" && compChoice == "paper") ||
+      (input == "paper" && compChoice == "stone") ||
+      (input == "stone" && compChoice == "scissors")
+    ) {
+      myOutputValue = `you win ${userName}! you entered ${input} and the computer choice is ${compChoice}! <br> So far your winning score is ${(userScore =
+        userScore + 1)}. Pretty good`;
+    }
+    //you lose
+    if (
+      (input == "scissors" && compChoice == "stone") ||
+      (input == "paper" && compChoice == "scissors") ||
+      (input == "stone" && compChoice == "paper")
+    ) {
+      myOutputValue = `you lose ${userName}! you entered ${input} and the computer choice is ${compChoice}! Comp score is ${(compScore =
+        compScore + 1)}.`;
+    }
   }
   return myOutputValue;
 };
