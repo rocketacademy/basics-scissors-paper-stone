717e31a50793162653632d3a9d19ce449835cbd4
CheongYuting <cheongyuting.design@gmail.com>
2021-10-19 20:57:22 +0800

    edited the boolean code to &&

--- script.js
+++ script.js
@@ -1,93 +1,108 @@
+var currentGameMode = "awaiting username";
+var username = "";
 var userwins = 0;
 var systemwins = 0;
 
 var main = function (input) {
   var lowercaseinput = input.toLowerCase(input);
 
-  var number = chooseobject();
-
-  // lets assign an object to each number generated
-  var systemobject = "";
-
-  if (number == 1) {
-    systemobject = "scissors";
-  }
-  if (number == 2) {
-    systemobject = "paper";
-  }
-  if (number == 3) {
-    systemobject = "stone";
-  }
-
   var myOutputValue = "";
 
-  // lets define how system can win
-  if (
-    (lowercaseinput == "scissors" && systemobject == "stone") ||
-    (lowercaseinput == "paper" && systemobject == "scissors") ||
-    (lowercaseinput == "stone" && systemobject == "paper")
-  ) {
-    systemwins = systemwins + 1;
+  // let's define the game modes
+  if (currentGameMode == "awaiting username") {
+    username = input;
+    currentGameMode = "spsgame";
     myOutputValue =
-      "Ops, you lost. " +
+      "Welcome to the game, " +
+      username +
+      "." +
       "<br>" +
-      "My " +
-      systemobject +
-      " beats your " +
-      lowercaseinput +
-      "!" +
-      "<br>" +
-      " Your score: " +
-      userwins +
-      " | My score: " +
-      systemwins;
-  }
+      "Start by choosing 'scissors', 'paper' or 'stone'.";
+  } else if (currentGameMode == "spsgame") {
+    console.log();
+    // start game
+    var number = chooseobject();
 
-  // lets define how a user can win
-  if (
-    (lowercaseinput == "scissors" && systemobject == "paper") ||
-    (lowercaseinput == "paper" && systemobject == "stone") ||
-    (lowercaseinput == "stone" && systemobject == "scissors")
-  ) {
-    userwins = userwins + 1;
-    myOutputValue =
-      "Dang, you won!" +
-      "<br>" +
-      "Your " +
-      lowercaseinput +
-      " beats my " +
-      systemobject +
-      "!" +
-      "<br>" +
-      "Your score: " +
-      userwins +
-      " | My score: " +
-      systemwins;
-  }
+    // lets assign an object to each number generated
+    var systemobject = "";
 
-  // lets define a draw
-  if (lowercaseinput == systemobject) {
-    myOutputValue =
-      "Ooo, you read my mind." +
-      "<br>" +
-      "I chose " +
-      systemobject +
-      " too. It's a draw!" +
-      "<br>" +
-      "Your score: " +
-      userwins +
-      " | My score: " +
-      systemwins;
-  }
+    if (number == 1) {
+      systemobject = "scissors";
+    }
+    if (number == 2) {
+      systemobject = "paper";
+    }
+    if (number == 3) {
+      systemobject = "stone";
+    }
+    // lets define how system can win
+    if (
+      (lowercaseinput == "scissors" && systemobject == "stone") ||
+      (lowercaseinput == "paper" && systemobject == "scissors") ||
+      (lowercaseinput == "stone" && systemobject == "paper")
+    ) {
+      systemwins = systemwins + 1;
+      myOutputValue =
+        "Ops, you lost. " +
+        "<br>" +
+        "My " +
+        systemobject +
+        " beats your " +
+        lowercaseinput +
+        "!" +
+        "<br>" +
+        " Your score: " +
+        userwins +
+        " | My score: " +
+        systemwins;
+    }
 
-  // lets add input validation
-  if (
-    lowercaseinput != "scissors" ||
-    lowercaseinput != "paper" ||
-    lowercaseinput != "stone"
-  ) {
-    myOutputValue =
-      "Huh? I didn't get that. You're supposed to choose either 'scissors', 'paper' or 'stone'. Let's try again!";
+    // lets define how a user can win
+    if (
+      (lowercaseinput == "scissors" && systemobject == "paper") ||
+      (lowercaseinput == "paper" && systemobject == "stone") ||
+      (lowercaseinput == "stone" && systemobject == "scissors")
+    ) {
+      userwins = userwins + 1;
+      myOutputValue =
+        "Dang, you won!" +
+        "<br>" +
+        "Your " +
+        lowercaseinput +
+        " beats my " +
+        systemobject +
+        "!" +
+        "<br>" +
+        "Your score: " +
+        userwins +
+        " | My score: " +
+        systemwins;
+    }
+
+    // lets define a draw
+    if (lowercaseinput == systemobject) {
+      myOutputValue =
+        "Ooo, you read my mind." +
+        "<br>" +
+        "I chose " +
+        systemobject +
+        " too. It's a draw!" +
+        "<br>" +
+        "Your score: " +
+        userwins +
+        " | My score: " +
+        systemwins;
+    }
+
+    // lets add input validation
+    if (
+      lowercaseinput != "scissors" &&
+      lowercaseinput != "paper" &&
+      lowercaseinput != "stone"
+    ) {
+      myOutputValue =
+        "Huh? I didn't get that. You're supposed to choose either 'scissors', 'paper' or 'stone'. Let's try again!";
+    }
   }
 
   return myOutputValue;
@@ -101,3 +116,5 @@ var chooseobject = function () {
 
   return randomNumber;
 };
+console.log("is random number generated");
+console.log(randomNumber);
