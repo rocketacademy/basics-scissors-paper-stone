a998800636b8f9bc1041db785388d3e17258668a
kangyaowg <kangyaowg@gmail.com>
2021-10-19 21:07:21 +0800

    take our korean to outside function

--- script.js
+++ script.js
@@ -1,135 +1,123 @@
 //Global Variable
-var username =0
-var computerWin = 0
-var userWin = 0
-var roundCount = 0
-var gamemode = 0
-var firstWinner = 0     //0 = no lead, 1 = User lead, 2 = computer lead
-var ultiWinner = 0
+var username = 0;
+var computerWin = 0;
+var userWin = 0;
+var roundCount = 0;
+var gamemode = 0;
+var firstWinner = 0; //0 = no lead, 1 = User lead, 2 = computer lead
+var ultiWinner = 0;
 
 var main = function (input) {
   // input username
-  if (username == 0){
-    username = input
-    return "Hi "+username+" :) <br> Please select game mode  (normal/reversed/korean/computer)"
-  } else{   
-  // select game mode
-  if (gamemode == 0 && input == "normal"){
-    gamemode = input
-    return "Normal mode selected. Please enter scissors paper or stone"
-  }else if (gamemode == 0 && input == "reversed"){
-    gamemode = input
-    return "Reversed mode selected. Please enter scissors paper or stone"
-  }else if (gamemode == 0 && input == "korean"){
-      gamemode = input
-      return "Korean mode selected. Please enter scissors paper or stone"
-  }else if (gamemode == 0 && input == "computer"){
-      gamemode = input
-      return "Computer mode selected. Computer will choose your input, please press submit again"
-  }else if (gamemode == 0 && (input != "normal" || input != "reversed")){
-    return "Hi "+username+" :) <br> Please select game mode  (normal/reversed/korean/computer)"
-  }
+  if (username == 0) {
+    username = input;
+    return (
+      "Hi " +
+      username +
+      " :) <br> Please select game mode  (normal/reversed/korean/computer)"
+    );
+  } else {
+    // select game mode
+    if (gamemode == 0 && input == "normal") {
+      gamemode = input;
+      return "Normal mode selected. Please enter scissors paper or stone";
+    } else if (gamemode == 0 && input == "reversed") {
+      gamemode = input;
+      return "Reversed mode selected. Please enter scissors paper or stone";
+    } else if (gamemode == 0 && input == "korean") {
+      gamemode = input;
+      return "Korean mode selected. Please enter scissors paper or stone";
+    } else if (gamemode == 0 && input == "computer") {
+      gamemode = input;
+      return "Computer mode selected. Computer will choose your input, please press submit again";
+    } else if (gamemode == 0 && (input != "normal" || input != "reversed")) {
+      return (
+        "Hi " +
+        username +
+        " :) <br> Please select game mode  (normal/reversed/korean/computer)"
+      );
+    }
   }
 
   //Computer Version
-  if (gamemode == "computer"){
+  if (gamemode == "computer") {
     programHand = chooseHand();
     input = chooseHand();
     gameStatus = checkWin(input, programHand);
     return (
-      "You selected "+ gamemode +" mode"+
+      "You selected " +
+      gamemode +
+      " mode" +
       "<br>Computer selected " +
       programHand +
-      "<br>" +username+" selected " +
+      "<br>" +
+      username +
+      " selected " +
       input +
       "<br><br>" +
-      gameStatus)
+      gameStatus
+    );
   }
 
   //korean version
-  if (gamemode == "korean"){
-    while (ultiWinner == 0){
-    if (
-      input != "scissors" &&
-      input != "stone" &&
-      input != "paper"
-    ){
-      output =
-        "Please input again either (paper) (scissors) (stone)";
-      return output;
-    }else if (input == "scissors" || input == "paper" || input == "stone") {
-      programHand = chooseHand();
-      gameStatus = checkWin(input, programHand);
-    }
-    if(firstWinner == 0 && gameStatus == "You Win!"){                           // User wins first round
-      firstWinner = 1
-      return "You Win the first round! Input 2nd round guess"
-    }else if (firstWinner == 0 && gameStatus == "You Lose!"){                   // Computer wins first round
-      firstWinner = 2
-      return "You Lose the first round! Avoid guess the same as computer!"
-    }else if (firstWinner == 0 && gameStatus == "Its a Draw!"){                 // First round draws
-      return "Draw! input again"
-    }else if (firstWinner == 1 && gameStatus == "Its a Draw!"){                 // User wins first round and draws second round, User is ultimate winner
-      ultiWinner == 1
-      return "You are the ultimate Winner!"
-    }else if (firstWinner == 2 && gameStatus == "Its a Draw!"){                 // Computer wins first round and draws second round, Computer is ultimate winner
-      ultiWinner == 2
-      return "You lose! Computer is ultimate winner!"
-    }else if (firstWinner == 1 && gameStatus != "Its a Draw!"){                 // User wins first round but not draw second round
-      firstWinner = 0
-      return "You lose second round! Try again from first round"
-    }else if (firstWinner == 2 && gameStatus != "Its a Draw!"){                 // Computer wins first round but not draw second round
-      firstWinner = 0
-      return "Computer loses second round! Try again"
-    }
-  }
+  if (gamemode == "korean") {
+    return korean(input);
   }
 
   // check input & generate result based on normal gamemode
-  if (
-    input != "scissors" &&
-    input != "stone" &&
-    input != "paper"
-  ){
-    output =
-      "Please input again either (paper) (scissors) (stone)";
+  if (input != "scissors" && input != "stone" && input != "paper") {
+    output = "Please input again either (paper) (scissors) (stone)";
     return output;
   } else if (input == "scissors" || input == "paper" || input == "stone") {
     programHand = chooseHand();
     gameStatus = checkWin(input, programHand);
   }
-  if (gamemode == "normal"){
-    if (gameStatus == "You Win!") {  
-      console.log("normal user win")             //User wins normal
+  if (gamemode == "normal") {
+    if (gameStatus == "You Win!") {
+      console.log("normal user win"); //User wins normal
       userWin = userWin + 1;
-  }else if (gameStatus = "You Lose!") {         //Computer wins normal
-      console.log("normal computer win")
-      computerWin = computerWin +1;
-  }}
-  else if (gamemode == "reversed"){ 
-    if (gameStatus == "You Win!") {               //User wins normal but reversed mode
-      console.log("reverse but normal user win")
+    } else if ((gameStatus = "You Lose!")) {
+      //Computer wins normal
+      console.log("normal computer win");
+      computerWin = computerWin + 1;
+    }
+  } else if (gamemode == "reversed") {
+    if (gameStatus == "You Win!") {
+      //User wins normal but reversed mode
+      console.log("reverse but normal user win");
       computerWin = computerWin + 1;
       gameStatus = "You Lose!";
-    } else if (gameStatus = "You Lose!") {      //Computer wins normal but reversed mode
-      console.log("reverse but normal computer win")
-      userWin = userWin +1;
+    } else if ((gameStatus = "You Lose!")) {
+      //Computer wins normal but reversed mode
+      console.log("reverse but normal computer win");
+      userWin = userWin + 1;
       gameStatus = "You Win!";
     }
   }
 
-  roundCount = roundCount +1
+  roundCount = roundCount + 1;
   return (
-    "You selected "+ gamemode +" mode"+
+    "You selected " +
+    gamemode +
+    " mode" +
     "<br>Computer selected " +
     programHand +
-    "<br>" +username+" selected " +
+    "<br>" +
+    username +
+    " selected " +
     input +
     "<br><br>" +
-    gameStatus + "<br> So far " +username+", you've been winning "+ userWin+"/"+roundCount+" turns. Pretty Good!"+
+    gameStatus +
+    "<br> So far " +
+    username +
+    ", you've been winning " +
+    userWin +
+    "/" +
+    roundCount +
+    " turns. Pretty Good!" +
     "<br><br> Now you can type 'scissors' 'paper' or 'stone' to play another round!"
   );
-}
+};
 
 // generate program hand
 var chooseHand = function () {
@@ -162,4 +150,45 @@ var checkWin = function (player, program) {
     status = "You Lose!";
   }
   return status;
+};
+
+//korean game
+var korean = function (input) {
+  while (ultiWinner == 0) {
+    if (input != "scissors" && input != "stone" && input != "paper") {
+      output = "Please input again either (paper) (scissors) (stone)";
+      return output;
+    } else if (input == "scissors" || input == "paper" || input == "stone") {
+      programHand = chooseHand();
+      gameStatus = checkWin(input, programHand);
+    }
+    if (firstWinner == 0 && gameStatus == "You Win!") {
+      // User wins first round
+      firstWinner = 1;
+      return "You Win the first round! Input 2nd round guess";
+    } else if (firstWinner == 0 && gameStatus == "You Lose!") {
+      // Computer wins first round
+      firstWinner = 2;
+      return "You Lose the first round! Avoid guess the same as computer!";
+    } else if (firstWinner == 0 && gameStatus == "Its a Draw!") {
+      // First round draws
+      return "Draw! input again";
+    } else if (firstWinner == 1 && gameStatus == "Its a Draw!") {
+      // User wins first round and draws second round, User is ultimate winner
+      ultiWinner == 1;
+      return "You are the ultimate Winner!";
+    } else if (firstWinner == 2 && gameStatus == "Its a Draw!") {
+      // Computer wins first round and draws second round, Computer is ultimate winner
+      ultiWinner == 2;
+      return "You lose! Computer is ultimate winner!";
+    } else if (firstWinner == 1 && gameStatus != "Its a Draw!") {
+      // User wins first round but not draw second round
+      firstWinner = 0;
+      return "You lose second round! Try again from first round";
+    } else if (firstWinner == 2 && gameStatus != "Its a Draw!") {
+      // Computer wins first round but not draw second round
+      firstWinner = 0;
+      return "Computer loses second round! Try again";
+    }
+  }
 };
