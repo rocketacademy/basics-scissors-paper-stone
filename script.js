03594ccd9ec1920289b23e1eb872ba8d4bf98a7f
Jia Cheng <jiachxng@gmail.com>
2021-10-23 12:46:29 +0800

    i created the scissors paper stone

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+var sps1 = 'scissors';
+var sps2 = 'paper';
+var sps3 = 'stone';
+var computerSPS ='';
+var myOutputValue = '';
+
+var rollDice = function () {
+  randomInteger = Math.floor(Math.random() * 3);
+  return randomInteger
 };
+
+var SPS = function(){
+  if (randomInteger == 0){
+    computerSPS = sps1;  
+  }
+  if (randomInteger == 1){
+    computerSPS = sps2;
+  }
+  if (randomInteger == 2){
+    computerSPS = sps3;
+  }
+  return computerSPS;
+};
+
+var main = function (input){
+  var myOutputValue = '';
+  if (input == computerSPS){
+    myOutputValue = 'It is a draw, please play again!';
+  }
+
+  if ((input == 'scissors' && computerSPS == 'paper') ||
+  (input == 'paper' && computerSPS == 'stone') ||
+  (input == 'stone' && computerSPS == 'scissors') ){
+  myOutputValue = 'You win!';
+  }
+  if ((input == 'scissors' && computerSPS == 'stone') ||
+  (input == 'paper' && computerSPS == 'scissors') ||
+  (input == 'stone' && computerSPS == 'paper') ){
+  myOutputValue = 'You lose! Please play again';
+  }
+}
